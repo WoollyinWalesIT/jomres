4b92c34b6916aafd1537c498fdce09f88d6f0aad
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2009-03-08 11:03:58 +0000

    Added license info to cms specific files

--- libraries/jomres/cms_specific/cmsms152/cms_specific_functions.php
+++ libraries/jomres/cms_specific/cmsms152/cms_specific_functions.php
@@ -1,5 +1,14 @@
 <?php 
+/*
+This file licensed DILLYGAF. I care so much that I haven't even bothered to write a license.
 
+The Jomres package is proprietary licensed, which means you can't be naughty and give it away, otherwise I'd never be able to make any money to keep developing it.
+
+That said, the files in the /jomres/libraries/jomres/cms_specific/*** directories are licensed under a dillygaf license (just google "dillygaf"), which means I don't care what you do with them, if you want to copy my files and adapt them to make your own and license them solely for your use you can. Even better, you could re-write them to work with X CMS and release them for other people to use.  The only thing you can't do is claim copyright of the original files otherwise you could stop me from using and distributing my own files, and that would be silly. 
+
+I'm hard working, I'm not a genius there are lots of CMSs out there I'm not familiar with and if you can modify this code to make Jomres work on the CMS of your choice then more power to your elbow.
+
+*/
 // ################################################################
 defined( '_JOMRES_INITCHECK' ) or die( 'Direct Access to '.__FILE__.' is not allowed.' );
 // ################################################################
--- libraries/jomres/cms_specific/joomla15/installfiles/admin.jomres.php
+++ libraries/jomres/cms_specific/joomla15/installfiles/admin.jomres.php
@@ -1,19 +1,14 @@
 <?php
-/**
-#
- * Controlling functions/switch for backend functionality
-#
- * @author Vince Wooll <sales@jomres.net>
-#
- * @version Jomres 2.7.5
-#
-* @package Jomres
-#
-* @copyright	2005-2008 Vince Wooll
-#
-* This is not free software, please do not distribute it. For licencing information, please visit http://www.jomres.net/
-* All rights reserved.
- */
+/*
+This file licensed DILLYGAF. I care so much that I haven't even bothered to write a license.
+
+The Jomres package is proprietary licensed, which means you can't be naughty and give it away, otherwise I'd never be able to make any money to keep developing it.
+
+That said, the files in the /jomres/libraries/jomres/cms_specific/*** directories are licensed under a dillygaf license (just google "dillygaf"), which means I don't care what you do with them, if you want to copy my files and adapt them to make your own and license them solely for your use you can. Even better, you could re-write them to work with X CMS and release them for other people to use.  The only thing you can't do is claim copyright of the original files otherwise you could stop me from using and distributing my own files, and that would be silly. 
+
+I'm hard working, I'm not a genius there are lots of CMSs out there I'm not familiar with and if you can modify this code to make Jomres work on the CMS of your choice then more power to your elbow.
+
+*/
 
 if (!defined('JPATH_BASE'))
 	{
