579211fd416c7eba9333d2df303c69e20fb3ab7d
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2009-03-30 10:04:58 +0000

    Fixed warning to give the correct error

--- jomres/integration.php
+++ jomres/integration.php
@@ -1431,7 +1431,7 @@ class mcHandler {
 			{
 			if (!@mkdir($this->remote_plugin_directory))
 				{
-				echo "Error, unable to make folder ".$this->remote_plugin_directory." automatically therefore cannot store booking session data. Please create the folder manually and ensure that it's writable by the web server";
+				echo "Error, unable to make folder ".$this->remote_plugin_directory." automatically therefore cannot store minicomponent path data. Please create the folder manually and ensure that it's writable by the web server";
 				exit;
 				}
 			}
