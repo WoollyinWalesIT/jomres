bdc83f787441e35f55cabe2d6b68227132edfcee
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2009-04-13 16:50:02 +0000

    Modified viewproperty so that booking a room link deliberately points to an invalid date when no date selected, this forces the booking engine to set the arrival date to today's date.

--- jomres/core-minicomponents/j00015viewproperty.class.php
+++ jomres/core-minicomponents/j00015viewproperty.class.php
@@ -252,7 +252,7 @@ class j00015viewproperty
 				{
 				$link				=	array();
 				$url=JOMRES_SITEPAGE_URL."&task=dobooking&amp;selectedProperty=$property_uid";
-				if ($mrConfig['fixedArrivalDateYesNo']=='1') // We'll add an invalid arrival date if the fixed arrival date setting is set to Yes. This way we can force the booking engine to see the arrival date is wrong and it'll rebuild the available rooms list, which it doesn't if the date is correct when coming from the Book a room link.
+				if ($mrConfig['fixedArrivalDateYesNo']=='1'|| $mrConfig['fixedPeriodBookings']=='1') // We'll add an invalid arrival date if the fixed arrival date setting is set to Yes. This way we can force the booking engine to see the arrival date is wrong and it'll rebuild the available rooms list, which it doesn't if the date is correct when coming from the Book a room link.
 					$url.="arrivalDate=2009-01-01";
 					
 					
