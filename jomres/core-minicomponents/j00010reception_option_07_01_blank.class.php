dd7b9ce1a1cb035ff85af9f210fe662cd175eb55
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2009-05-01 14:37:08 +0000

    Licenses updated for clarity

--- jomres/core-minicomponents/j00010reception_option_07_01_blank.class.php
+++ jomres/core-minicomponents/j00010reception_option_07_01_blank.class.php
@@ -1,20 +1,38 @@
 <?php
 /**
-#
- * Mini-component core file: Constructs and displays the receptionist's menu
-#
+
+ * Mini-component core file
+
  * @author Vince Wooll <sales@jomres.net>
-#
+
  * @version Jomres 4
-#
+
 * @package Jomres
 * @subpackage mini-components
-#
+
 * @copyright	2005-2009 Vince Wooll
-#
-* This is not free software, please do not distribute it. For licencing information, please visit http://www.jomres.net/
-* All rights reserved.
- */
+
+Jomres is distributed as a mix of two licenses (excepting other files in the libraries folder, which are independantly licensed). 
+
+The first, proprietary license, refers to Jomres as a package. You cannot share it, period. You can see the full license here http://www.jomres.net/license.html. There are some exceptions, and these files are independantly licensed (see the /jomres/libraries/phptools folder for example)
+
+To be explicit, it refers to all files found in the /jomres folder excepting those examples mentioned previously, and those in the /jomres/core-minicomponents,  /jomres/libraries/jomres/cms_specific and the /jomres/templates folders.
+
+The files in the /jomres/core-minicomponents,  /jomres/libraries/jomres/cms_specific and the /jomres/templates folders, whilst copyright Vince Wooll, are licensed differently to allow those users who wish to develop and distribute their own third party plugins for Jomres. Those files are licensed under the MIT license, which allows third party vendors to modify them to suit their own requirements and if so desired, distribute them for free or cost.
+
+Files not in /jomres/core-minicomponents,  /jomres/libraries/jomres/cms_specific and the /jomres/templates folders are subject to the Jomres proprietary license (http://www.jomres.net/license.html) and cannot be shared/distributed/copied without express written permission of Vince Wooll.
+
+
+The MIT License
+
+Copyright (c) 2005-2009 Vince Wooll
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this file and associated documentation files (the "File"), to deal in the File without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the File, and to permit persons to whom the File is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the File.
+
+THE FILE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE FILE OR THE USE OR OTHER DEALINGS IN THE FILE.
+*/
 
 // ################################################################
 defined( '_JOMRES_INITCHECK' ) or die( 'Direct Access to '.__FILE__.' is not allowed.' );
