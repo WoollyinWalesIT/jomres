c19015fb28f2448080f991ec1f7c735cd73ed558
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2009-05-04 20:15:35 +0000

    fixed issue with IE8 background to beautytips being fixed.

--- jomres/libraries/jomres/classes/jomres_tooltips.class.php
+++ jomres/libraries/jomres/classes/jomres_tooltips.class.php
@@ -123,7 +123,7 @@ class jomres_tooltips
 						cornerRadius: 20, 
 						padding: 20,
 						strokeWidth: 1,
-						width:            \''.$width.'\',
+						width: \'auto\',
 						positions:        [\'right\',\'left\',\'top\',\'bottom\',\'most\'],
 						});
 					</script>
