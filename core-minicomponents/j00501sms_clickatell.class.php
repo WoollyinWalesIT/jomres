1f0f4e22de7f0229088c405854b860988d46f3f3
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2009-05-19 11:32:58 +0000

    .

---
+++ core-minicomponents/j00501sms_clickatell.class.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Core file
+ * @author Vince Wooll <sales@jomres.net>
+ * @version Jomres 4
+* @package Jomres
+* @copyright	2005-2009 Vince Wooll
+
+Jomres is distributed as a mix of two licenses (excepting other files in the libraries folder, which are independantly licensed). 
+
+The first, proprietary license, refers to Jomres as a package. You cannot share it, period. You can see the full license here http://www.jomres.net/license.html. There are some exceptions, and these files are independantly licensed (see the /jomres/libraries/phptools folder for example)
+The files in the /jomres/core-minicomponents,  /jomres/libraries/jomres/cms_specific and the /jomres/templates folders, whilst copyright Vince Wooll, are licensed differently to allow those users who wish, to develop and distribute their own third party plugins for Jomres. Those files are licensed under the MIT license, which allows third party vendors to modify them to suit their own requirements and if so desired, distribute them for free or cost. 
+
+################################################################
+This file is subject to the Jomres proprietary license, please do not distribute it. For licencing information, please visit 
+http://www.jomres.net/index.php?option=com_content&task=view&id=214&Itemid=86 and http://www.jomres.net/license.html
+################################################################
+*/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( 'Direct Access to '.__FILE__.' is not allowed.' );
+// ################################################################
+
+/**
+#
+ * Configuration panel for gallery link input
+ #
+* @package Jomres
+#
+ */
+class j00501sms_clickatell {
+	/**
+	#
+	 * Constructor: Outputs the gallery link config inputs
+	#
+	 */
+	function j00501sms_clickatell($componentArgs)
+		{
+		// Must be in all minicomponents. Minicomponents with templates that can contain editable text should run $this->template_touch() else just return 
+		global $MiniComponents;
+		if ($MiniComponents->template_touch)
+			{
+			$this->template_touchable=false; return;
+			}
+		global $configurationPanel,$jrConfig,$thisJRUser;
+		
+		$sms_clickatell_settings = new jrportal_sms_clickatell_settings();
+		$sms_clickatell_settings->get_sms_clickatell_settings();
+		
+		if ($sms_clickatell_settings->sms_clickatellConfigOptions['active'] == "1")
+			{
+			$mrConfig=$componentArgs['mrConfig'];
+			$configurationPanel->startPanel(_JRPORTAL_SMS_CLICKATELL_TABTITLE);
+			$configurationPanel->setleft(_JRPORTAL_SMS_CLICKATELL_NOTIFICATION_MOBILENUMBER);
+			$configurationPanel->setmiddle('<input type="text" class="inputbox"  size="50" name="cfg_sms_clickatell_notification_number" value="'.$mrConfig['sms_clickatell_notification_number'].'" />');
+			$configurationPanel->setright(_JRPORTAL_SMS_CLICKATELL_NOTIFICATION_MOBILENUMBER_DESC);
+			$configurationPanel->insertSetting();
+			$configurationPanel->endPanel();
+			}
+		}
+
+	/**
+	#
+	 * Must be included in every mini-component
+	#
+	 * Returns any settings the the mini-component wants to send back to the calling script. In addition to being returned to the calling script they are put into an array in the mcHandler object as eg. $mcHandler->miniComponentData[$ePoint][$eName]
+	#
+	 */
+	// This must be included in every Event/Mini-component
+	function getRetVals()
+		{
+		return null;
+		}
+	}
+?>
---
+++ core-minicomponents/j16000sms_clickatell_settings.class.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Core file
+ * @author Vince Wooll <sales@jomres.net>
+ * @version Jomres 4
+* @package Jomres
+* @copyright	2005-2009 Vince Wooll
+
+Jomres is distributed as a mix of two licenses (excepting other files in the libraries folder, which are independantly licensed). 
+
+The first, proprietary license, refers to Jomres as a package. You cannot share it, period. You can see the full license here http://www.jomres.net/license.html. There are some exceptions, and these files are independantly licensed (see the /jomres/libraries/phptools folder for example)
+The files in the /jomres/core-minicomponents,  /jomres/libraries/jomres/cms_specific and the /jomres/templates folders, whilst copyright Vince Wooll, are licensed differently to allow those users who wish, to develop and distribute their own third party plugins for Jomres. Those files are licensed under the MIT license, which allows third party vendors to modify them to suit their own requirements and if so desired, distribute them for free or cost. 
+
+################################################################
+This file is subject to the Jomres proprietary license, please do not distribute it. For licencing information, please visit 
+http://www.jomres.net/index.php?option=com_content&task=view&id=214&Itemid=86 and http://www.jomres.net/license.html
+################################################################
+*/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( 'Direct Access to '.__FILE__.' is not allowed.' );
+// ################################################################
+
+/**
+#
+ * Delete an optional extra
+ #
+* @package Jomres
+#
+ */
+class j16000sms_clickatell_settings {
+	/**
+	#
+	 * Constructor:  Delete an optional extra
+	#
+	 */
+	function j16000sms_clickatell_settings()
+		{
+		// Must be in all minicomponents. Minicomponents with templates that can contain editable text should run $this->template_touch() else just return 
+		global $MiniComponents;
+		if ($MiniComponents->template_touch)
+			{
+			$this->template_touchable=false; return;
+			}
+		global $ePointFilepath;
+		$output=array();
+		$pageoutput=array();
+
+		$sms_clickatell_settings = new jrportal_sms_clickatell_settings();
+		$sms_clickatell_settings->get_sms_clickatell_settings();
+		
+		$yesno = array();
+		$yesno[] = jomresHTML::makeOption( '0', _JOMRES_COM_MR_NO );
+		$yesno[] = jomresHTML::makeOption( '1', _JOMRES_COM_MR_YES );
+	
+		$output['PAGETITLE'] 		=_JRPORTAL_SMS_CLICKATELL_TITLE;
+		$output['HUSERNAME']		=_JRPORTAL_SMS_CLICKATELL_USERNAME;
+		$output['HPASSWORD']		=_JRPORTAL_SMS_CLICKATELL_PASSWORD;
+		$output['HAPI_ID']			=_JRPORTAL_SMS_CLICKATELL_APIID;
+		$output['INSTRUCTIONS']		=_JRPORTAL_SMS_CLICKATELL_INSTRUCTIONS;
+		$output['HACTIVE']			=_JOMCOMP_WISEPRICE_ACTIVE;
+		
+		
+		$output['ACTIVE']	= jomresHTML::selectList( $yesno, 'active','class="inputbox" size="1"', 'value', 'text', $sms_clickatell_settings->sms_clickatellConfigOptions['active']);
+		
+		$output['USERNAME']	= $sms_clickatell_settings->sms_clickatellConfigOptions['username'];
+		$output['PASSWORD']	= $sms_clickatell_settings->sms_clickatellConfigOptions['password'];
+		$output['API_ID'] 	= $sms_clickatell_settings->sms_clickatellConfigOptions['api_id'];
+		$output['JOMRESTOKEN'] ='<input type="hidden" name="jomrestoken" value="'.jomresSetToken().'"><input type="hidden" name="no_html" value="1">';
+
+		$jrtbar = new jomres_toolbar();
+		$jrtb  = $jrtbar->startTable();
+		$jrtb .= $jrtbar->toolbarItem('save','','',true,'save_sms_clickatell_settings');
+		$jrtb .= $jrtbar->toolbarItem('cancel',jomresURL(JOMRES_SITEPAGE_URL_ADMIN),'');
+		$jrtb .= $jrtbar->endTable();
+		$output['JOMRESTOOLBAR']=$jrtb;
+
+		$pageoutput[]=$output;
+		$tmpl = new patTemplate();
+		$tmpl->setRoot( JOMRES_TEMPLATEPATH_ADMINISTRATOR );
+		$tmpl->readTemplatesFromInput( 'sms_clickatell_settings.html');
+		$tmpl->addRows( 'pageoutput',$pageoutput);
+		$tmpl->displayParsedTemplate();
+		}
+
+	/**
+	#
+	 * Must be included in every mini-component
+	#
+	 * Returns any settings the the mini-component wants to send back to the calling script. In addition to being returned to the calling script they are put into an array in the mcHandler object as eg. $mcHandler->miniComponentData[$ePoint][$eName]
+	#
+	 */
+	function getRetVals()
+		{
+		return null;
+		}
+	}
+?>
