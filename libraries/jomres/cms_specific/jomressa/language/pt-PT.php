0d5f3266c6d12e3fe48a550f23e811c4db0be5f1
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2009-09-26 10:45:41 +0000

    Added jomressa alternative language files to svn

---
+++ libraries/jomres/cms_specific/jomressa/language/pt-PT.php
@@ -0,0 +1,43 @@
+<?php
+##################################################################
+defined( '_JOMRES_INITCHECK' ) or die( 'Direct Access to '.__FILE__.' is not allowed.' );
+##################################################################
+
+define('_JOMRES_SA_TITLE','Jomres Standalone');
+define('_JOMRES_SA_LOG_IN','Log in');
+define('_JOMRES_SA_HELLO','Hello');
+define('_JOMRES_SA_SUBMIT',' Submit ');
+define('_JOMRES_SA_YOU_ARE_LOGGED_IN','You are currently logged in.');
+define('_JOMRES_SA_UPDATE_ACCOUNT','Edit account');
+define('_JOMRES_SA_UPDATE_PROFILE','Update user PROFILE');
+define('_JOMRES_SA_USER_ADMIN','Admin page (user / access level update)');
+define('_JOMRES_SA_LOG_OUT','Log out');
+define('_JOMRES_SA_PAGE_ADMIN','Admin page');
+
+define('_JOMRES_SA_FORMS_ENTERLOGINDETAILS' , "Please enter your login and password.");
+define('_JOMRES_SA_FORMS_USERNAME' , "User name");
+define('_JOMRES_SA_FORMS_PASSWORD' , "Password");
+define('_JOMRES_SA_FORMS_AUTOLOGIN' , "Automatic login?");
+define('_JOMRES_SA_FORMS_NOTREGISTEREDYET' , "Not registered yet?");
+define('_JOMRES_SA_FORMS_REGISTER' , "Register");
+define('_JOMRES_SA_FORMS_CLICKHERE' , "Click here");
+define('_JOMRES_SA_FORMS_FORGOTPASSWORD' , "Forgot your password?");
+
+define('_JOMRES_SA_PREVIEW' , "Preview site");
+
+define('_JOMRES_SA_FORMS_UPDATEUSER_TITLE' , "Update user information");
+define('_JOMRES_SA_FORMS_UPDATEUSER_DESC' , "Use this form to modify the account information (fields with a * are required).");
+define('_JOMRES_SA_FORMS_UPDATEUSER_CONFIRMPASSWORD' , "Confirm password");
+define('_JOMRES_SA_FORMS_UPDATEUSER_REALNAME' , "Real name");
+define('_JOMRES_SA_FORMS_UPDATEUSER_EMAIL' , "E-mail");
+define('_JOMRES_SA_FORMS_UPDATEUSER_EXTRAINFO' , "Extra info");
+define('_JOMRES_SA_FORMS_CONFIRMPASSWORDIGNORE' , "Leave blank to ignore");
+define('_JOMRES_SA_FORMS_UPDATEUSER_MINCHARS' , "(min. 8 chars.)");
+
+define('_JOMRES_SA_FORMS_REGISTRATION_TITLE' , "Registration");
+define('_JOMRES_SA_FORMS_REGISTRATION_DESC' , "Please fill in the following fields (fields with a * are required).");
+
+define('_JOMRES_SA_FORMS_FORGOT_TITLE' , "Forgot your password/login?");
+define('_JOMRES_SA_FORMS_FORGOT_DESC' , "Please enter the e-mail address you used during registration.");
+
+
