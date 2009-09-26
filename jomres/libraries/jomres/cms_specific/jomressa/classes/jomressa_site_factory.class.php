d55fbc2dffcca342113d25a043e5d3689d393f24
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2009-09-26 10:36:07 +0000

    Added iswrapped functionality to SA

--- jomres/libraries/jomres/cms_specific/jomressa/classes/jomressa_site_factory.class.php
+++ jomres/libraries/jomres/cms_specific/jomressa/classes/jomressa_site_factory.class.php
@@ -15,8 +15,8 @@ class jomressa_site_factory
 		else
 			$this->currpage="frontend";
 		$this->heads = array(1=>"",2=>"",3=>"");
-		
-		$this->heads[1][] = '<meta name="generator" content="Jomres Standalone" />';
+		if (!defined('JOMRES_WRAPPED'))
+			$this->heads[1][] = '<meta name="generator" content="Jomres Standalone" />';
 		}
 
 	public static function getInstance()
@@ -35,36 +35,52 @@ class jomressa_site_factory
 		
 	public function setHead($type,$content=array())
 		{
-		
-		switch ($type) 
+		if (!defined('JOMRES_WRAPPED'))
 			{
-			case "javascript":
-				$this->heads[3][] = '<script type="text/javascript" src="'.$content['path'].$content['filename'].'"></script>';
-			break;
-			case "css":
-				$this->heads[2][] ='<link type="text/css" rel="stylesheet" href="'.$content['path'].$content['filename'].'" />';
-			break;
-			case "description":
-				$this->heads[1]["description"] ='<meta name="description" content="'.$content['description'].'" />';
-			break;
-			case "keywords":
-				$this->heads[1]["keywords"] = '<meta name="keywords" content="'.$content['keywords'].'" />';
-			break;
-			case "charset":
-				$this->heads[1][] = '<meta http-equiv="Content-Type" content="text/html; charset='.$content['charset'].'" />';
-			break;
-			case "title":
-				$this->heads[1]['title'] = '<title>'.$content['title'].'</title>';
-			break;
-			case "revisit":
-				$this->heads[1][] = '<META NAME="revisit" CONTENT="'.$content['revisit'].' days">';
-			break;
-			case "distribution":
-				$this->heads[1][] = '<META NAME="distribution" CONTENT="'.$content['distribution'].'">';
-			break;
-			default:
-				
-			break;
+			switch ($type) 
+				{
+				case "javascript":
+					$this->heads[3][] = '<script type="text/javascript" src="'.$content['path'].$content['filename'].'"></script>';
+				break;
+				case "css":
+					$this->heads[2][] ='<link type="text/css" rel="stylesheet" href="'.$content['path'].$content['filename'].'" />';
+				break;
+				case "description":
+					$this->heads[1]["description"] ='<meta name="description" content="'.$content['description'].'" />';
+				break;
+				case "keywords":
+					$this->heads[1]["keywords"] = '<meta name="keywords" content="'.$content['keywords'].'" />';
+				break;
+				case "charset":
+					$this->heads[1][] = '<meta http-equiv="Content-Type" content="text/html; charset='.$content['charset'].'" />';
+				break;
+				case "title":
+					$this->heads[1]['title'] = '<title>'.$content['title'].'</title>';
+				break;
+				case "revisit":
+					$this->heads[1][] = '<META NAME="revisit" CONTENT="'.$content['revisit'].' days">';
+				break;
+				case "distribution":
+					$this->heads[1][] = '<META NAME="distribution" CONTENT="'.$content['distribution'].'">';
+				break;
+				default:
+					
+				break;
+				}
+			}
+		else
+			{
+			switch ($type) 
+				{
+				case "javascript":
+					$this->heads[3][] = '<script type="text/javascript" src="'.$content['path'].$content['filename'].'"></script>';
+				break;
+				case "css":
+					$this->heads[2][] ='<link type="text/css" rel="stylesheet" href="'.$content['path'].$content['filename'].'" />';
+				default:
+					
+				break;
+				}
 			}
 		}
 		
