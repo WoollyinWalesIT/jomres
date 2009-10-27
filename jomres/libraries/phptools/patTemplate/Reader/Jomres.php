26651e78c68679838b27fff1a6904fb91480f909
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2009-10-27 07:47:43 +0000

    Added custom paths to showtime class, and the ability to Jomres.phptools/patTemplate/Reader/Jomres.php to pick up this custom path and use it instead of the default path.

--- jomres/libraries/phptools/patTemplate/Reader/Jomres.php
+++ jomres/libraries/phptools/patTemplate/Reader/Jomres.php
@@ -7,21 +7,29 @@ class patTemplate_Reader_Jomres extends patTemplate_Reader
 	{
 	var	$_name = 'Jomres';
 
-
 	function readTemplates($templatename)
 		{
-		$default_root = $this->_options[root]['__default'];
-		$custom_templates =jomres_getSingleton('jomres_custom_template_handler');
-		if ($templatename != "srch.html" && $templatename != "index.html")
+		$custom_paths = get_showtime('custom_paths');
+		if (array_key_exists($templatename,$custom_paths))
 			{
-			if (file_exists($custom_templates->default_template_files_folder.JRDS.$templatename))
-				$content= $custom_templates->getTemplateData($templatename);
-			else
-				$content=file_get_contents($default_root.JRDS.$templatename );
+			$default_root = $custom_paths[$templatename];
+			$content=file_get_contents($default_root.JRDS.$templatename );
 			}
 		else
 			{
-			$content=file_get_contents($default_root.JRDS.$templatename );
+			$default_root = $this->_options[root]['__default'];
+			$custom_templates =jomres_getSingleton('jomres_custom_template_handler');
+			if ($templatename != "srch.html" && $templatename != "index.html")
+				{
+				if (file_exists($custom_templates->default_template_files_folder.JRDS.$templatename))
+					$content= $custom_templates->getTemplateData($templatename);
+				else
+					$content=file_get_contents($default_root.JRDS.$templatename );
+				}
+			else
+				{
+				$content=file_get_contents($default_root.JRDS.$templatename );
+				}
 			}
 		$templates = $this->parseString($content);
 		return $templates;
