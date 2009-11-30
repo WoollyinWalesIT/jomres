37aa2e6a6fc85a7c31f7d537f5038376c15a4e76
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2009-11-30 12:26:54 +0000

    Changed headers to indict Jomres' new GPL license

--- jomres/index.php
+++ jomres/index.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Core file
+ * @author Vince Wooll <sales@jomres.net>
+ * @version Jomres 4 
+* @package Jomres
+* @copyright	2005-2009 Vince Wooll
+* Jomres is currently available for use in all personal or commercial projects under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly. 
+**/
+
 ini_set('error_reporting', E_ALL|E_STRICT);
 ini_set("display_errors",1);
 $dir =  dirname(realpath(__FILE__));
