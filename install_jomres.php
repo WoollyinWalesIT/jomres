1c30e536ec6cc735e977ad70f7b626687150d63d
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2009-12-16 13:11:58 +0000

    Removed two defunct property features from installer.

--- install_jomres.php
+++ install_jomres.php
@@ -1703,8 +1703,6 @@ function insertSampleData()
 
 		$result=doInsertSql("INSERT INTO `#__jomresportal_c_rates` VALUES (1,'default',2,10,'GBP','2009-05-04 13:07:51',0,'0000-00-00 00:00:00')","");
 
-		$result=doInsertSql("INSERT INTO `#__jomres_hotel_features` VALUES (1, 'Sky', 'Sky TV in every room', NULL, '1')","");
-		$result=doInsertSql("INSERT INTO `#__jomres_hotel_features` VALUES (2, 'Disabled access', 'Disabled access available', 'jomres/uploadedimages/pfeatures/facilities_Disabled_Access.png', '1')","");
 		$result=doInsertSql("INSERT INTO `#__jomres_hotel_features` VALUES (3, 'Airport', 'Close to the airport', 'jomres/uploadedimages/pfeatures/airport_nearby.png', '0',0)","");
 
 		$result=doInsertSql("INSERT INTO `#__jomres_hotel_features` VALUES (4, 'Minibar', 'Minibar in room', 'jomres/uploadedimages/pfeatures/air_conditioning.png', '0',0)","");
