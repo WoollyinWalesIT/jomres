7c56f260400f42dd642db9e65b413a0d03e9fe21
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2010-01-16 10:33:33 +0000

    Added html purifier library and supporting code in Jomres to add an extra layer of security for input and output filtering.

--- jomres/admin/admin.jomres.html.php
+++ jomres/admin/admin.jomres.html.php
@@ -81,6 +81,11 @@ function showSiteConfig( $jrConfig, &$lists,$jsInputFormatDropdownList,$licensek
 			<td class="jradmin_subheader_la" valign="top">'._JOMRES_SINGLEPROPERTYINSTALLATION_DESC.'</td>
 		</tr>
 		<tr align="center" valign="middle">
+			<td class="jradmin_subheader_la" valign="top">'._JOMRES_HTMP_PURIFIER.'</td>
+			<td class="jradmin_subheader_la" valign="top">'.$lists['use_html_purifier'].'</td>
+			<td class="jradmin_subheader_la" valign="top">'._JOMRES_HTMP_PURIFIER_DESC.'</td>
+		</tr>
+		<tr align="center" valign="middle">
 			<td class="jradmin_subheader_la" valign="top">'._JOMRES_COM_FAUXHEADERS.'</td>
 			<td class="jradmin_subheader_la" valign="top">'.$lists['outputHeadersInline'].'</td>
 			<td class="jradmin_subheader_la" valign="top">'._JOMRES_COM_FAUXHEADERS_DESC.'</td>
--- jomres/libraries/jomres/functions/jr_gettext.php
+++ jomres/libraries/jomres/functions/jr_gettext.php
@@ -177,6 +177,8 @@ function jr_gettext($theConstant,$theValue,$okToEdit=TRUE,$isLink=FALSE)
 				}
 		}
 	
+	$theText = jomres_purify_html($theText);
+	
 	switch ($jrConfig['utfHTMLdecode'])
 		{
 		case 0:
@@ -190,5 +192,43 @@ function jr_gettext($theConstant,$theValue,$okToEdit=TRUE,$isLink=FALSE)
 			return $theText;
 		}
 	}
+
+
+// Added to this file because it's expected to be used by jr_gettext
+function jomres_purify_html($dirty)
+	{
+	$siteConfig = jomres_getSingleton('jomres_config_site_singleton');
+	$jrConfig=$siteConfig->get();
+	$clean="";
+	
+	$performance_monitor =jomres_getSingleton('jomres_performance_monitor');
+	$performance_monitor->set_point("pre-purification ".time());
+	if ($jrConfig['use_html_purifier'] == "1")
+		{
+		require_once(JOMRESCONFIG_ABSOLUTE_PATH.JRDS.'jomres'.JRDS.'libraries'.JRDS.'htmlpurifier'.JRDS.'HTMLPurifier.standalone.php');
+		// Allowed Elements in HTML
+		$HTML_Allowed_Elms = 'a, abbr, acronym, b, blockquote, br, caption, cite, code, dd, del, dfn, div, dl, dt, em, h1, h2, h3, h4, h5, h6, i, img, ins, kbd, li, ol, p, pre, span, strong, sub, sup, table, tbody, td, tfoot, th, thead, tr, tt, ul, var';
+
+		// Allowed Element Attributes in HTML, element must also be allowed in Allowed Elements for these attributes to work.
+		$HTML_Allowed_Attr = 'abbr.title, acronym.title, blockquote.cite, div.style, h1.style, h2.style, h3.style, h4.style, h5.style, h6.style, img.src, img.alt, img.title, img.class, img.style, img.height, img.width, ol.style, p.style, span.style, span.class, span.id, table.class, table.id, table.border, table.cellpadding, table.cellspacing, table.style, table.width, td.abbr, td.align, td.class, td.id, td.colspan, td.rowspan, td.style, td.valign, tr.align, tr.class, tr.id, tr.style, tr.valign, th.abbr, th.align, th.class, th.id, th.colspan, th.rowspan, th.style, th.valign, ul.style';
+
+		$config = HTMLPurifier_Config::createDefault();
+		$config->set('Cache', 'SerializerPath', JOMRESCONFIG_ABSOLUTE_PATH.JRDS.'jomres'.JRDS.'temp'.JRDS);
+		$config->set('HTML', 'AllowedElements', $HTML_Allowed_Elms); // sets allowed html elements that can be used.
+		$config->set('HTML', 'AllowedAttributes', $HTML_Allowed_Attr); // sets allowed html attributes that can be used.
+
+		$config->set('Core', 'Encoding', 'UTF-8');
+		$config->set('HTML', 'Doctype', 'XHTML 1.0 Strict');
+		$config->set('AutoFormat', 'RemoveEmpty', true);
+		$purifier = new HTMLPurifier($config);
+		$clean = $purifier->purify($dirty);
+		}
+	else
+		$clean = $dirty; // ugh. Of course it's not clean, but if somebody chooses to not use this code, what can you do?
+	unset($purifier);
+	$performance_monitor->set_point("post-purification".time());
+	return $clean;
+	}
+
 
 ?>
