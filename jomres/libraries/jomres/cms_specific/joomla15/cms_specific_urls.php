509a35a34a9fa94174f66138e21d66b0299d0884
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2010-04-12 16:50:54 +0000

    Added an option to cms_specific/joomla15/cms_specific_urls.php 'nofollowtmpl' to tell Jomres to not adopt the tmpl=component when building urls. Useful if, for example, you want Jomres to not include tmpl=component in it's links after it's been called via a url with tmpl=component in the url.

--- jomres/libraries/jomres/cms_specific/joomla15/cms_specific_urls.php
+++ jomres/libraries/jomres/cms_specific/joomla15/cms_specific_urls.php
@@ -56,7 +56,7 @@ $tmpl="";
 if (!isset($_GET['tmpl']) )
 	$_GET['tmpl']=false;
 
-if ($jrConfig['isInIframe'] == (bool)"1" || strstr($scriptname,'index2.php') || $_GET['tmpl'] == 'component' )
+if ( ($jrConfig['isInIframe'] == (bool)"1" || strstr($scriptname,'index2.php') || $_GET['tmpl'] == 'component' ) && !isset($_REQUEST['nofollowtmpl']) )
 	{
 	$index = "index.php";
 	$tmpl="&tmpl=component";
@@ -70,7 +70,7 @@ if (isset($_GET['format']) )
 	if ($_GET['format'] == "raw")
 		define("JOMRES_WRAPPED",1);
 	}
-	
+
 define("JOMRES_SITEPAGE_URL_NOHTML",get_showtime('live_site').'/'."index.php?option=com_jomres&tmpl=component&no_html=1&popup=1&Itemid=".$jomresItemid."");
 define("JOMRES_SITEPAGE_URL_ADMIN",get_showtime('live_site').'/'.JOMRES_ADMINISTRATORDIRECTORY."/index.php?option=com_jomres");
 define("JOMRES_SITEPAGE_URL_SSL",$ssllink."/index.php?option=com_jomres&Itemid=".$jomresItemid."");
