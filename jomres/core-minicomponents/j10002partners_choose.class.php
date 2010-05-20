e57d9ea91d37e42c2bd28f16ced2f8997f8eb668
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2010-05-20 07:53:15 +0000

    added currently unused admin button for partners

---
+++ jomres/core-minicomponents/j10002partners_choose.class.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Core file
+ * @author Vince Wooll <sales@jomres.net>
+ * @version Jomres 4 
+* @package Jomres
+* @copyright	2005-2009 Vince Wooll
+* Jomres is currently available for use in all personal or commercial projects under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly. 
+**/
+
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( 'Direct Access to this file is not allowed.' );
+// ################################################################
+
+class j10002partners_choose
+	{
+	function j10002partners_choose()
+		{
+		// Must be in all minicomponents. Minicomponents with templates that can contain editable text should run $this->template_touch() else just return 
+		$MiniComponents =jomres_getSingleton('mcHandler');
+		if ($MiniComponents->template_touch)
+			{
+			$this->template_touchable=false; return;
+			}
+		return true;
+		$htmlFuncs =jomres_getSingleton('html_functions');
+		$this->cpanelButton=$htmlFuncs->cpanelButton(JOMRES_SITEPAGE_URL_ADMIN.'&task=partners_choose', 'partners.png', _JOMRES_PARTNERS_TITLE);
+		}
+	
+	
+	// This must be included in every Event/Mini-component
+	function getRetVals()
+		{
+		return $this->cpanelButton;
+		}
+	}
+?>
