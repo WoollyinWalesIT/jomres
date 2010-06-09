ee07d8aaeb61f4ae43dab6624e711a9d0f63c89e
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2010-06-09 17:51:07 +0000

    Implemented jquery ui's datepicker functionality to replace the older dhtml date picker which hasn't been updated in years and is starting to show it's age (for ex. August in Chrome doesn't seem to show). I'll leave the older code in-situ for now in case somebody wants it, but in time, once I'm sure that the new code works flawlessly, we will remove it.

--- libraries/jomres/functions/functions.php
+++ libraries/jomres/functions/functions.php
@@ -1808,13 +1808,109 @@ function userHasBeenLoggedOut() {
 } // end of function
 
 
-
 /**
 #
  * Makes a javascript date input field. Creates a random name for the form element each time so that multiple javascript forms can be used on the same page without collision
 #
 */
 function generateDateInput($fieldName,$dateValue,$myID=FALSE,$siteConfig=FALSE,$jrc=FALSE)
+	{
+	// We need to give the javascript date function a random name because it will be called by both the component and modules
+	$uniqueID="";
+	// If this date picker is "arrivalDate" then we need to create a departure date input name too, then set it in showtime. With that we'll be able to tell this set of functionality what the id of the
+	// departureDate is so that it can set it's date when this one changes
+	if ($fieldName != "departureDate")
+		{
+		list($usec,$sec)=explode(" ",microtime());
+		mt_srand($sec * $usec);
+		$possible='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefhijklmnopqrstuvwxyz';
+		for ($i=0; $i<10; $i++)
+			{
+			$key=mt_rand(0, strlen($possible)-1);
+			$uniqueID.=$possible[$key];
+			}
+		set_showtime('departure_date_unique_id',$uniqueID."_XXX") ;
+		}
+	else
+		{
+		$uniqueID=get_showtime('departure_date_unique_id');
+		}
+	
+	if ($dateValue=="")
+		$dateValue = date("Y/m/d");
+	$dateValue=JSCalmakeInputDates($dateValue,$siteConfig);
+	
+	$siteConfig = jomres_getSingleton('jomres_config_site_singleton');
+	$jrConfig=$siteConfig->get();
+	$dateFormat=$jrConfig['cal_input'];
+	$dateFormat = strtolower(str_replace("%","",$dateFormat)); // For the new jquery calendar, we'll strip out the % symbols. This should mean that we don't need to force upgraders to reset their settings.
+	$dateFormat = str_replace("y","yy",$dateFormat);
+	$dateFormat = str_replace("m","mm",$dateFormat);
+	$dateFormat = str_replace("d","dd",$dateFormat);
+
+	if (!defined('_JOMRES_CALENDAR_FORMAT'))
+		define('_JOMRES_CALENDAR_FORMAT','dd/mm/yy');
+
+	$alt_field_string = "";
+	$depature_date_doc_ready = "";
+	if ($fieldName=="arrivalDate")
+		{
+		$alt_field_string = '
+			altField: "#'.get_showtime('departure_date_unique_id').'",
+			
+			';
+		}
+		
+	$output .= '<script type="text/javascript">
+	jQuery(function() {
+		jQuery("#'.$uniqueID.'").datepicker( { 
+			dateFormat: "'.$dateFormat.'", 
+			minDate: 0, maxDate: "+5Y",
+			buttonImage: \''.get_showtime('live_site').'/jomres/images/calendar.png\',
+			buttonImageOnly: true,
+			showOn: "both",
+			changeMonth: true,
+			changeYear: true,
+			numberOfMonths: 1,
+			showOtherMonths: true, 
+			selectOtherMonths: true,
+			showButtonPanel: true,
+			prevText: "'._JOMRES_CALENDAR_PREV.'",
+			nextText: "'._JOMRES_CALENDAR_NEXT.'",
+			currentText: "'._JOMRES_CALENDAR_TODAY.'",
+			'.$alt_field_string.'
+			isRTL: "'._JOMRES_CALENDAR_RTL.'",
+			dayNamesMin: 
+				[
+				"'._JOMRES_COM_MR_WEEKDAYS_SUNDAY_ABBR.'","'._JOMRES_COM_MR_WEEKDAYS_MONDAY_ABBR.'","'._JOMRES_COM_MR_WEEKDAYS_TUESDAY_ABBR.'","'._JOMRES_COM_MR_WEEKDAYS_WEDNESDAY_ABBR.'",
+				"'._JOMRES_COM_MR_WEEKDAYS_THURSDAY_ABBR.'","'._JOMRES_COM_MR_WEEKDAYS_FRIDAY_ABBR.'","'._JOMRES_COM_MR_WEEKDAYS_SATURDAY_ABBR.'"
+				],
+			monthNames: 
+				[
+				"'._JRPORTAL_MONTHS_LONG_0.'","'._JRPORTAL_MONTHS_LONG_1.'","'._JRPORTAL_MONTHS_LONG_2.'","'._JRPORTAL_MONTHS_LONG_3.'","'._JRPORTAL_MONTHS_LONG_4.'","'._JRPORTAL_MONTHS_LONG_5.'",
+				"'._JRPORTAL_MONTHS_LONG_6.'","'._JRPORTAL_MONTHS_LONG_7.'","'._JRPORTAL_MONTHS_LONG_8.'","'._JRPORTAL_MONTHS_LONG_9.'","'._JRPORTAL_MONTHS_LONG_10.'","'._JRPORTAL_MONTHS_LONG_11.'"
+				],
+			monthNamesShort: 
+				[
+				"'._JRPORTAL_MONTHS_LONG_0.'","'._JRPORTAL_MONTHS_LONG_1.'","'._JRPORTAL_MONTHS_LONG_2.'","'._JRPORTAL_MONTHS_LONG_3.'","'._JRPORTAL_MONTHS_LONG_4.'","'._JRPORTAL_MONTHS_LONG_5.'",
+				"'._JRPORTAL_MONTHS_LONG_6.'","'._JRPORTAL_MONTHS_LONG_7.'","'._JRPORTAL_MONTHS_LONG_8.'","'._JRPORTAL_MONTHS_LONG_9.'","'._JRPORTAL_MONTHS_LONG_10.'","'._JRPORTAL_MONTHS_LONG_11.'"
+				]
+			} );
+		
+	});
+	</script>
+	<input type="text" name="'.$fieldName.'" id="'.$uniqueID.'" value="'.$dateValue.'" readonly="readonly"/>
+	';
+	return $output;
+	}
+	
+// Old code
+/**
+#
+ * Makes a javascript date input field. Creates a random name for the form element each time so that multiple javascript forms can be used on the same page without collision
+#
+*/
+function generateDateInput2($fieldName,$dateValue,$myID=FALSE,$siteConfig=FALSE,$jrc=FALSE)
 	{
 	$siteConfig = jomres_getSingleton('jomres_config_site_singleton');
 	$jrConfig=$siteConfig->get();
