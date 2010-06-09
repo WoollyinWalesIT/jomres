026c6582108b181e34b24f65ea1d75e1a61d4af8
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2010-06-09 07:36:09 +0000

    Added FILTER_FLAG_STRIP_HIGH to the generate tab anchor function. Whilst firefox and IE happily work with utf8 chars in an anchor, Safari doesn't.

--- libraries/jomres/functions/functions.php
+++ libraries/jomres/functions/functions.php
@@ -46,7 +46,7 @@ function jomres_generate_tab_anchor($string)
 	$unwanted = array("'","\"",',');
 	$string = str_replace($unwanted,"-",$string);
 	if (function_exists('filter_var'))
-		$anchor = filter_var($string, FILTER_SANITIZE_SPECIAL_CHARS);
+		$anchor = filter_var($string, FILTER_SANITIZE_SPECIAL_CHARS,FILTER_FLAG_STRIP_HIGH);
 	else
 		$anchor = getEscaped($string);
 
