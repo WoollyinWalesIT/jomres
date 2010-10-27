c6b663bb23938f567153d598d1b22d0eac1b14c1
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2010-10-27 15:11:55 +0000

    Moved manager/reception/user toolbars out into their own minicomponents, moved top and bottom templates out into their own minicomponents. This allows us to totally remove these toolbars programatically, if we so wish.

---
+++ core-minicomponents/j00060toptemplate.class.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * Core file
+ * @author Vince Wooll <sales@jomres.net>
+ * @version Jomres 4
+* @package Jomres
+* @copyright	2005-2010 Vince Wooll
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly, however all images, css and javascript which are copyright Vince Wooll are not GPL licensed and are not freely distributable.
+**/
+
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+
+class j00060toptemplate {
+	function j00060toptemplate($componentArgs)
+		{
+		// Must be in all minicomponents. Minicomponents with templates that can contain editable text should run $this->template_touch() else just return
+		$MiniComponents =jomres_getSingleton('mcHandler');
+		if ($MiniComponents->template_touch)
+			{
+			$this->template_touchable=false; return;
+			}
+		$siteConfig = jomres_getSingleton('jomres_config_site_singleton');
+		$jrConfig=$siteConfig->get();
+		$thisJRUser=jomres_getSingleton('jr_user');
+		
+		$tz = $componentArgs['tz'];
+		$jomreslang = $componentArgs['jomreslang'];
+		
+		if (_JOMRES_DETECTED_CMS == "joomla15" || _JOMRES_DETECTED_CMS == "joomla16" )
+			{
+			jr_import('jomres_management_view');
+			$jomres_management_view = new jomres_management_view();
+			if (isset($_POST['management_view']))
+				{
+				if ((bool)$_POST['management_view'] == false)
+					$request = str_replace("tmpl=component","",$_SERVER["REQUEST_URI"]);
+				if ((bool)$_POST['management_view'] == true)
+					$request =  jomresURL($_SERVER["REQUEST_URI"]."&amp;tmpl=component");
+					
+				$jomres_management_view->set_view((bool)$_POST['management_view']);
+				
+				$tmpBookingHandler->close_jomres_session();
+				jomresRedirect( $request,"");
+				}
+			$management_view = $jomres_management_view->get_view();
+			}
+
+		if (!defined('JOMRES_NOHTML'))
+			{
+			$output=array();
+			
+			$output['editing_mode_dropdown']='';
+			$editing_mode =jomres_getSingleton('jomres_editing_mode');
+			$result = $editing_mode->make_editing_mode_dropdown();
+			if ($result)
+				$output['EDITING_MODE_DROPDOWN']=$result;
+			
+			if (_JOMRES_DETECTED_CMS == "joomla15" || _JOMRES_DETECTED_CMS == "joomla16" )
+				{
+				if ($thisJRUser->userIsManager)
+					{
+					if (file_exists(JOMRES_IMAGELOCATION_ABSPATH.'logo.png'))
+						$output['LOGO_RELATIVE_URL']=JOMRES_IMAGELOCATION_RELPATH.'/logo.png';
+					elseif (file_exists(JOMRES_IMAGELOCATION_ABSPATH.'logo.jpg'))
+						$output['LOGO_RELATIVE_URL']=JOMRES_IMAGELOCATION_RELPATH.'/logo.jpg';
+					else
+						$output['LOGO_RELATIVE_URL']=get_showtime('live_site').'/jomres/images/jrlogo.png';
+					$management_dropdown = $jomres_management_view->get_dropdown();
+					$output['MANAGEMENT_VIEW_DROPDOWN']=$management_dropdown;
+					if ($jrConfig['use_timezone_switcher'] == "1")
+						{
+						$output['TIMEZONE_DROPDOWN']=$tz->get_dropdown();
+						$output['TIMEZONEBLURB']= outputDate(date("Y/m/d"))." ".date("H:i:s");
+						}
+					}
+				}
+
+			$output['LANGDROPDOWN']=$jomreslang->get_languageselection_dropdown();
+			$output['BACKLINK']='<a href="javascript:history.go(-1)">'.jr_gettext('_JOMRES_COM_MR_BACK',_JOMRES_COM_MR_BACK).'</a>';
+			$output['LIVESITE']=get_showtime('live_site');
+			$output['DATEPICKERLANG']=JOMRESDATEPICKERLANG;
+			$messaging = array();
+			$sticky_messaging = array();
+			if ($jrConfig['useJomresMessaging'] == '1')
+				{
+				$jomres_messaging =jomres_getSingleton('jomres_messages');
+				$messages = $jomres_messaging->get_messages();
+				
+				if (count($messages)>0)
+					{
+					foreach ($messages as $mes)
+						{
+						$m['MESSAGE']=$mes;
+						$messaging[] = $m;
+						}
+					}
+				$jomres_sticky_messaging =jomres_getSingleton('jomres_sticky_messages');
+				$sticky_messages = $jomres_sticky_messaging->get_messages();
+				
+				if (count($sticky_messages)>0)
+					{
+					foreach ($sticky_messages as $mes)
+						{
+						$m['MESSAGE']=$mes;
+						$sticky_messaging[] = $m;
+						}
+					}
+				}
+			$pageoutput[]=$output;
+			$tmpl = new patTemplate();
+			$tmpl->setRoot( JOMRES_TEMPLATEPATH_FRONTEND );
+			if ($management_view)
+				$tmpl->readTemplatesFromInput( 'management_top.html');
+			else
+				$tmpl->readTemplatesFromInput( 'top.html');
+			$tmpl->addRows( 'pageoutput',$pageoutput);
+			$tmpl->addRows( 'messages',$messaging);
+			$tmpl->addRows( 'sticky_messages',$sticky_messaging);
+			$tmpl->displayParsedTemplate();
+			$pageoutput=array();
+			$output=array();
+			}
+
+		}
+
+	/**
+	#
+	 * Must be included in every mini-component
+	#
+	 */
+	// This must be included in every Event/Mini-component
+	function getRetVals()
+		{
+		return null;
+		}
+	}
+?>
---
+++ core-minicomponents/j00061bottomtemplate.class.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Core file
+ * @author Vince Wooll <sales@jomres.net>
+ * @version Jomres 4
+* @package Jomres
+* @copyright	2005-2010 Vince Wooll
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly, however all images, css and javascript which are copyright Vince Wooll are not GPL licensed and are not freely distributable.
+**/
+
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+
+class j00061bottomtemplate {
+	function j00061bottomtemplate($componentArgs)
+		{
+		// Must be in all minicomponents. Minicomponents with templates that can contain editable text should run $this->template_touch() else just return
+		$MiniComponents =jomres_getSingleton('mcHandler');
+		if ($MiniComponents->template_touch)
+			{
+			$this->template_touchable=false; return;
+			}
+		$jomres_tooltips =jomres_getSingleton('jomres_tooltips');
+		$tmpl = new patTemplate();
+		$tmpl->setRoot( JOMRES_TEMPLATEPATH_FRONTEND );
+		if ($management_view)
+			$tmpl->readTemplatesFromInput( 'management_bottom.html');
+		else
+			$tmpl->readTemplatesFromInput( 'bottom.html');
+		$tmpl->displayParsedTemplate();
+		}
+
+	/**
+	#
+	 * Must be included in every mini-component
+	#
+	 */
+	// This must be included in every Event/Mini-component
+	function getRetVals()
+		{
+		return null;
+		}
+	}
+?>
