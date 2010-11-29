a6dc1a81aa02c0670d2139f871df66ed6b51bfac
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2010-11-29 12:35:30 +0000

    Small change to list user's invoices so that template is still shown if the list is empty.

--- jomres/core-minicomponents/j06005list_usersinvoices.class.php
+++ jomres/core-minicomponents/j06005list_usersinvoices.class.php
@@ -2,10 +2,10 @@
 /**
  * Core file
  * @author Vince Wooll <sales@jomres.net>
- * @version Jomres 4 
+ * @version Jomres 4
 * @package Jomres
 * @copyright	2005-2010 Vince Wooll
-* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly, however all images, css and javascript which are copyright Vince Wooll are not GPL licensed and are not freely distributable. 
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly, however all images, css and javascript which are copyright Vince Wooll are not GPL licensed and are not freely distributable.
 **/
 
 // ################################################################
@@ -45,44 +45,43 @@ class j06005list_usersinvoices
 				}
 
 			$invoices=invoices_getinvoicesfor_juser($id,$stat);
-		//var_dump($invoices);exit;
+
+			$invoice_id_array=array();
+			foreach ($invoices as $invoice)
+				{
+				$invoice_id_array[]=$invoice['id'];
+				}
+			$invoices_items = invoices_getalllineitems_forinvoice_ids($invoice_id_array);
+
+			$output=array();
+			$pageoutput=array();
+			$rows=array();
+
+			$output['PAGETITLE']=_JRPORTAL_INVOICES_TITLE;
+			$output['HUSER']=_JRPORTAL_INVOICES_USER;
+			$output['HSTATUS']=_JRPORTAL_INVOICES_STATUS;
+			$output['HRAISED']=_JRPORTAL_INVOICES_RAISED;
+			$output['HDUE']=_JRPORTAL_INVOICES_DUE;
+			$output['HPAID']=_JRPORTAL_INVOICES_STATUS_PAID;
+			$output['HSUBSCRIPTION']=_JRPORTAL_INVOICES_SUBSCRIPTION;
+			$output['HINITTOTAL']=_JRPORTAL_INVOICES_INITTOTAL;
+			$output['HRECURTOTAL']=_JRPORTAL_INVOICES_RECUR_TOTAL;
+			$output['HFREQ']=_JRPORTAL_INVOICES_RECUR_FREQUENCY;
+			$output['HDOM']=_JRPORTAL_INVOICES_RECUR_DOMONTH;
+			$output['HCURRENCYCODE']=_JRPORTAL_INVOICES_CURRENCYCODE;
+
+			$output['TASK_FILTER_ANY']='<a href="'.JOMRES_SITEPAGE_URL.'&task=list_usersinvoices">'._JOMRES_FRONT_ROOMSMOKING_EITHER.'</a>';
+			$output['TASK_FILTER_UNPAID']='<a href="'.JOMRES_SITEPAGE_URL.'&task=list_usersinvoices&status=unpaid">'._JRPORTAL_INVOICES_STATUS_UNPAID.'</a>';
+			$output['TASK_FILTER_PAID']='<a href="'.JOMRES_SITEPAGE_URL.'&task=list_usersinvoices&status=paid">'._JRPORTAL_INVOICES_STATUS_PAID.'</a>';
+			$output['TASK_FILTER_CANCELLED']='<a href="'.JOMRES_SITEPAGE_URL.'&task=list_usersinvoices&status=cancelled">'._JRPORTAL_INVOICES_STATUS_CANCELLED.'</a>';
+			$output['TASK_FILTER_PENDING']='<a href="'.JOMRES_SITEPAGE_URL.'&task=list_usersinvoices&status=pending">'._JRPORTAL_INVOICES_STATUS_PENDING.'</a>';
+
 			if (count($invoices)>0)
 				{
-				$invoice_id_array=array();
 				foreach ($invoices as $invoice)
 					{
-					$invoice_id_array[]=$invoice['id'];
-					}
-				$invoices_items = invoices_getalllineitems_forinvoice_ids($invoice_id_array);
-
-				$output=array();
-				$pageoutput=array();
-				$rows=array();
-				
-				$output['PAGETITLE']=_JRPORTAL_INVOICES_TITLE;
-				$output['HUSER']=_JRPORTAL_INVOICES_USER;
-				$output['HSTATUS']=_JRPORTAL_INVOICES_STATUS;
-				$output['HRAISED']=_JRPORTAL_INVOICES_RAISED;
-				$output['HDUE']=_JRPORTAL_INVOICES_DUE;
-				$output['HPAID']=_JRPORTAL_INVOICES_STATUS_PAID;
-				$output['HSUBSCRIPTION']=_JRPORTAL_INVOICES_SUBSCRIPTION;
-				$output['HINITTOTAL']=_JRPORTAL_INVOICES_INITTOTAL;
-				$output['HRECURTOTAL']=_JRPORTAL_INVOICES_RECUR_TOTAL;
-				$output['HFREQ']=_JRPORTAL_INVOICES_RECUR_FREQUENCY;
-				$output['HDOM']=_JRPORTAL_INVOICES_RECUR_DOMONTH;
-				$output['HCURRENCYCODE']=_JRPORTAL_INVOICES_CURRENCYCODE;
-
-				$output['TASK_FILTER_ANY']='<a href="'.JOMRES_SITEPAGE_URL.'&task=list_usersinvoices">'._JOMRES_FRONT_ROOMSMOKING_EITHER.'</a>';
-				$output['TASK_FILTER_UNPAID']='<a href="'.JOMRES_SITEPAGE_URL.'&task=list_usersinvoices&status=unpaid">'._JRPORTAL_INVOICES_STATUS_UNPAID.'</a>';
-				$output['TASK_FILTER_PAID']='<a href="'.JOMRES_SITEPAGE_URL.'&task=list_usersinvoices&status=paid">'._JRPORTAL_INVOICES_STATUS_PAID.'</a>';
-				$output['TASK_FILTER_CANCELLED']='<a href="'.JOMRES_SITEPAGE_URL.'&task=list_usersinvoices&status=cancelled">'._JRPORTAL_INVOICES_STATUS_CANCELLED.'</a>';
-				$output['TASK_FILTER_PENDING']='<a href="'.JOMRES_SITEPAGE_URL.'&task=list_usersinvoices&status=pending">'._JRPORTAL_INVOICES_STATUS_PENDING.'</a>';
-
-				foreach ($invoices as $invoice)
-					{
-	
 					$r=array();
-					
+
 					$inv_id = $invoice['id'];
 					$invoice_items = $invoices_items[$inv_id];
 					$item_name_string = "";
@@ -91,9 +90,8 @@ class j06005list_usersinvoices
 						$item_name_string .= $invoice_item['name']."<br/>";
 						}
 					$r['ITEMS']=$item_name_string;
-
 					$r['ID']=$invoice['id'];
-					
+
 					jr_import('jrportal_user_functions');
 					$user_obj = new jrportal_user_functions();
 					$user_deets=$user_obj->getJoomlaUserDetailsForJoomlaId($invoice['cms_user_id']);
@@ -123,29 +121,27 @@ class j06005list_usersinvoices
 						if ($invoice['subscription'] == "0" && $invoice['status'] != "1")
 							$r['PAYNOW']='<a href="'.JOMRES_SITEPAGE_URL.'&task=immediatepay&id='.$invoice['id'].'"><img src = "jomres/images/btn_paynow_SM.gif" /></a>';
 						}
-					
-					
-					
 					$r['EDITLINK']='<a href="'.JOMRES_SITEPAGE_URL.'&task=view_invoice&id='.$invoice['id'].'">'.$infoIcon.'</a>';
 					$rows[]=$r;
 					}
-				$output['JOMRES_SITEPAGE_URL']=JOMRES_SITEPAGE_URL;
-				
-				$pageoutput[]=$output;
-				$tmpl = new patTemplate();
-				$tmpl->setRoot( JOMRES_TEMPLATEPATH_FRONTEND );
-				$tmpl->readTemplatesFromInput( 'frontend_list_invoices.html');
-				$tmpl->addRows( 'pageoutput',$pageoutput);
-				$tmpl->addRows( 'rows',$rows);
-				$tmpl->displayParsedTemplate();
 				}
+
+			$output['JOMRES_SITEPAGE_URL']=JOMRES_SITEPAGE_URL;
+
+			$pageoutput[]=$output;
+			$tmpl = new patTemplate();
+			$tmpl->setRoot( JOMRES_TEMPLATEPATH_FRONTEND );
+			$tmpl->readTemplatesFromInput( 'frontend_list_invoices.html');
+			$tmpl->addRows( 'pageoutput',$pageoutput);
+			$tmpl->addRows( 'rows',$rows);
+			$tmpl->displayParsedTemplate();
 			}
 		}
-	
-	
+
+
 	// This must be included in every Event/Mini-component
 	function getRetVals()
 		{
 		return null;
-		}	
+		}
 	}
