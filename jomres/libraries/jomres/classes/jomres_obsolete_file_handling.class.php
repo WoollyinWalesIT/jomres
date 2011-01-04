d5cba05ca2d50d708591d356e5c3db4d63b355a0
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2011-01-04 11:38:32 +0000

    Removed jquery-1.4.2.min.js from our javascript library

--- jomres/libraries/jomres/classes/jomres_obsolete_file_handling.class.php
+++ jomres/libraries/jomres/classes/jomres_obsolete_file_handling.class.php
@@ -152,6 +152,7 @@ class jomres_obsolete_file_handling
 		$this->add_obs_file($this->dir_javascript.'jquery.corner.js');
 		$this->add_obs_file($this->dir_javascript.'interface.js');
 		$this->add_obs_file($this->dir_javascript.'jquery.blockUI.js');
+		$this->add_obs_file($this->dir_javascript.'jquery-1.4.2.min.js');
 		
 		$this->add_obs_file($this->dir_libraries.'PHPMailer_v2.0.0'.JRDS.'class.phpmailer.php');
 		$this->add_obs_file($this->dir_libraries.'PHPMailer_v2.0.0'.JRDS.'class.smtp.php');
