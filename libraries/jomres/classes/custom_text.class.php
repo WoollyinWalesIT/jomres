4114bedda694676ad423c18d84f460f22ff80b8b
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2011-01-15 14:53:56 +0000

    Modified custom_text.class.php to actually work as a singleton

--- libraries/jomres/classes/custom_text.class.php
+++ libraries/jomres/classes/custom_text.class.php
@@ -18,7 +18,30 @@ defined( '_JOMRES_INITCHECK' ) or die( '' );
 
 class custom_text
 	{
-	function custom_text() 
+	private static $configInstance;
+	private static $internal_debugging;
+
+	public function __construct() 
+		{
+		self::$internal_debugging = false;
+		$this->xcustom_text();
+		}
+
+	public static function getInstance()
+		{
+		if (!self::$configInstance)
+			{
+			self::$configInstance = new custom_text();
+			}
+		return self::$configInstance;
+		}
+		
+	public function __clone()
+		{
+		trigger_error('Cloning not allowed on a singleton object', E_USER_ERROR);
+		}
+		
+	function xcustom_text() 
 		{
 		$this->lang=get_showtime('lang');
 		$this->global_custom_text=array();
@@ -40,22 +63,29 @@ class custom_text
 		{
 		if ($property_uid > 0)
 			$this->property_uid = $property_uid;
-		$current_custom_text=$this->global_custom_text;
-		$query="SELECT constant,customtext FROM #__jomres_custom_text WHERE property_uid = '".(int)$property_uid."' AND language = '".$this->lang."'";
-		
-		$customTextList=doSelectSql($query);
-		if (count($customTextList))
+		if (!isset($this->property_uids_custom_text[(int)$this->property_uid]))
 			{
-			$customTextArray=array();
-			foreach ($customTextList as $text)
+			$current_custom_text=$this->global_custom_text;
+			$query="SELECT constant,customtext FROM #__jomres_custom_text WHERE property_uid = '".(int)$property_uid."' AND language = '".$this->lang."'";
+			
+			$customTextList=doSelectSql($query);
+			if (count($customTextList))
 				{
-				$theConstant=str_replace("sc<x>ript","script",$text->constant);
-				$current_custom_text[$theConstant]=stripslashes($text->customtext);
-				//echo $theConstant." - ".$text->customtext."<br>
-				//";
+				$customTextArray=array();
+				foreach ($customTextList as $text)
+					{
+					$theConstant=str_replace("sc<x>ript","script",$text->constant);
+					$current_custom_text[$theConstant]=stripslashes($text->customtext);
+					//echo $theConstant." - ".$text->customtext."<br>
+					//";
+					}
+				//echo $property_uid." ".$current_custom_text."  CUSTOM<br>";
+				$this->property_uids_custom_text[(int)$property_uid]= $current_custom_text;
 				}
-			//echo $property_uid." ".$current_custom_text."  CUSTOM<br>";
-			$this->property_uids_custom_text[(int)$property_uid]= $current_custom_text;
+			}
+		else
+			{
+			$current_custom_text=$this->property_uids_custom_text[$property_uid];
 			}
 		return $current_custom_text;
 		}
