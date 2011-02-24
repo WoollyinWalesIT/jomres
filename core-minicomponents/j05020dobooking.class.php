bec8218c5465a3128e3fdb121bd7b73915595544
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2011-02-24 09:18:39 +0000

    .

--- core-minicomponents/j05020dobooking.class.php
+++ core-minicomponents/j05020dobooking.class.php
@@ -167,6 +167,8 @@ class j05020dobooking {
 		$output[]		=jr_gettext('_JOMRES_NUMBER_OF_ROOMS',_JOMRES_NUMBER_OF_ROOMS);
 		$output[]		=jr_gettext('_JOMRES_PARTNER_DISCOUNT',_JOMRES_PARTNER_DISCOUNT);
 		$output[]		=jr_gettext('_JOMRES_BOOKINGFORM_NOROOMSSELECTEDYET',_JOMRES_BOOKINGFORM_NOROOMSSELECTEDYET);
+		$output[]		=jr_gettext('_JOMRES_FRONT_MR_BOOKED',_JOMRES_FRONT_MR_BOOKED);
+		
 		
 		
 		foreach ($output as $o)
