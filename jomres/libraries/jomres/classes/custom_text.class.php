662812e9e1f8d7d54474a75f498278b7315e6ed6
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2011-03-14 16:27:30 +0000

    Further changes to custom_text class to improve detection of current language strings.

--- jomres/libraries/jomres/classes/custom_text.class.php
+++ jomres/libraries/jomres/classes/custom_text.class.php
@@ -85,12 +85,10 @@ class custom_text
 			$this->property_uid = $property_uid;
 		
 		$current_custom_text=$this->global_custom_text[$this->lang];
-		
-		//var_dump($this->all_properties_custom_text[$this->property_uid][$this->lang]);exit;
-		
-		if ( (int) $this->property_uid > 0 && isset($this->global_custom_text[$this->lang]) )
+
+		if ( (int) $this->property_uid > 0 && isset($this->global_custom_text[$this->lang]) &&  isset($this->all_properties_custom_text[$this->property_uid][$this->lang]))
 			$current_custom_text=array_merge ($this->global_custom_text[$this->lang],$this->all_properties_custom_text[$this->property_uid][$this->lang]);
-		elseif (!isset($this->global_custom_text[$this->lang]))
+		elseif (isset($this->all_properties_custom_text[$this->property_uid][$this->lang]))
 			$current_custom_text=$this->all_properties_custom_text[$this->property_uid][$this->lang];
 
 		return $current_custom_text;
