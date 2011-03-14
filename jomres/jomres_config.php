9c569e35c2f897ebc96a49997670b10d5a0d61e2
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2011-03-14 16:34:52 +0000

    Added bulgarian to Jomres lang library. Many thanks Hristo Yanev

--- jomres/jomres_config.php
+++ jomres/jomres_config.php
@@ -14,7 +14,7 @@ defined( '_JOMRES_INITCHECK' ) or die( '' );
 // ################################################################
 
 
-$mrConfig['version']='4.7.4';
+$mrConfig['version']='4.7.5';
 $mrConfig['newTariffModels'] = "1";
 $mrConfig['singlePersonSuppliment']='0';
 $mrConfig['singlePersonSupplimentCost']='10';
