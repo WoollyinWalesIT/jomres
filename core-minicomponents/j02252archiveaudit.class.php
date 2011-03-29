b7eafd06ec4d53bdaa73238f95dbf422ff90b742
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2011-03-29 11:16:58 +0000

    Improved archive audit trail functionality to only archive 1000 records at a time, and run far fewer queries.

--- core-minicomponents/j02252archiveaudit.class.php
+++ core-minicomponents/j02252archiveaudit.class.php
@@ -34,31 +34,31 @@ class j02252archiveaudit {
 			{
 			$this->template_touchable=false; return;
 			}
-		if (!jomresCheckToken()) {trigger_error ("Invalid token", E_USER_ERROR);}
+
 		$defaultProperty=getDefaultProperty();
 		$uidArray=array();
 		$saveMessage=jr_gettext('_JOMRES_MR_AUDIT_ARCHIVED_MESSAGE',_JOMRES_MR_AUDIT_ARCHIVED_MESSAGE,FALSE);
-		$query="SELECT `uid`,`date` ,`time` ,`owner` ,`op` ,`args` ,`property_uid` FROM #__jomres_audit WHERE property_uid = ".(int)$defaultProperty;
+		$query="SELECT `uid`,`date` ,`time` ,`owner` ,`op` ,`args` ,`property_uid` FROM #__jomres_audit WHERE property_uid = ".(int)$defaultProperty." LIMIT 1000";
 		$auditList=doSelectSql($query);
+		
+		$query="INSERT INTO #__jomres_audit_archive (`date`,`time`,`owner`,`op`,`args`,`property_uid`) VALUES ";
 		foreach ($auditList as $audit)
 			{
 			$uidArray[]=$audit->uid;
 			$args=addslashes($audit->args);
-			$query="INSERT INTO #__jomres_audit_archive (`date`,`time`,`owner`,`op`,`args`,`property_uid`) VALUES
-			('".$audit->date."','".$audit->time."','".$audit->owner."','".$audit->op."','".$args."',".(int)$audit->property_uid.")";
-			doInsertSql($query,$query);
-			//if (!doInsertSql($query,$query))
-				//trigger_error ("Archive audit, unable to move data to archive table", E_USER_ERROR);
+			$query .= "('".$audit->date."','".$audit->time."','".$audit->owner."','".$audit->op."','".$args."',".(int)$audit->property_uid."),";
 			}
-		$counter=0;
+		$query=substr($query,0,strlen($query)-1);
+		$result =doInsertSql($query,$query);
+		$gor=genericOr($uidArray,'uid');
 		foreach ($uidArray as $uid)
 			{
-			$query="DELETE FROM #__jomres_audit WHERE uid = ".(int)$uid."";
+			$query="DELETE FROM #__jomres_audit WHERE ".$gor;
 			if (!doInsertSql($query,""))
 				trigger_error ("Archive audit, unable to delete archived logs", E_USER_ERROR);
-			$counter++;
+			
 			}
-		jomres_audit(jr_gettext('_JOMRES_MR_AUDIT_ARCHIVED_AUDIT',_JOMRES_MR_AUDIT_ARCHIVED_AUDIT,FALSE),$counter.$saveMessage);
+		jomres_audit(jr_gettext('_JOMRES_MR_AUDIT_ARCHIVED_AUDIT',_JOMRES_MR_AUDIT_ARCHIVED_AUDIT,FALSE),count($uidArray).$saveMessage);
 		$jomres_messaging =jomres_getSingleton('jomres_messages');
 		$jomres_messaging->set_message($counter.$saveMessage);
 		jomresRedirect( jomresURL(JOMRES_SITEPAGE_URL."&task=showAuditTrail"), $saveMessage );
