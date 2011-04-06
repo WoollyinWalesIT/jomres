89cb465b7e24811fbc4f13942724e74ee9b99306
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2011-04-06 13:55:22 +0000

    And another jomres_decode.

--- jomres/core-minicomponents/j06002listyourproperties.class.php
+++ jomres/core-minicomponents/j06002listyourproperties.class.php
@@ -72,7 +72,7 @@ class j06002listyourproperties
 			else
 				$jrtb .= $jrtbar->toolbarItem('publish',jomresURL(JOMRES_SITEPAGE_URL.'&task=publishProperty'.jomresURLToken().'&property_uid='.$p->propertys_uid),jr_gettext('_JOMRES_COM_MR_VRCT_UNPUBLISH',_JOMRES_COM_MR_VRCT_UNPUBLISH,false) );
 			$r['PUBLISHLINK']=$jrtb .= $jrtbar->endTable();
-			$r['PROPERTYNAME']=$p->property_name;
+			$r['PROPERTYNAME']=jomres_decode($p->property_name);
 			$r['PROPERTYADDRESS']=jomres_decode($p->property_street).', '.jomres_decode($p->property_town).', '.jomres_decode($p->property_region).', '.jomres_decode($p->property_country).', '.$p->property_postcode;
 			$rows[]=$r;
 			}
