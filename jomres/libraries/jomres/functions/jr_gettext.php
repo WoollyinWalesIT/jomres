45cb9b4b1491d610618c895d3cdc9a8187799a2d
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2011-04-28 12:49:48 +0000

    Overhauled the input filtering functionality of Jomres, removing the < x > code that's inserted by our own remove xss code. Whilst it was lovely and safe, it tends to interfere too much with things like tasks and email addresses.
    Instead the html purifier functionality has been rewritten to be called as a singleton in a new class. This appears to be much quicker than we did it before because instead of initialising a new class every time, we're reusing the same instance. We've also upgraded html purifier to 4.3.0, instead of the older, obsolete 4.0.

--- jomres/libraries/jomres/functions/jr_gettext.php
+++ jomres/libraries/jomres/functions/jr_gettext.php
@@ -216,29 +216,16 @@ function jomres_purify_html($dirty,$editing)
 	
 	$performance_monitor =jomres_getSingleton('jomres_performance_monitor');
 	$performance_monitor->set_point("pre-purification ".time());
-	if ($jrConfig['use_html_purifier'] == "1" && !$editing)
-		{
-		require_once(JOMRESCONFIG_ABSOLUTE_PATH.JRDS.'jomres'.JRDS.'libraries'.JRDS.'htmlpurifier'.JRDS.'HTMLPurifier.standalone.php');
-		// Allowed Elements in HTML
-		$HTML_Allowed_Elms = 'a, abbr, acronym, b, blockquote, br, caption, cite, code, dd, del, dfn, div, dl, dt, em, h1, h2, h3, h4, h5, h6, i, img, ins, kbd, li, ol, p, pre, span, strong, sub, sup, table, tbody, td, tfoot, th, thead, tr, tt, ul, var';
-
-		// Allowed Element Attributes in HTML, element must also be allowed in Allowed Elements for these attributes to work.
-		$HTML_Allowed_Attr = 'abbr.title, acronym.title, blockquote.cite, div.style, h1.style, h2.style, h3.style, h4.style, h5.style, h6.style, img.src, img.alt, img.title, img.class, img.style, img.height, img.width, ol.style, p.style, span.style, span.class, span.id, table.class, table.id, table.border, table.cellpadding, table.cellspacing, table.style, table.width, td.abbr, td.align, td.class, td.id, td.colspan, td.rowspan, td.style, td.valign, tr.align, tr.class, tr.id, tr.style, tr.valign, th.abbr, th.align, th.class, th.id, th.colspan, th.rowspan, th.style, th.valign, ul.style';
-
-		$config = HTMLPurifier_Config::createDefault();
-		$config->set('Cache', 'SerializerPath', JOMRESCONFIG_ABSOLUTE_PATH.JRDS.'jomres'.JRDS.'temp'.JRDS);
-		$config->set('HTML', 'AllowedElements', $HTML_Allowed_Elms); // sets allowed html elements that can be used.
-		$config->set('HTML', 'AllowedAttributes', $HTML_Allowed_Attr); // sets allowed html attributes that can be used.
-
-		$config->set('Core', 'Encoding', 'UTF-8');
-		$config->set('HTML', 'Doctype', 'XHTML 1.0 Strict');
-		$config->set('AutoFormat', 'RemoveEmpty', true);
-		$purifier = new HTMLPurifier($config);
-		$clean = $purifier->purify($dirty);
-		}
-	else
-		$clean = $dirty; // ugh. Of course it's not clean, but if somebody chooses to not use this code, what can you do?
-	unset($purifier);
+	$html_purifier = jomres_getSingleton('jomres_html_purifier_singleton');
+	$clean = $html_purifier->purify($dirty);
+	
+	// if ($jrConfig['use_html_purifier'] == "1" && !$editing)
+		// {
+		// $html_purifier = jomres_getSingleton('jomres_html_purifier_singleton');
+		// $clean = $html_purifier->purify($dirty);
+		// }
+	// else
+		// $clean = $dirty; // ugh. Of course it's not clean, but if somebody chooses to not use this code, what can you do?
 	$performance_monitor->set_point("post-purification".time());
 	return $clean;
 	}
