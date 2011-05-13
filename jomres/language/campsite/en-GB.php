a738325221d2ecc0f83dc066af6be7840fdec7cd
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2011-05-13 14:51:00 +0000

    Reworded the optional extras selection header to make it more generic.

--- jomres/language/campsite/en-GB.php
+++ jomres/language/campsite/en-GB.php
@@ -609,7 +609,7 @@ define('_JOMRES_AJAXFORM_AVAILABLE','Availability ');
 define('_JOMRES_AJAXFORM_AVAILABLE_DESC','Select the pitchs that you require');
 define('_JOMRES_AJAXFORM_AVAILABLE_DESC_SRP','Look here to see if the campsite is available at this time.');
 define('_JOMRES_AJAXFORM_EXTRAS','Enhance Your Stay');
-define('_JOMRES_AJAXFORM_EXTRAS_DESC','Select any optional extras you want to include in the booking');
+define('_JOMRES_AJAXFORM_EXTRAS_DESC','Select any services you would like to include in the booking');
 define('_JOMRES_COM_PERDAY','Per night');
 define('_JOMRES_AJAXFORM_ADDRESS','Your Details');
 define('_JOMRES_AJAXFORM_ADDRESS_DESC','Please enter your details. Note that all fields are required except your mobile number');
