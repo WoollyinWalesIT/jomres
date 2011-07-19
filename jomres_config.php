b38ac706553bf49705ab8ca85717e8ed30195487
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2011-07-19 10:55:01 +0000

    Finalise 5.2.1

--- jomres_config.php
+++ jomres_config.php
@@ -14,7 +14,7 @@ defined( '_JOMRES_INITCHECK' ) or die( '' );
 // ################################################################
 
 
-$mrConfig['version']='5.2stable';
+$mrConfig['version']='5.2.1';
 $mrConfig['newTariffModels'] = "1";
 $mrConfig['singlePersonSuppliment']='0';
 $mrConfig['singlePersonSupplimentCost']='10';
