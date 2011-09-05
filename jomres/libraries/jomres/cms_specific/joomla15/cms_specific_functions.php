51ac37af741fffe80d95da2c84f39f1cd548531a
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2011-09-05 10:31:14 +0000

    Removed a redundant line that looks like it was a left-over from J1.0.

--- jomres/libraries/jomres/cms_specific/joomla15/cms_specific_functions.php
+++ jomres/libraries/jomres/cms_specific/joomla15/cms_specific_functions.php
@@ -138,8 +138,6 @@ function jomres_cmsspecific_getRegistrationURL()
 	
 function jomres_cmsspecific_getTextEditor($name, $content, $hiddenField, $width, $height, $col, $row)
 	{
-	global $mainframe, $_MAMBOTS;
-	include( JOMRESCONFIG_ABSOLUTE_PATH."/editor/editor.php");
 	$ret = "";
 	$editor =& JFactory::getEditor();
 	$ret =$editor->display($name, $content, $width, $height, $col, $row);
