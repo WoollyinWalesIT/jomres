ffa39c7ff7eab668b660c70f92e62106a5f9725d
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2011-10-11 11:27:39 +0000

    Further changes to the cms_specific_urls.php script for finding the itemid.

--- libraries/jomres/cms_specific/joomla17/cms_specific_urls.php
+++ libraries/jomres/cms_specific/joomla17/cms_specific_urls.php
@@ -9,34 +9,45 @@
 **/
 
 
+
+
+
 // ################################################################
+
 defined( '_JOMRES_INITCHECK' ) or die( 'Direct Access to this file is not allowed.' );
+
 // ################################################################
+
+
 
 $siteConfig = jomres_getSingleton('jomres_config_site_singleton');
 $jrConfig=$siteConfig->get();
-
 $scriptname=str_replace("/","",$_SERVER['PHP_SELF']);
 if (strstr($scriptname,'install_jomres.php'))
 	set_showtime('live_site',str_replace("/jomres","",get_showtime('live_site')));
 
 $ssllink	= str_replace("https://","http://",get_showtime('live_site'));
-
 define('JOMRES_ADMINISTRATORDIRECTORY',"administrator");
-
 
 	$query = "SELECT id"
 		. "\n FROM #__menu"
 		. "\n WHERE "
 		. "\n published = 1"
 		. "\n AND link LIKE 'index.php?option=com_jomres' LIMIT 1";
+
 	$itemQueryRes = doSelectSql($query);
 	if (count($itemQueryRes)>0)
 		{
-		foreach ($itemQueryRes as $i)
-			{
-			$jomresItemid = $i->id;
-			}
+		// get application
+		$app   = JFactory::getApplication();
+		// get menu
+		$menu   = $app->getMenu();
+		// get active menu id
+		$activeId = $menu->getActive()->id;
+		// get active menu
+		$active   = $menu->getActive();
+		// set jomresItemid
+		$jomresItemid = $active->id;
 		}
 	else
 		{if (isset($jrConfig['jomresItemid']))
@@ -45,8 +56,7 @@ define('JOMRES_ADMINISTRATORDIRECTORY',"administrator");
 			$jomresItemid = 0; //should only kick in on install
 		}
 
-
-	$jrConfig=$siteConfig->set_setting("jomresItemid",$jomresItemid);
+$jrConfig=$siteConfig->set_setting("jomresItemid",$jomresItemid);
 
 $index = "index.php";
 $tmpl="";
@@ -61,26 +71,18 @@ if ( ($jrConfig['isInIframe'] == (bool)"1" || strstr($scriptname,'index2.php') |
 	}
 else
 	define("JOMRES_WRAPPED",0);
-	
+
 if (isset($_GET['format']) )
 	{
 	if ($_GET['format'] == "raw")
 		define("JOMRES_WRAPPED",1);
 	}
 
-if ( isset($_REQUEST['is_wrapped']) )
-	{
-	if ( $_REQUEST['is_wrapped']=="1" )
-		$tmpl .= $tmpl."&is_wrapped=1";
-	}
-
 define("JOMRES_SITEPAGE_URL_NOHTML",get_showtime('live_site').'/'."index.php?option=com_jomres&tmpl=component&no_html=1&popup=1&Itemid=".$jomresItemid."");
 define("JOMRES_SITEPAGE_URL_ADMIN",get_showtime('live_site').'/'.JOMRES_ADMINISTRATORDIRECTORY."/index.php?option=com_jomres");
 define("JOMRES_SITEPAGE_URL_SSL",$ssllink."/index.php?option=com_jomres&Itemid=".$jomresItemid."");
-
 define("JOMRES_SITEPAGE_URL_AJAX",get_showtime('live_site').'/'."index.php?option=com_jomres&tmpl=component&jrajax=1&no_html=1&popup=1&Itemid=".$jomresItemid."");
 define("JOMRES_SITEPAGE_URL_AJAX_ADMIN",get_showtime('live_site').'/'.JOMRES_ADMINISTRATORDIRECTORY."/index.php?option=com_jomres&tmpl=component&jrajax=1&no_html=1&popup=1");
-
 define("JOMRES_SITEPAGE_URL_RAW",get_showtime('live_site')."/index.php?option=com_jomres&format=raw");
 define("JOMRES_SITEPAGE_URL_NOSEF",get_showtime('live_site')."/index.php?option=com_jomres");
 
@@ -94,3 +96,4 @@ if (class_exists('JFactory'))
 	}
 else
 	define("JOMRES_SITEPAGE_URL",get_showtime('live_site')."/".$index."?option=com_jomres&Itemid=".$jomresItemid.$tmpl);
+
