3106fb0fb2d152b5df571af02251f4a66222d473
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2011-10-14 11:24:01 +0000

    Added Joseph's newly contributed router.php for J1.7.

--- jomres/libraries/jomres/cms_specific/joomla17/installfiles/router.php
+++ jomres/libraries/jomres/cms_specific/joomla17/installfiles/router.php
@@ -9,14 +9,13 @@
 **/
 
 defined( '_JEXEC' ) or die( '' );
+jimport( 'joomla.filter.output' );
 
 function JomresBuildRoute(&$query)
 	{
 	static $items;
 	$route_query = $query;  // We need to work within this function with the $route_query variable, not $query. It seems to be that the assignation &$query on some servers means that once the property name query further down has been run, then Joomla's $query becomes whatever the sql query was. Don't know why, and I'm not going to dig around to find out. We'll work internally on $route_query, then set $query to $route_query at the end, that seems to fix it.
-	
-	if (!defined('_JOMRES_INITCHECK'))
-		define('_JOMRES_INITCHECK', 1 );
+	if (!defined('_JOMRES_INITCHECK')) define('_JOMRES_INITCHECK', 1 );
 	global $thisJRUser;
 	if (file_exists(JPATH_BASE.DS.'jomres'.DS.'core-plugins'.DS.'alternative_init'.DS.'alt_init.php'))
 		require_once(JPATH_BASE.DS.'jomres'.DS.'core-plugins'.DS.'alternative_init'.DS.'alt_init.php');
@@ -24,6 +23,7 @@ function JomresBuildRoute(&$query)
 		require_once(JPATH_BASE.DS.'jomres'.DS.'remote_plugins'.DS.'alternative_init'.DS.'alt_init.php');
 	else
 		require_once(JPATH_BASE.DS.'jomres'.DS.'integration.php');
+	
 	$jrConfig					=	getSiteSettings();
 	$segments = array();
 	
@@ -50,7 +50,7 @@ function JomresBuildRoute(&$query)
 		{
 		case 'viewproperty':
 			$segments[] = $route_query['task'];
-			$segments[] = jomres_decode($property_name);
+			$segments[] = JFilterOutput::stringURLSafe(jomres_decode($property_name));
 			$segments[] = $route_query['property_uid'];
 			if (isset($route_query['task']))
 					unset( $route_query['task'] );
@@ -59,7 +59,7 @@ function JomresBuildRoute(&$query)
 			break;
 		case 'dobooking':
 			$segments[] = "dobooking";
-			$segments[] = jomres_decode($property_name);
+			$segments[] = JFilterOutput::stringURLSafe(jomres_decode($property_name));
 			$segments[] = $route_query['selectedProperty'];
 			if (isset($route_query['task']))
 					unset( $route_query['task'] );
@@ -109,21 +109,21 @@ function JomresBuildRoute(&$query)
 		$segments[] = $jrConfig['sef_task_alias_search'];
 		if (isset($route_query['town']))
 			{
-			$segments[] = _JOMRES_COM_MR_VRCT_PROPERTY_HEADER_TOWN;
-			$segments[] = JomresFilterString($route_query['town']);
-			unset( $route_query['town'] );
+			$segments[] = JFilterOutput::stringURLSafe(_JOMRES_COM_MR_VRCT_PROPERTY_HEADER_TOWN);
+			$segments[] = JFilterOutput::stringURLSafe($route_query['town']);
+			unset($route_query['town']);
 			}
 		if (isset($route_query['region']))
 			{
-			$segments[] = _JOMRES_COM_MR_VRCT_PROPERTY_HEADER_REGION;
-			$segments[] = JomresFilterString($route_query['region']);
-			unset( $route_query['region'] );
+			$segments[] = JFilterOutput::stringURLSafe(_JOMRES_COM_MR_VRCT_PROPERTY_HEADER_REGION);
+			$segments[] = JFilterOutput::stringURLSafe($route_query['region']);
+			unset($route_query['region']);
 			}
 		if (isset($route_query['country']))
 			{
-			$segments[] = _JOMRES_COM_MR_VRCT_PROPERTY_HEADER_COUNTRY;
-			$segments[] = JomresFilterString($route_query['country']);
-			unset( $route_query['country'] );
+			$segments[] = JFilterOutput::stringURLSafe(_JOMRES_COM_MR_VRCT_PROPERTY_HEADER_COUNTRY);
+			$segments[] = JFilterOutput::stringURLSafe($route_query['country']);
+			unset($route_query['country']);
 			}
 		if (isset($route_query['send']))
 			unset( $route_query['send'] );
@@ -138,18 +138,26 @@ function JomresBuildRoute(&$query)
 // ******************************************************************************************************************************
 // ******************************************************************************************************************************
 	
-	
 function JomresParseRoute($segments)
 	{
+		if (!defined('_JOMRES_INITCHECK')) define('_JOMRES_INITCHECK', 1 );
+	global $thisJRUser;
+	if (file_exists(JPATH_BASE.DS.'jomres'.DS.'core-plugins'.DS.'alternative_init'.DS.'alt_init.php'))
+		require_once(JPATH_BASE.DS.'jomres'.DS.'core-plugins'.DS.'alternative_init'.DS.'alt_init.php');
+	elseif (file_exists(JPATH_BASE.DS.'jomres'.DS.'remote_plugins'.DS.'alternative_init'.DS.'alt_init.php') )
+		require_once(JPATH_BASE.DS.'jomres'.DS.'remote_plugins'.DS.'alternative_init'.DS.'alt_init.php');
+	else
+		require_once(JPATH_BASE.DS.'jomres'.DS.'integration.php');
 	$vars = array();
 	global $thisJRUser;
 	if (!defined('_JOMRES_INITCHECK'))
 		define('_JOMRES_INITCHECK', 1 );
 	require_once('jomres/integration.php');
-	$jrConfig					=	getSiteSettings();
+	$jrConfig =	getSiteSettings();
 	switch($segments[0])
 		{
 		case 'viewproperty':
+			
 			$vars['task'] = "viewproperty";
 			$vars['property_uid'] = $segments[2];
 			break;
@@ -163,6 +171,9 @@ function JomresParseRoute($segments)
 			$searchParam = strtolower($segments[1]);
 			$vars['send'] = "Search";
 			$vars['calledByModule'] = 'mod_jomsearch_m0';
+			if($searchParam==JFilterOutput::stringURLSafe(_JOMRES_COM_MR_VRCT_PROPERTY_HEADER_TOWN)) $searchParam= 'town';
+			if($searchParam==JFilterOutput::stringURLSafe(_JOMRES_COM_MR_VRCT_PROPERTY_HEADER_COUNTRY)) $searchParam= 'country';
+			if($searchParam==JFilterOutput::stringURLSafe(_JOMRES_COM_MR_VRCT_PROPERTY_HEADER_REGION)) $searchParam= 'region';
 			$vars[$searchParam] = $segments[2];
 			break;
 		case 'showTariffs':
