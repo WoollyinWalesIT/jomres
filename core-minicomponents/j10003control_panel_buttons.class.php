0e2683d61f4209fe8e778028c938ca844a3165e3
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2011-11-01 16:46:18 +0000

    menus modified to ensure that dashboard categories are moved to the beginning of the menus.

--- core-minicomponents/j10003control_panel_buttons.class.php
+++ core-minicomponents/j10003control_panel_buttons.class.php
@@ -47,6 +47,15 @@ class j10003control_panel_buttons {
 			}
 		ksort ($control_panel_buttons_categorised);
 
+		// We need to find the dashboard category in this array, extract it then put it at the beginning.
+		$dashboard_category = jr_gettext( "_JOMRES_CUSTOMCODE_MENUCATEGORIES_MAIN" , "main" ,false,false);
+		$dashboard_category_contents = $control_panel_buttons_categorised[$dashboard_category];
+		unset($control_panel_buttons_categorised[$dashboard_category]);
+		$new_arr = array($dashboard_category=>$dashboard_category_contents );
+		foreach ($control_panel_buttons_categorised as $key => $val)
+			$new_arr[$key]=$val;
+		$control_panel_buttons_categorised = $new_arr;
+		
 		$MiniComponents->triggerEvent('10004', array('control_panel_buttons_categorised' => $control_panel_buttons_categorised) );
 		}
 
