9e9e19ba747d4610d09e942a2cc73f49c1a23d89
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2011-11-03 09:21:52 +0000

    Modified switch active property so that the returned property names are buttons, makes them more obvious.

--- core-minicomponents/j06001ui_find_properties.class.php
+++ core-minicomponents/j06001ui_find_properties.class.php
@@ -46,7 +46,7 @@ class j06001ui_find_properties
 				else
 					$pn=$thisJRUser->authorisedPropertyDetails[$property->propertys_uid]['property_name'];
 				
-				$output .= '<a href="'.jomresURL(JOMRES_SITEPAGE_URL.'&thisProperty='.$property->propertys_uid).'">'.$pn.'</a><br/>' ;
+				$output .= '<div><a href="'.jomresURL(JOMRES_SITEPAGE_URL.'&thisProperty='.$property->propertys_uid).'" class="fg-button ui-state-default ui-corner-all">'.$pn.'</a/div>' ;
 				}
 			}
 		echo $output;
