d2fcd342a21a89eebdd08eecab3cb9633e13bcf2
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2011-11-04 16:36:17 +0000

    After testing, further improved the ajax list properties function.

--- core-minicomponents/j06000ajax_list_properties.class.php
+++ core-minicomponents/j06000ajax_list_properties.class.php
@@ -22,37 +22,17 @@ class j06000ajax_list_properties
 			$this->template_touchable=false; return;
 			}
 		
-		$number_of_results = 3;
 		$tmpBookingHandler =jomres_getSingleton('jomres_temp_booking_handler');
 		$last_id = (int)$_REQUEST['lastID'];
-		if (!isset($tmpBookingHandler->tmpsearch_data['ajax_list_properties_sets']) ) // We'll create some data sets now, that'll make it slightly quicker in subsequent calls
+		$original_search_results = $tmpBookingHandler->tmpsearch_data['ajax_list_search_results'];
+		$count = count($original_search_results);
+		for ($i=0;$i<=$count;$i++)
 			{
-			$siteConfig = jomres_getSingleton('jomres_config_site_singleton');
-			$jrConfig=$siteConfig->get();
-
-			$original_search_results = $tmpBookingHandler->tmpsearch_data['ajax_list_search_results'];
-			$counter = 0;
-
-			array_splice($original_search_results, 0,(int)$jrConfig['property_list_limit']);
-
-			$count = count($original_search_results);
-			$tmp_sets = array_chunk($original_search_results,$number_of_results); // this is a new function for me. Even after all this time, there's always a first.
-
-			$counter = 0;
-			$sets = array();
-			foreach ($tmp_sets as $set)
+			if ($original_search_results[$i] == $last_id)
 				{
-				if ($counter == 0)
-					$newkey = $last_id;
-				else
-					$newkey = $last_key;
-
-				$sets[$newkey] = $set;
-				$last_key = end($set);
-				$counter++;
-
+				$this->resultBucket = array( $original_search_results[$i+1]);
+				break;
 				}
-			$tmpBookingHandler->tmpsearch_data['ajax_list_properties_sets'] = $sets;
 			}
 
 		$this->resultBucket=$tmpBookingHandler->tmpsearch_data['ajax_list_properties_sets'][$last_id];
