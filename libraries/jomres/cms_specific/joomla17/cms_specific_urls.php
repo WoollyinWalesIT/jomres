5d540f2e68f6ba7a43eb12062fee359dac0c2c2f
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2011-12-13 16:29:44 +0000

    Added a new flag to disable the top template ( "topoff=1" )

--- libraries/jomres/cms_specific/joomla17/cms_specific_urls.php
+++ libraries/jomres/cms_specific/joomla17/cms_specific_urls.php
@@ -99,6 +99,20 @@ if ( isset($_REQUEST['menuoff']) )
 		set_showtime('menuoff',false);
 		}
 	}
+
+if ( isset($_REQUEST['topoff']) )
+	{
+	if ( $_REQUEST['topoff']=="1" )
+		{
+		$tmpl .= $tmpl."&topoff=1";
+		set_showtime('topoff',true);
+		}
+	else
+		{
+		$tmpl .= $tmpl."&topoff=0";
+		set_showtime('topoff',false);
+		}
+	}
 	
 define("JOMRES_SITEPAGE_URL_NOHTML",get_showtime('live_site').'/'."index.php?option=com_jomres&tmpl=component&no_html=1&popup=1&Itemid=".$jomresItemid."");
 define("JOMRES_SITEPAGE_URL_ADMIN",get_showtime('live_site').'/'.JOMRES_ADMINISTRATORDIRECTORY."/index.php?option=com_jomres".$tmpl);
