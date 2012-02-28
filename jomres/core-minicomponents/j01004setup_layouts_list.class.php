94ee9fe3bc44d74e7a20451c21b39bd2565c377e
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2012-02-28 12:27:10 +0000

    Added the foundations of new functionality that allows different layouts in the property list.

---
+++ jomres/core-minicomponents/j01004setup_layouts_list.class.php
@@ -0,0 +1,51 @@
+<?php
+/**
+* Core file
+* @author Vince Wooll <sales@jomres.net>
+* @version Jomres 6
+* @package Jomres
+* @copyright	2005-2012 Vince Wooll
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly, however all images, css and javascript which are copyright Vince Wooll are not GPL licensed and are not freely distributable. 
+**/
+
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+
+class j01004setup_layouts_list
+	{
+	function j01004setup_layouts_list($componentArgs)
+		{
+		// Must be in all minicomponents. Minicomponents with templates that can contain editable text should run $this->template_touch() else just return
+		$MiniComponents =jomres_getSingleton('mcHandler');
+		if ($MiniComponents->template_touch)
+			{
+			$this->template_touchable=true; return;
+			}
+		$property_list_layouts = get_showtime('property_list_layouts');
+		if (is_null($property_list_layouts))
+			$property_list_layouts = array();
+		$property_list_layouts["list"] = array ("layout"=>"list_properties.html","title"=>jr_gettext("_JOMRES_PROPERTYLIST_LAYOUT_LIST",_JOMRES_PROPERTYLIST_LAYOUT_LIST,false,false),"path"=>null);
+		set_showtime('property_list_layouts',$property_list_layouts);
+		}
+
+	function touch_template_language()
+		{
+		$output=array();
+		$output[]		=jr_gettext('_JOMRES_PROPERTYLIST_LAYOUT_LIST', _JOMRES_PROPERTYLIST_LAYOUT_LIST);
+
+		foreach ($output as $o)
+			{
+			echo $o;
+			echo "<br/>";
+			}
+		}
+	// This must be included in every Event/Mini-component
+	function getRetVals()
+		{
+		return $this->propertys_uids;
+		}
+	}
+?>
---
+++ jomres/core-minicomponents/j01004setup_layouts_tiles.class.php
@@ -0,0 +1,50 @@
+<?php
+/**
+* Core file
+* @author Vince Wooll <sales@jomres.net>
+* @version Jomres 6
+* @package Jomres
+* @copyright	2005-2012 Vince Wooll
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly, however all images, css and javascript which are copyright Vince Wooll are not GPL licensed and are not freely distributable. 
+**/
+
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+class j01004setup_layouts_tiles
+	{
+	function j01004setup_layouts_tiles($componentArgs)
+		{
+		// Must be in all minicomponents. Minicomponents with templates that can contain editable text should run $this->template_touch() else just return
+		$MiniComponents =jomres_getSingleton('mcHandler');
+		if ($MiniComponents->template_touch)
+			{
+			$this->template_touchable=true; return;
+			}
+		$property_list_layouts = get_showtime('property_list_layouts');
+		if (is_null($property_list_layouts))
+			$property_list_layouts = array();
+		$property_list_layouts["tile"] = array ("layout"=>"list_properties_photos_layout.html","title"=>jr_gettext("_JOMRES_PROPERTYLIST_LAYOUT_TILE",_JOMRES_PROPERTYLIST_LAYOUT_TILE,false,false),"path"=>null);
+		set_showtime('property_list_layouts',$property_list_layouts);
+		}
+
+	function touch_template_language()
+		{
+		$output=array();
+		$output[]		=jr_gettext('_JOMRES_PROPERTYLIST_LAYOUT_TILE', _JOMRES_PROPERTYLIST_LAYOUT_TILE);
+
+		foreach ($output as $o)
+			{
+			echo $o;
+			echo "<br/>";
+			}
+		}
+	// This must be included in every Event/Mini-component
+	function getRetVals()
+		{
+		return $this->propertys_uids;
+		}
+	}
+?>
