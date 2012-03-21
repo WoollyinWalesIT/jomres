66a48e7733dc7696e8ffcd17ec1ebf3dbe445c81
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2012-03-21 14:44:11 +0000

    Added a routine that'll adapt to silly increment levels set in search by price ranges, when highest prices are too high, causing out of memory errors if left as is.

--- jomres/libraries/jomres/classes/jomSearch.class.php
+++ jomres/libraries/jomres/classes/jomSearch.class.php
@@ -1131,6 +1131,11 @@ function prepPriceRangeSearch($increments=10)
 	$count=count($allTariffs)-1;
 	$highest = $allTariffs[$count];
 	
+	// Found during testing, when one property has the price 100,000,000 and the increments is left to the default 20, you'll get an out of memory error.
+	// This is because you'll have up to half a million possible ranges. Here we'll test highest/increments. If the result is > 100 we'll have to set the increments to something a bit more sensible to stave off out of memory errors.
+	if ($highest/$increments > 10000)
+		$increments = $increments * 1000;
+
 	$ranges=my_range(0,$highest,$increments);
 	foreach ($ranges as $range)
 		{
