799095446b2cba1189989c42b98b2a990c1ad829
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2012-03-21 17:10:49 +0000

    Made the reviews popup the same as before 800/500

--- jomres/javascript/list_properties.js
+++ jomres/javascript/list_properties.js
@@ -3,8 +3,8 @@ function toggle_review_div(uid,property_name)
 	div_id = "#property_reviews"+uid;
 	jomresJquery( div_id ).dialog({
 		resizable: false,
-		height: 490,
-		width : 520,
+		height: 800,
+		width : 500,
 		modal : true,
 		resizable: true,
 		open: function() 
