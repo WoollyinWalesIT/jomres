4db3d40f7dfe079f2fdd2827d7f8cb88b3264fd2
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2012-04-16 09:17:12 +0000

    Added a new javascript function that allows us to parse js returned by ajax functionality to find and trigger any javascript in the returned data. Credit : http://www.developersnippets.com/2009/05/20/evaluate-scripts-while-working-on-ajax-requests/

--- javascript/jomres.js
+++ javascript/jomres.js
@@ -5,6 +5,37 @@ var jomresJquery = jQuery.noConflict();
 
 if (navigator.appName == 'Microsoft Internet Explorer') window.onerror=Block_Error;function Block_Error(){return true;};
 
+/* Credit : http://www.developersnippets.com/2009/05/20/evaluate-scripts-while-working-on-ajax-requests/ */
+function parse_ajax_returned_scripts(_source) {
+		var source = _source;
+		var scripts = new Array();
+ 
+		// Strip out tags
+		while(source.indexOf("<script") > -1 || source.indexOf("</script") > -1) {
+			var s = source.indexOf("<script");
+			var s_e = source.indexOf(">", s);
+			var e = source.indexOf("</script", s);
+			var e_e = source.indexOf(">", e);
+ 
+			// Add to scripts array
+			scripts.push(source.substring(s_e+1, e));
+			// Strip from source
+			source = source.substring(0, s) + source.substring(e_e+1);
+		}
+ 
+		// Loop through every script collected and eval it
+		for(var i=0; i<scripts.length; i++) {
+			try {
+				eval(scripts[i]);
+			}
+			catch(ex) {
+				// do what you want here when a script fails
+			}
+		}
+		// Return the cleaned source
+		return source;
+}
+
 
 function module_popup(random_identifier,property_uid){
 	jomresJquery('#module_'+random_identifier+'_popup').dialog({
