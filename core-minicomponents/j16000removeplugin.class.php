e0097378ee17b7379b47c88dee7d99f29231f7e7
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2012-04-18 12:36:38 +0000

    plugin manager updated to use anchors to make it a little more user friendly.

--- core-minicomponents/j16000removeplugin.class.php
+++ core-minicomponents/j16000removeplugin.class.php
@@ -34,7 +34,7 @@ class j16000removeplugin
 		$registry = new minicomponent_registry(false);
 		$registry->regenerate_registry();
 		
-		if (!$debugging) jomresRedirect(JOMRES_SITEPAGE_URL_ADMIN."&task=showplugins");
+		if (!$debugging) jomresRedirect(JOMRES_SITEPAGE_URL_ADMIN."&task=showplugins#".$pluginName);
 		}
 
 
