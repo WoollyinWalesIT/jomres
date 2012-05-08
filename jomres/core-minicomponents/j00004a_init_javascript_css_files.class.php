1b8e096739fe4b3b5b83aee15ee0bca0afbf9ac5
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2012-05-08 13:07:28 +0000

    Changed how we decide if the module popup js should be output.

--- jomres/core-minicomponents/j00004a_init_javascript_css_files.class.php
+++ jomres/core-minicomponents/j00004a_init_javascript_css_files.class.php
@@ -38,7 +38,7 @@ class j00004a_init_javascript_css_files {
 		if (defined("JOMRES_JSCALLED") )
 			return;
 		
-		if (!defined(JOMRES_NOHTML) )
+		if ( $_REQUEST['no_html']!="1")
 			echo '<script type="text/javascript">var module_pop_ajax_url = "'.JOMRES_SITEPAGE_URL_AJAX.'&task=module_popup&nofollowtmpl=1&id="</script>';
 		
 		$siteConfig = jomres_singleton_abstract::getInstance('jomres_config_site_singleton');
