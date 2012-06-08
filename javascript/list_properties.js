13a5a0d1739dbeb06c34ab06bf4810252e4a6a28
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2012-06-08 11:40:12 +0000

    Added loading animation to bottom of live scrolling feature so that user can tell if a new property is being loaded.

--- javascript/list_properties.js
+++ javascript/list_properties.js
@@ -22,13 +22,7 @@ function toggle_review_div(uid,property_name)
 		jomresJquery(div_id).dialog("close");
 	});
 	};
-	
-	
 
-
-
-	
-	
 function shortlist(property_uid){
 	jomresJquery.get(live_site_ajax+"&task=ajax_shortlist&property_uid="+property_uid, function(data) {
 		jomresJquery('#shortlist_'+property_uid).html(data);
@@ -41,11 +35,15 @@ function lastAddedLiveFunc()
 	{
 	id = jomresJquery(".jomres_property_list_propertywrapper:last").attr("id");
 	if (id != last_scrolled_id){
+		var animation='<div id="animation"><img src="jomres/images/ajax_animation/broken_circle.gif" /></div>';
+		jomresJquery(".jomres_property_list_propertywrapper:last").after(animation);
 		jomresJquery.get(live_site_ajax+"&task=ajax_list_properties&nofollowtmpl&lastID="+id,
 			function(data){
-				if (data != "") {
+			jomresJquery("#animation").remove();
+			if (data != "") {
 				jomresJquery(".jomres_property_list_propertywrapper:last").after(data);
 				}
+			
 			killScroll = false; // IMPORTANT - Make function available again.
 			var bol = jomresJquery("input[type=checkbox][name=compare]:checked").length >= 3;
 			jomresJquery("input[type=checkbox][name=compare]").not(":checked").attr("disabled",bol);
