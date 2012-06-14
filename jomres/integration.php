06d5a1d67559aa012903a493106c9b8afbf293e4
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2012-06-14 09:29:06 +0000

    Added spaces around new input-medium classes.

--- jomres/integration.php
+++ jomres/integration.php
@@ -1012,7 +1012,7 @@ class jomresHTML
 	function selectList( $arr, $name, $attribs, $key, $text, $default=NULL )
 		{
 		$attribs = str_replace('class="inputbox"',"",$attribs);
-		$attribs .= 'class="input-medium"';
+		$attribs .= ' class="input-medium" ';
 		$output = '<select name="'.$name.'" id="'.$name.'" '.$attribs.'>';
 		for ($i=0, $n= count( $arr ); $i < $n; $i++ )
 			{
@@ -1029,7 +1029,7 @@ class jomresHTML
 
 	function integerSelectList( $start, $end, $increment, $name, $attribs, $selected, $format="" )
 		{
-		$attribs = str_replace('class="inputbox"',"",$attribs);
+		$attribs = str_replace(' class="inputbox" ',"",$attribs);
 		$attribs .= 'class="input-medium"';
 		$arry 	= array();
 		for ($i=(int)$start; $i <= (int)$end; $i+=(int)$increment)
