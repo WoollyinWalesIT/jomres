81920093f9c3adc8d838fa40d6389430dbec1f21
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2012-06-15 16:34:26 +0000

    Shopping cart modified to show prices converted to the global currency code. Without this change, if two bookings are saved to the cart, if they're using different currencies then the wrong prices will be shown on the cart.

--- libraries/jomres/classes/jomres_cart.class.php
+++ libraries/jomres/classes/jomres_cart.class.php
@@ -20,32 +20,63 @@ class jomres_cart
 		$tmpBookingHandler =jomres_singleton_abstract::getInstance('jomres_temp_booking_handler');
 		if (!isset($tmpBookingHandler->cart_data))
 			$tmpBookingHandler->cart_data = array();
+			
+		$siteConfig = jomres_singleton_abstract::getInstance('jomres_config_site_singleton');
+		$jrConfig=$siteConfig->get();
+		
+		$this->currency_code = $jrConfig['globalCurrencyCode'];
 		$this->contract_total = 0.00;
 		$this->deposit_required = 0.00;
+		$this->number_of_bookings = 0;
+		$this->items = array(); // Provides us with a simplified array with the individual bookings, and their currency converted total and deposit figures
+		
 		$this->calc_totals();
 		}
 	
 	function calc_totals()
 		{
+		$siteConfig = jomres_singleton_abstract::getInstance('jomres_config_site_singleton');
+		$jrConfig=$siteConfig->get();
+		$do_conversion = true;
+		if ($jrConfig['use_conversion_feature'] != "1")
+			$do_conversion = false;
+		
 		$tmpBookingHandler =jomres_singleton_abstract::getInstance('jomres_temp_booking_handler');
+		jr_import('jomres_currency_conversion');
+		$conversion = new jomres_currency_conversion();
 		if ( count($tmpBookingHandler->cart_data) > 0)
 			{
-			foreach ($tmpBookingHandler->cart_data as $data)
+			//var_dump($tmpBookingHandler->cart_data);exit;
+			foreach ($tmpBookingHandler->cart_data as $key=>$data)
 				{
-				$this->contract_total = $this->contract_total + (float)$data['contract_total'];
-				$this->deposit_required = $this->deposit_required + (float)$data['deposit_required'];
+				$contract_total = (float)$data['contract_total'];
+				$deposit_required = (float)$data['deposit_required'];
+				$currencycode=$data['property_currencycode'];
+				if($conversion->this_code_can_be_converted($currencycode) && $do_conversion)
+					{
+					$contract_total 		= $conversion->convert_sum($contract_total,$currencycode,$this->currency_code);
+					$deposit_required 		= $conversion->convert_sum($deposit_required,$currencycode,$this->currency_code);
+					}
+				$this->number_of_bookings++;
+				$this->items[$key]=array("total"=>$contract_total,"deposit"=>$deposit_required);
+				$this->contract_total 		= $this->contract_total + $contract_total;
+				$this->deposit_required 	= $this->deposit_required + $deposit_required;
 				}
 			}
 		}
-	
+
 	function move_from_temp_booking_to_cart_array()
 		{
 		$random_identifier = generateJomresRandomString(20);
 		$tmpBookingHandler =jomres_singleton_abstract::getInstance('jomres_temp_booking_handler');
+		set_booking_number();
 		if (!isset($tmpBookingHandler->cart_data))
 			$tmpBookingHandler->cart_data = array();
 		$tmpBookingHandler->cart_data[$random_identifier] = $tmpBookingHandler->tmpbooking;
+		$tmpBookingHandler->cart_data[$random_identifier]['tmpguest'] = $tmpBookingHandler->tmpguest;
+		
 		$tmpBookingHandler->resetTempBookingData();
+		$tmpBookingHandler->resetTempGuestData();
 		$tmpBookingHandler->saveAllData();
 		// Have to do this here, because the redirect will exit the script from running before we get to the end of jomres.php, and the latter "close session" functionality will not be triggered
 		$tmpBookingHandler->close_jomres_session();
@@ -67,14 +98,20 @@ class jomres_cart
 		
 	function build_booking_form_data_for_payment_gateways()
 		{
+		$siteConfig = jomres_singleton_abstract::getInstance('jomres_config_site_singleton');
+		$jrConfig=$siteConfig->get();
 		$this->calc_totals();
 		$tmpBookingHandler =jomres_singleton_abstract::getInstance('jomres_temp_booking_handler');
 		$tmpBookingHandler->resetTempBookingData();
 		$tmpBookingHandler->updateBookingField('contract_total',$this->contract_total);
 		$tmpBookingHandler->updateBookingField('deposit_required',$this->deposit_required);
 		$tmpBookingHandler->updateBookingField('cart_payment',true);
+		$tmpBookingHandler->updateBookingField('property_currencycode',$jrConfig['globalCurrencyCode']);
+		
 		$tmpBookingHandler->saveAllData();
 		$tmpBookingHandler->close_jomres_session();
+		
+		
 		return array ("contract_total"=>$this->contract_total,"deposit_required"=>$this->deposit_required);
 		}
 	}
