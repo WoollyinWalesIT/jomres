3553111e4c7bc47d053d61ecf3472d6fe9c3804e
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2012-07-23 09:59:46 +0000

    Various tweaks to allow modules to install and work in Joomla 3.0

--- core-minicomponents/j16000addplugin.class.php
+++ core-minicomponents/j16000addplugin.class.php
@@ -297,7 +297,7 @@ class j16000addplugin
 					{
 					if ($plugin_class->data['type'] == "mambot" || $plugin_class->data['type'] == "module")
 						{
-						if (_JOMRES_DETECTED_CMS == "joomla16" || _JOMRES_DETECTED_CMS == "joomla17" || _JOMRES_DETECTED_CMS == "joomla25")
+						if (_JOMRES_DETECTED_CMS == "joomla16" || _JOMRES_DETECTED_CMS == "joomla17" || _JOMRES_DETECTED_CMS == "joomla25" || _JOMRES_DETECTED_CMS == "joomla30")
 							{
 							if (!$debugging) jomresRedirect(get_showtime("live_site")."/".JOMRES_ADMINISTRATORDIRECTORY."/index.php?option=com_installer&view=discover");
 							}
