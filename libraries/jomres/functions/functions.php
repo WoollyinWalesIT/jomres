39960c7556d8255965e6133b9316ac0f1272ca11
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2012-08-14 09:40:47 +0000

    Fixed a bug where sometimes prices weren't being converted in the property list.

--- libraries/jomres/functions/functions.php
+++ libraries/jomres/functions/functions.php
@@ -163,9 +163,9 @@ function get_property_price_for_display_in_lists($property_uid)
 			if (isset($pricesFromArray[$property_uid]))
 				{
 				if ($mrConfig['prices_inclusive']=="0")
-					$price=output_price ($current_property_details->get_gross_accommodation_price($pricesFromArray[$property_uid],$property_uid) * $multiplier,true,true);
+					$price=output_price ($current_property_details->get_gross_accommodation_price($pricesFromArray[$property_uid],$property_uid) * $multiplier,"",true,true);
 				else
-					$price=output_price ($pricesFromArray[$property_uid] * $multiplier,true,true);
+					$price=output_price ($pricesFromArray[$property_uid] * $multiplier,"",true,true);
 					
 				if ($mrConfig['tariffChargesStoredWeeklyYesNo'] == "1" && $mrConfig['tariffmode'] == "1")
 					$post_text = "&nbsp;".jr_gettext('_JOMRES_COM_MR_LISTTARIFF_ROOMRATEPERWEEK',_JOMRES_COM_MR_LISTTARIFF_ROOMRATEPERWEEK);
@@ -208,13 +208,13 @@ function get_property_price_for_display_in_lists($property_uid)
 			else
 				{
 				$pre_text =jr_gettext('_JOMRES_COM_MR_EXTRA_PRICE',_JOMRES_COM_MR_EXTRA_PRICE);
-				$price = output_price($current_property_details->real_estate_property_price,true,false);
+				$price = output_price($current_property_details->real_estate_property_price,"",true,false);
 				$post_text = '';
 				}
 			}
 		else
 			{
-			$pre_text = jr_gettext('_JOMRES_COM_MR_EXTRA_PRICE',_JOMRES_COM_MR_EXTRA_PRICE,true,false);
+			$pre_text = jr_gettext('_JOMRES_COM_MR_EXTRA_PRICE',_JOMRES_COM_MR_EXTRA_PRICE,"",true,false);
 			$price=output_price($current_property_details->real_estate_property_price);
 			$post_text = '';
 			}
