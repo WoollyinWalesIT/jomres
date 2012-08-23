c82377d31d7ab5c158114908b3f19d5d4ed200e1
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2012-08-23 18:16:34 +0000

    Added jquery easing and a new slide and toggle jquery function.

--- jomres/javascript/jomres.js
+++ jomres/javascript/jomres.js
@@ -5,6 +5,14 @@ var jomresJquery = jQuery.noConflict();
 
 if (navigator.appName == 'Microsoft Internet Explorer') window.onerror=Block_Error;function Block_Error(){return true;};
 
+jomresJquery.fn.fadeThenSlideToggle = function(speed, easing, callback) {
+  if (this.is(":hidden")) {
+    return this.slideDown(speed, easing).fadeTo(speed, 1, easing, callback);
+  } else {
+    return this.fadeTo(speed, 0, easing).slideUp(speed, easing, callback);
+  }
+};
+
 function toggle_button_class(id)
 	{
 	var isActive = jomresJquery(id).hasClass('active');
