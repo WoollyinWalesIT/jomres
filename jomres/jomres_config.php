028726dd8b423fc93c577611e2b26e1a851fe31d
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2012-08-29 09:44:27 +0000

    Fixed an incorrect version number.

--- jomres/jomres_config.php
+++ jomres/jomres_config.php
@@ -15,7 +15,7 @@ defined( '_JOMRES_INITCHECK' ) or die( '' );
 // ################################################################
 
 
-$mrConfig['version']='7.0.-2';
+$mrConfig['version']='7.-1.0';
 
 $mrConfig['newTariffModels'] = "1";
 $mrConfig['singlePersonSuppliment']='0';
