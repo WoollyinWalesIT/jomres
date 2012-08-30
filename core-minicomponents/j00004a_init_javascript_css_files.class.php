7c722c46efeb0979f54245c7c6a678814e22a632
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2012-08-30 13:53:46 +0000

    Enabled jquery regardless of the bootstrap setting.

--- core-minicomponents/j00004a_init_javascript_css_files.class.php
+++ core-minicomponents/j00004a_init_javascript_css_files.class.php
@@ -75,11 +75,11 @@ class j00004a_init_javascript_css_files {
 		
 		$management_view=jomresGetParam($_REQUEST,'tmpl',false);
 		
-		if (!using_bootstrap() || $management_view )
-			{
+		//if (!using_bootstrap() || $management_view )
+		//	{
 			set_showtime("jquery.core.js",'jquery-1.7.1.min.js');
 			set_showtime("jquery.core.js.relpath",'jomres/javascript/');
-			}
+		//	}
 		set_showtime("jquery.ui.js","jquery-ui-1.8.16.custom.min.js");
 		set_showtime("jquery.ui.js.relpath",'jomres/javascript/');
 
