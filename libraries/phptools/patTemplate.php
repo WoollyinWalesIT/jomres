b33ec310814b4db25768b6850d511a15b89a8e05
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2012-09-19 12:33:11 +0000

    Undid the previous change, whilst a good idea, it strips out most of the system's javascript (seeing as it's all in comments for page validation reasons). Added a new common definition COMMON_THIS_PAGE_URL and accompanying function, and set the short modifier filter on by default.

--- libraries/phptools/patTemplate.php
+++ libraries/phptools/patTemplate.php
@@ -242,7 +242,7 @@ class patTemplate
 		}
 
 		$this->setType( $type );
-		$this->applyInputFilter('StripComments');
+		$this->applyInputFilter('ShortModifiers');
 	}
 
    /**
@@ -858,8 +858,26 @@ class patTemplate
 		
 		$backtrace = debug_backtrace();
 		$common_strings['COMMON_SOURCE_SCRIPT']=$backtrace[1]['file'];
+
+		$common_strings['COMMON_THIS_PAGE_URL']=$this->curPageURL();
 		
 		return $common_strings;
+		}
+		
+	function curPageURL() 
+		{
+		 $pageURL = 'http';
+		if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
+		$pageURL .= "://";
+		if ($_SERVER["SERVER_PORT"] != "80") 
+			{
+			$pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+			}
+		else
+			{
+			$pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+			}
+		return $pageURL;
 		}
    /**
 	* Adds an object to a template
