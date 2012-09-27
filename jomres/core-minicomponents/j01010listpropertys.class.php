12624d2a8c4e6696940db2cd5873d93ec7ae5a81
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2012-09-27 15:06:24 +0000

    Fixed an issue where live scrolling was only scrolling once, due to a bug introduced in a v6.6.6 change.

--- jomres/core-minicomponents/j01010listpropertys.class.php
+++ jomres/core-minicomponents/j01010listpropertys.class.php
@@ -107,7 +107,7 @@ class j01010listpropertys {
 			$propertys_uids=$tmpArray;
 			}
 		
-		if (JOMRES_NOHTML != 1 || get_showtime('task') == "ajax_search_filter")
+		if ( !AJAXCALL || get_showtime('task') == "ajax_search_filter")
 			{
 			$propertys_uids=$MiniComponents->triggerEvent('01009',array('propertys_uids'=>$propertys_uids) ); // Pre list properties parser. Allows us to to filter property lists if required
 			$tmpBookingHandler->tmpsearch_data['ajax_list_search_results'] = $propertys_uids;
