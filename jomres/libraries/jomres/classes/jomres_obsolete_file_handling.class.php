3d8b0094792c2edf52c8a3ba8d965d36bfa5576f
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2012-10-02 14:18:14 +0000

    Added j00501editingmode.class.php to the obsolete file list.

--- jomres/libraries/jomres/classes/jomres_obsolete_file_handling.class.php
+++ jomres/libraries/jomres/classes/jomres_obsolete_file_handling.class.php
@@ -688,6 +688,8 @@ class jomres_obsolete_file_handling
 		
 		$this->add_obs_file($this->dir_javascript.'jquery-ui-1.8.13.custom.js');
 		$this->add_obs_file($this->dir_javascript.'jquery-ui-1.8.16.custom.min.js');
+		
+		$this->add_obs_file($this->dir_minicomponents.'j00501editingmode.class.php');
 		}
 	
 	function add_obs_file($path_and_file)
