9b2f81597202da443b8fb759c3b86b782f01f2fa
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2012-10-11 10:21:58 +0000

    Occasionally it's normal for the session file name to have not been created, added a check to temp booking handler to take that into account.

--- libraries/jomres/classes/jomres_temp_booking_handler.class.php
+++ libraries/jomres/classes/jomres_temp_booking_handler.class.php
@@ -268,7 +268,8 @@ class jomres_temp_booking_handler
 	function close_jomres_session()
 		{
 		$data=array('IP'=>$_SERVER,'tmpbooking'=>$this->tmpbooking,'cart_data'=>$this->cart_data,'tmpguest'=>$this->tmpguest,'tmpsearch_data'=>$this->tmpsearch_data,'tmplang'=>$this->tmplang,'user_settings'=>$this->user_settings);
-		file_put_contents($this->sessionfile, serialize($data));
+		if (!is_null($this->sessionfile)) // In certain circumstances the session file might not have been created (typically administrator area functionality)
+			file_put_contents($this->sessionfile, serialize($data));
 		}
 
 	function _remove_old_jomres_sessions()
