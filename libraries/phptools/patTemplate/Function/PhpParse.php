36403c94c0e26925b17a799b3a4883c2c8706bb5
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2012-10-19 12:28:17 +0000

    Adding an experimental php parser function to patTemplate lib.

---
+++ libraries/phptools/patTemplate/Function/PhpParse.php
@@ -0,0 +1,41 @@
+<?PHP
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+/**
+ * patTemplate function that parses PHP code in your templates. Custom Jomres patTemplate function
+ *
+ * $Id: PhpParse.php 
+ *
+ * @package		patTemplate
+ * @subpackage	Functions
+ * @author		Vince Wooll
+ */
+
+class patTemplate_Function_PhpParse extends patTemplate_Function
+{
+   /**
+	* name of the function
+	* @access	private
+	* @var		string
+	*/
+	var $_name	=	'PhpParse';
+
+   /**
+	* call the function
+	*
+	* @access	public
+	* @param	array	parameters of the function (= attributes of the tag)
+	* @param	string	content of the tag
+	* @return	string	content to insert into the template
+	*/ 
+	function call( $params, $content )
+	{
+		ob_start();
+		@eval( $content );
+		$content = ob_get_contents();
+		ob_end_clean();
+		return $content;
+	}
+}
+?>
