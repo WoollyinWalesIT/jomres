97fd2cc553bb978a9768f7a20adccdff5f2be6aa
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2012-10-30 10:54:07 +0000

    Added an mb_ucwords and jr_ucwords pair of functions.

--- libraries/jomres/functions/multibye_functions.php
+++ libraries/jomres/functions/multibye_functions.php
@@ -30,4 +30,18 @@ function jr_strtolower($str)
 		$result = mb_strtolower($str);
 	return $result;
 	}
+	
+function jr_ucwords($str)
+	{
+	return mb_ucwords($str);
+	}
+	
+if(!function_exists('mb_ucwords'))
+	{
+	function mb_ucwords($str)
+		{
+		return mb_convert_case($str, MB_CASE_TITLE, "UTF-8");
+		}
+	}
+	
 ?>
