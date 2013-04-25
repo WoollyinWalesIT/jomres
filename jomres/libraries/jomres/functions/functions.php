b99311a1b0db951ecf783cc0c56401a2104fe23a
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2013-04-25 11:20:21 +0000

    Fixed a typo introduced in the last changeset.

--- jomres/libraries/jomres/functions/functions.php
+++ jomres/libraries/jomres/functions/functions.php
@@ -13,7 +13,7 @@
 defined( '_JOMRES_INITCHECK' ) or die( '' );
 // ################################################################
 
-genericLike($idArray,$fieldToSearch,$idArrayisInteger=true)
+function genericLike($idArray,$fieldToSearch,$idArrayisInteger=true)
 	{
 	$newArr=array();
 	foreach ($idArray as $id)
