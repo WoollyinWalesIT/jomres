69f246faed12ac1098ec4d73aa135a2b098da28d
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2013-07-01 09:08:44 +0000

    Added a new minicomponent that'll return the region dropdown, intended for use via ajax.

---
+++ core-minicomponents/j06000get_region_dropdown_for_country_code.class.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Core file
+ *
+ * @author Vince Wooll <sales@jomres.net>
+ * @version Jomres 7
+ * @package Jomres
+ * @copyright    2005-2013 Vince Wooll
+ * Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly, however all images, css and javascript which are copyright Vince Wooll are not GPL licensed and are not freely distributable.
+ **/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+class j06000get_region_dropdown_for_country_code
+	{
+	function j06000get_region_dropdown_for_country_code()
+		{
+		$MiniComponents = jomres_singleton_abstract::getInstance( 'mcHandler' );
+		if ( $MiniComponents->template_touch )
+			{
+			$this->template_touchable = false;
+			return;
+			}
+		$countryCode = jomresGetParam( $_REQUEST, 'country', "" );
+		$currentRegion = jomresGetParam( $_REQUEST, 'region', "" );
+		$dropdown = setupRegions( $countryCode, $currentRegion );
+		echo $dropdown;
+		}
+
+	// This must be included in every Event/Mini-component
+	function getRetVals()
+		{
+		return null;
+		}
+	}
+
+?>
