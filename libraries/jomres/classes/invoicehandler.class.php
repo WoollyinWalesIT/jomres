b11b44987da677c7d58d30982838ddef308772d0
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2013-07-16 08:41:42 +0000

    Improved the invoice handler so that updated invoices are correctly defined as vat exempt or not.

--- libraries/jomres/classes/invoicehandler.class.php
+++ libraries/jomres/classes/invoicehandler.class.php
@@ -171,22 +171,22 @@ class invoicehandler extends jrportal_invoice
 		$line_item->recur_discount = $line_item_data[ 'recur_discount' ];
 
 		$i_total                         = ( (float) $line_item->init_price * (int) $line_item->init_qty ) - (float) $line_item->init_discount;
+		$r_total                = ( (float) $line_item->recur_price * (int) $line_item->recur_qty ) - (float) $line_item->recur_discount;
+		
 		$line_item->init_total           = $i_total;
 		if ($this->vat_will_be_charged)
 			{
 			$init_toal_tax                   = number_format( $i_total / 100 * $line_item->tax_rate, 2, '.', '' );
+			$recur_toal_tax         = number_format( ( $r_total / 100 ) * $line_item->tax_rate, 2, '.', '' );
 			}
 		else
 			{
 			$init_toal_tax                   = 0;
+			$recur_toal_tax                   = 0;
 			}
 
 		$line_item->init_total_inclusive = $i_total + $init_toal_tax;
-
 		$this->init_total = $this->init_total + $line_item->init_total_inclusive;
-
-		$r_total                = ( (float) $line_item->recur_price * (int) $line_item->recur_qty ) - (float) $line_item->recur_discount;
-		$recur_toal_tax         = number_format( ( $r_total / 100 ) * $line_item->tax_rate, 2, '.', '' );
 		$line_item->recur_total = $r_total + $recur_toal_tax;
 		$this->recur_total      = number_format( $line_item->recur_total, 2, '.', '' );
 
