7bb759c37af0b65a985e9360b8ba3c06fc3e6a72
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2013-07-19 10:36:07 +0000

    Added some changes that (again) prevent double clicking when making payments to prevent the blank emails that some people get. Added some changes that correct errors created by phpstorm's formatting of template, where {XXX} was spread over multiple lines.

--- jomres/jomres_config.php
+++ jomres/jomres_config.php
@@ -16,7 +16,7 @@ defined( '_JOMRES_INITCHECK' ) or die( '' );
 // ################################################################
 
 
-$mrConfig[ 'version' ] = '7.2.19';
+$mrConfig[ 'version' ] = '7.3.0';
 
 $mrConfig[ 'newTariffModels' ]                 = "2";
 $mrConfig[ 'singlePersonSuppliment' ]          = '0';
