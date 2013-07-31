fe19850d4e0628c7868de3807131b3ce60d9adec
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2013-07-31 08:01:30 +0000

    Property name added to module popup.

--- core-minicomponents/j06000module_popup.class.php
+++ core-minicomponents/j06000module_popup.class.php
@@ -62,7 +62,8 @@ class j06000module_popup
 				{
 				$output[ 'STARSIMAGES' ] .= "<img src=\"" . get_showtime( 'live_site' ) . "/jomres/images/star.png\" alt=\"star\" border=\"0\" />";
 				}
-
+			
+			$output[ 'PROPERTY_NAME' ]                 = $current_property_details->property_name;
 			$output[ 'PROPERTY_STREET' ]               = $current_property_details->property_street;
 			$output[ 'PROPERTY_TOWN' ]                 = $current_property_details->property_town;
 			$output[ 'PROPERTY_POSTCODE' ]             = $current_property_details->property_postcode;
