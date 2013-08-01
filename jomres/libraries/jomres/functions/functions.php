8b4bb699468cfc1b76827da9799835197956bfab
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2013-08-01 09:54:32 +0000

    Modified the jomres_decode function (the output filter) as it was allowing users to inject javascript into certain pages.

--- jomres/libraries/jomres/functions/functions.php
+++ jomres/libraries/jomres/functions/functions.php
@@ -14,6 +14,52 @@
 defined( '_JOMRES_INITCHECK' ) or die( '' );
 // ################################################################
 
+function jomres_decode( $string )
+	{
+	$string = str_replace( "&#34;", '&ldquo;', $string );
+	$string = str_replace( "&#38;", "&", $string );
+	$string = str_replace( "&#39;", "&#96;", $string );
+	$string = str_replace( "&#60;x&#62;", "", $string ); // Older versions of Jomres used to add <x> to strings to make them safe. We don't need that any more, however if we don't add this line then some older installations may show <x> in, for example, the property details.
+	$string = str_replace( "&#60;", "<", $string );
+	$string = str_replace( "&#62;", ">", $string );
+	$string = str_replace( "&lt;", "<", $string );
+	$string = str_replace( "&gt;", ">", $string );
+
+	$string = str_replace( "\xe2\x80\xa8", '', $string ); // Strip out some naughty little ascii line seperators that are absolute devils to remove if you're not looking for them. They'll break your javascript.
+	return $string;
+	}
+
+
+/* 
+The above function should suffice for most circumstances, however we may find later that some users will want more freedom to enter data that shows up in output, later on. If that's the case we'll swap the above function to this one.
+*/
+
+/*
+function jomres_decode( $string )
+	{
+	$siteConfig = jomres_singleton_abstract::getInstance( 'jomres_config_site_singleton' );
+	$jrConfig   = $siteConfig->get();
+	if ( $jrConfig[ 'input_filtering' ] == "weak" )
+		{
+		$string = html_entity_decode( $string, ENT_QUOTES );
+		}
+	else
+		{
+		$string = str_replace( "&#34;", '&ldquo;', $string );
+		$string = str_replace( "&#38;", "&", $string );
+		$string = str_replace( "&#39;", "&#96;", $string );
+		$string = str_replace( "&#60;x&#62;", "", $string ); // Older versions of Jomres used to add <x> to strings to make them safe. We don't need that any more, however if we don't add this line then some older installations may show <x> in, for example, the property details.
+		$string = str_replace( "&#60;", "<", $string );
+		$string = str_replace( "&#62;", ">", $string );
+		$string = str_replace( "&lt;", "<", $string );
+		$string = str_replace( "&gt;", ">", $string );
+		}
+
+	$string = str_replace( "\xe2\x80\xa8", '', $string ); // Strip out some naughty little ascii line seperators that are absolute devils to remove if you're not looking for them. They'll break your javascript.
+	return $string;
+	}
+*/
+
 
 function echo_backtrace()
 	{
@@ -5161,22 +5207,7 @@ function this_cms_is_joomla()
 	return true;
 	}
 
-function jomres_decode( $string )
-	{
-	$string = htmlspecialchars_decode( $string, ENT_QUOTES );
-	$string = str_replace( "&#34;", '"', $string );
-	$string = str_replace( "&#38;", "&", $string );
-	$string = str_replace( "&#39;", "'", $string );
-	$string = str_replace( "&#60;x&#62;", "", $string );
-	$string = str_replace( "&#60;", "<", $string );
-	$string = str_replace( "&#62;", ">", $string );
-	$string = str_replace( "&lt;", "<", $string );
-	$string = str_replace( "&gt;", ">", $string );
 
-	$string = str_replace( "\xe2\x80\xa8", '', $string ); // Strip out some naughty little ascii line seperators that are absolute devils to remove if you're not looking for them. They'll break your javascript.
-	//$string = str_replace ("&#39;","'",$string);
-	return $string;
-	}
 
 
 //----------------------------------------
