b48b9ab72fea215d9d1b3a9a25597257e87b83ee
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2013-09-23 17:45:16 +0000

    Temp disabled some calls to javascript files that aren't currently being used.

--- core-minicomponents/j00004a_init_javascript_css_files.class.php
+++ core-minicomponents/j00004a_init_javascript_css_files.class.php
@@ -256,13 +256,13 @@ class j00004a_init_javascript_css_files
 			set_showtime( "tmpl.min.js.relpath", 					"jomres/javascript/media_centre/" );
 			}
 			
-		if (using_bootstrap())
+		/* if (using_bootstrap())
 			{
 			set_showtime( "bootstrap-tour.js", "bootstrap-tour.js" );
 			set_showtime( "bootstrap-tour.js.relpath", "jomres/javascript/" );
 			set_showtime( "jquery.cookie.js", "jquery.cookie.js" );
 			set_showtime( "jquery.cookie.js.relpath", "jomres/javascript/" );
-			}
+			} */
 		}
 
 	/**
