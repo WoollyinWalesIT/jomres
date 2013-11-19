dea4669bc745fef307b4866d822ac96b4c84d56e
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2013-11-19 12:07:48 +0000

    Finalise Jomres 7.5.4

--- jomres_config.php
+++ jomres_config.php
@@ -16,7 +16,7 @@ defined( '_JOMRES_INITCHECK' ) or die( '' );
 // ################################################################
 
 
-$mrConfig[ 'version' ] = '7.5.3';
+$mrConfig[ 'version' ] = '7.5.4';
 
 $mrConfig[ 'newTariffModels' ]                 = "2";
 $mrConfig[ 'singlePersonSuppliment' ]          = '0';
