0c056ad5f5191d24baceb51b4323f24000937e2a
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2013-11-21 17:27:10 +0000

    Removed a setting that no longer has any relevance.

--- jomres/core-minicomponents/j01025showtariffs.class.php
+++ jomres/core-minicomponents/j01025showtariffs.class.php
@@ -182,14 +182,6 @@ class j01025showtariffs
 						$r[ 'NOTWEEKENDS' ] = jr_gettext( '_JOMRES_FRONT_TARIFFS_NOTWEEKEND', _JOMRES_FRONT_TARIFFS_NOTWEEKEND );
 					$theRate = number_format( ( $price_inc_vat ), 2, '.', '' );
 
-					if ( $mrConfig[ 'showGoogleCurrencyLink' ] == "1" )
-						{
-						$currency = $mrConfig[ 'property_currencycode' ];
-						if ( $jrConfig[ 'useGlobalCurrency' ] == "1" ) $currency = $jrConfig[ 'globalCurrencyCode' ];
-						$theLink                    = 'http://www.xe.com/pca/input.cgi?From=' . $currency . '&Amount=' . $theRate;
-						$theText                    = jr_gettext( '_JOMRES_CURRENCYCONVERSIONTEXT', _JOMRES_CURRENCYCONVERSIONTEXT );
-						$r[ 'GOOGLECURRENCYLINKS' ] = '<a href=' . $theLink . ' rel="nofollow" target="_blank">' . $theText . '</a><br />';
-						}
 					if ( $tariff->roomrateperday > 0 )
 						{
 						if ( $counter % 2 ) $r[ 'CLASS' ] = "even";
