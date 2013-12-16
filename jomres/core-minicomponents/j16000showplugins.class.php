09d491f6e8d1bbc0363480fcd27915c1363f6946
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2013-12-16 11:58:35 +0000

    Added page blocking when Upgrade All Plugins is clicking in Plugin Manager. Provides feedback to users ensuring that they do not attempt to use the page until the plugin upgrade process has been done (essential on systems with slower internet connections).

--- jomres/core-minicomponents/j16000showplugins.class.php
+++ jomres/core-minicomponents/j16000showplugins.class.php
@@ -37,6 +37,7 @@ class j16000showplugins
 		$registry = new minicomponent_registry( true );
 		$registry->regenerate_registry();
 		jomres_cmsspecific_addheaddata( "javascript", 'jomres/javascript/', "shop.js", false );
+		jomres_cmsspecific_addheaddata( "javascript", 'jomres/javascript/', "jquery.blockUI.js", false );
 		if ( $MiniComponents->template_touch )
 			{
 			$this->template_touchable = false;
