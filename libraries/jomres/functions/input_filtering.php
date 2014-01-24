95c51eb5d3b23d3d3c521228f1910b472657966d
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2014-01-24 09:41:41 +0000

    Improved input filtering. Whilst javascript cannot be entered in a meaningful way to cause XSS problems, we've added a check for "; and '; anyway to further confound attempts.

--- libraries/jomres/functions/input_filtering.php
+++ libraries/jomres/functions/input_filtering.php
@@ -217,17 +217,19 @@ function jomresGetParam( $request, $element, $def = null, $mask = '' ) // variab
 				if ( $jrConfig[ 'input_filtering' ] != "weak" )
 					{
 					$clean = jomres_purify_html( $dirty );
+					if (strstr($clean,"&#39;;") || strstr($clean,"&#34;;")|| strstr($clean,"Jzs=")|| strstr($clean,"Ijs=") || strstr($clean,"&quot;;") ) // '; "; '; (base64) "; (base64)
+						throw new Exception ("Error, illegal use of Javascript");
 					}
 				else
 					{
-
 					$clean = str_replace( "'", "&#180;", $dirty );
 					}
 				}
 			else
 				{
 				$clean = jomres_sanitise_string( $dirty , $element );
-				
+				if (strstr($clean,"&#39;;") || strstr($clean,"&#34;;")|| strstr($clean,"Jzs=")|| strstr($clean,"Ijs=") || strstr($clean,"&quot;;") ) // '; "; '; (base64) "; (base64)
+					throw new Exception ("Error, illegal use of Javascript");
 				}
 			
 			break;
