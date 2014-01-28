144df224e5a3cd000954c6ffb0908d8f131369a8
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2014-01-28 11:44:19 +0000

    Changed how ajax lists parse data as some hosts were unable to use the older functionality.

--- jomres/core-minicomponents/j06005list_invoices_ajax.class.php
+++ jomres/core-minicomponents/j06005list_invoices_ajax.class.php
@@ -92,7 +92,7 @@ class j06005list_invoices_ajax
 			$sWhere = "AND (";
 			for ( $i=0 ; $i<count($aColumns) ; $i++ )
 				{
-				$sWhere .= "".$aColumns[$i]." LIKE '%".mysql_real_escape_string( $_GET['sSearch'] )."%' OR ";
+				$sWhere .= "".$aColumns[$i]." LIKE '%".jomresGetParam( $_GET, 'sSearch', '' )."%' OR ";
 				}
 			$sWhere = substr_replace( $sWhere, "", -3 );
 			$sWhere .= ')';
