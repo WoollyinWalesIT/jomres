e1a15470436ad4698f55ee82ad1cf8612465268e
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2014-03-27 12:42:35 +0000

    Added Main menu dashboard link to WP Admin for Wordpress super property managers.

---
+++ core-minicomponents/j00010reception_option_01_wp_control_panel.class.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Core file
+ *
+ * @author Vince Wooll <sales@jomres.net>
+ * @version Jomres 7
+ * @package Jomres
+ * @copyright    2005-2013 Vince Wooll
+ * Jomres (tm) PHP, CSS & Javascript files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly.
+ **/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+class j00010reception_option_01_wp_control_panel
+	{
+	function j00010reception_option_01_wp_control_panel( $componentArgs )
+		{
+		// Must be in all minicomponents. Minicomponents with templates that can contain editable text should run $this->template_touch() else just return
+		$MiniComponents = jomres_singleton_abstract::getInstance( 'mcHandler' );
+		if ( $MiniComponents->template_touch )
+			{
+			$this->template_touchable = false;
+
+			return;
+			}
+		
+		$thisJRUser = jomres_singleton_abstract::getInstance( 'jr_user' );
+		
+		if (this_cms_is_wordpress() && $thisJRUser->superPropertyManager) 
+			{
+			$this->cpanelButton = jomres_mainmenu_option( JOMRES_SITEPAGE_URL_ADMIN , 'wp-icon.png', jr_gettext( '_JOMRES_CONTROLPANEL', _JOMRES_CONTROLPANEL, false, false ), null, jr_gettext( "_JOMRES_CUSTOMCODE_JOMRESMAINMENU_RECEPTION_HOME", _JOMRES_CUSTOMCODE_JOMRESMAINMENU_RECEPTION_HOME, false, false ) );
+			}
+		}
+
+
+	// This must be included in every Event/Mini-component
+	function getRetVals()
+		{
+		return $this->cpanelButton;
+		}
+	}
+
+?>
