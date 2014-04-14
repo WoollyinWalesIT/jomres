18b141c14c6f8623c008feda7bd51c8371ef1516
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2014-04-14 17:38:03 +0000

    Added a new "budget" option to property list toolbar, which allows users to see at a glance which properties are within their budget.

---
+++ jomres/core-minicomponents/j06000ajax_budget.class.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Core file
+ *
+ * @author Vince Wooll <sales@jomres.net>
+ * @version Jomres 7
+ * @package Jomres
+ * @copyright    2005-2013 Vince Wooll
+ * Jomres (tm) PHP, CSS & Javascript files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly.
+ **/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+class j06000ajax_budget
+	{
+	function j06000ajax_budget()
+		{
+		$MiniComponents = jomres_singleton_abstract::getInstance( 'mcHandler' );
+		if ( $MiniComponents->template_touch )
+			{
+			$this->template_touchable = false;
+
+			return;
+			}
+		
+		
+		$requested_budget = (int)jomresGetParam( $_REQUEST, 'budget_figure', 0 );
+		jr_import('jomres_user_budget');
+		$budget = new jomres_user_budget();
+		$budget->set_budget($requested_budget);
+		}
+
+	// This must be included in every Event/Mini-component
+	function getRetVals()
+		{
+		return null;
+		}
+	}
+
+?>
