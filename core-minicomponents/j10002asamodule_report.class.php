3d4c262753f4413f4db6d4b180715f761f665509
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2014-05-09 10:31:20 +0000

    Disabled asamodule report from showing on WP, due to how urls are generated the report doesn't make sense on WP installations.

--- core-minicomponents/j10002asamodule_report.class.php
+++ core-minicomponents/j10002asamodule_report.class.php
@@ -29,7 +29,7 @@ class j10002asamodule_report
 		$siteConfig = jomres_singleton_abstract::getInstance( 'jomres_config_site_singleton' );
 		$jrConfig   = $siteConfig->get();
 
-		if ($jrConfig[ 'advanced_site_config' ] == "1")
+		if ($jrConfig[ 'advanced_site_config' ] == "1" && !this_cms_is_wordpress() )
 			{
 			$htmlFuncs          = jomres_singleton_abstract::getInstance( 'html_functions' );
 			$this->cpanelButton = $htmlFuncs->cpanelButton( JOMRES_SITEPAGE_URL_ADMIN . '&task=asamodule_report', 'EditText.png', jr_gettext( "_JOMRES_CUSTOMCODE_ASAMODULE", _JOMRES_CUSTOMCODE_ASAMODULE, false, false ), "/".JOMRES_ROOT_DIRECTORY."/images/jomresimages/small/", jr_gettext( "_JOMRES_CUSTOMCODE_MENUCATEGORIES_DEVELOPERS", _JOMRES_CUSTOMCODE_MENUCATEGORIES_DEVELOPERS, false, false ) );
