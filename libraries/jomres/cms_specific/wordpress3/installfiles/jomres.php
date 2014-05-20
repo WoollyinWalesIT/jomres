d699bc87865edc01a7911d4044ba36778cd60ed6
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2014-05-20 09:26:25 +0000

    Added a fix to the wordpress jomres.php to ensure that if editable javascript is included then bootstrap is set as a dependency.

--- libraries/jomres/cms_specific/wordpress3/installfiles/jomres.php
+++ libraries/jomres/cms_specific/wordpress3/installfiles/jomres.php
@@ -174,7 +174,10 @@ if ( ! class_exists( 'wp_jomres' ) )
 				{
 				foreach ($this->js as $js)
 					{
-					wp_register_script($js['0'], $js['1'], array(), $js['2']);
+					if (strpos($js['1'], 'bootstrap-editable'))
+						wp_register_script($js['0'], $js['1'], array("bootstrap"), $js['2']);
+					else
+						wp_register_script($js['0'], $js['1'], array("jquery"), $js['2']);
 					wp_enqueue_script($js['0']);
 					}
 				}
