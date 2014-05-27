17a3b30e50836ebe14f566428693396d96037b5b
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2014-05-27 15:33:03 +0000

    Updated version numbers, copyright dates and license information in all core files. integration.php updated with a small routine to try to determine which file has failed to call jomres_root.php, if JOMRES_ROOT_DIRECTORY isn't set.

--- language/de-DE.php
+++ language/de-DE.php
@@ -3,9 +3,9 @@
  * Core file
  *
  * @author Vince Wooll <sales@jomres.net>
- * @version Jomres 7
+ * @version Jomres 8
  * @package Jomres
- * @copyright    2005-2013 Vince Wooll
+ * @copyright	2005-2014 Vince Wooll
  * Jomres is currently available for use in all personal or commercial projects under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly.
  **/
 // German Translation since Jomres V2: Roger Perren (http://www.blueforce.ch)
