0e1e5d3cf79c4b19327f8844fa955809d7484065
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2014-06-30 10:33:16 +0000

    Added ability to view the changelog in the administrator -> developer tools. Reformatted the changelog so that we can start making use of BS labels to make it more readable.

---
+++ jomres/core-minicomponents/j16000changelog.class.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Core file
+ *
+ * @author Vince Wooll <sales@jomres.net>
+ * @version Jomres 8
+ * @package Jomres
+ * @copyright	2005-2014 Vince Wooll
+ * Jomres (tm) PHP, CSS & Javascript files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly.
+ **/
+
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+class j16000changelog
+	{
+	function j16000changelog()
+		{
+		$MiniComponents = jomres_singleton_abstract::getInstance( 'mcHandler' );
+		if ( $MiniComponents->template_touch )
+			{
+			$this->template_touchable = false;
+
+			return;
+			}
+		$changelog = nl2br(file_get_contents( JOMRESCONFIG_ABSOLUTE_PATH . JRDS . JOMRES_ROOT_DIRECTORY . JRDS . 'CHANGELOG_JOMRES' ));
+		echo $changelog;
+		
+		}
+
+
+	// This must be included in every Event/Mini-component
+	function getRetVals()
+		{
+		return null;
+		}
+	}
