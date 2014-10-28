3145d7eaabefe3f72dbbfc3ffff4e20e510eedaf
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2014-10-28 10:43:16 +0000

    Tweaked dobooking insertion script to ignore quick booking bookings as the booking data archive doesn't exist for them.

--- core-minicomponents/j03020insertbooking.class.php
+++ core-minicomponents/j03020insertbooking.class.php
@@ -439,9 +439,12 @@ class j03020insertbooking
 						'" . (int) $property_uid . "','" . (float) $single_person_suppliment . "','$extras','" . (string) $extrasquantities . "','" . (float) $extrasValue . "','" . (float) $tax . "','$cartnumber','$datetime','" . (float) $room_total . "','" . (float) $discount . "','$ccode','" . $discount_details . "','" . $bookersUsername . "'," . (int) $coupon_id . "," . $approved . " , '".$booking_data_archive_id ."' , '".$secret_key."' , '".get_showtime('lang')."') ";
 					$contract_uid = doInsertSql( $query, "" );
 					
-					$query = "UPDATE #__jomres_booking_data_archive SET contract_uid = ".$contract_uid." WHERE id = ".$booking_data_archive_id;
-					doInsertSql( $query, "" );
-					
+					if ( $booking_data_archive_id > 0 ) // Quick bookings will log a mysql error, but quick bookings don't use the booking data archive.
+						{
+						$query = "UPDATE #__jomres_booking_data_archive SET contract_uid = ".$contract_uid." WHERE id = ".$booking_data_archive_id;
+						doInsertSql( $query, "" );
+						}
+						
 					if ( $mrConfig[ 'singleRoomProperty' ] == 1 ) 
 						$newtext = $tmpBookingHandler->getBookingFieldVal( "lastminutediscount" );
 					else
