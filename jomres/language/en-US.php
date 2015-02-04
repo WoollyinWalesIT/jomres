2d47bea70486576e9d0c35ee867075e2440a7427
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2015-02-04 09:33:39 +0000

    Moved caching options into new tab Move commission rate site config option to ROI tab.

--- jomres/language/en-US.php
+++ jomres/language/en-US.php
@@ -2584,3 +2584,6 @@ jr_define('_JOMRES_MULTISITES_SELECT_A_SITE', 'Select a site');
 jr_define('_JOMRES_MULTISITES_MULTISITES_LABEL', 'Site id');
 
 jr_define('_JOMRES_IS_EU_COUNTRY', 'EU Country?');
+
+jr_define('_JOMRES_WORD_CACHING', 'Caching');
+
--- jomres/language/propertyrental/pl-PL.php
+++ jomres/language/propertyrental/pl-PL.php
@@ -2358,3 +2358,6 @@ jr_define('_JOMRES_MULTISITES_SELECT_A_SITE','Wybierz stronę');
 jr_define('_JOMRES_MULTISITES_MULTISITES_LABEL','Id strony');
 
 jr_define('_JOMRES_IS_EU_COUNTRY', 'EU Country?');
+
+jr_define('_JOMRES_WORD_CACHING', 'Caching');
+
