d83e5442ac3418d868af31ed948099739bf449f5
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2015-02-19 10:28:23 +0000

    Finalise Jomres 8.1.19

--- jomres/jomres_config.php
+++ jomres/jomres_config.php
@@ -16,7 +16,7 @@ defined( '_JOMRES_INITCHECK' ) or die( '' );
 // ################################################################
 
 
-$mrConfig[ 'version' ] = '8.1.18';
+$mrConfig[ 'version' ] = '8.1.19';
 
 $mrConfig[ 'newTariffModels' ]                 = "2";
 $mrConfig[ 'singlePersonSuppliment' ]          = '0';
