8413080b89e328ffd1b6f07171b67b2a8809d03c
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2015-03-11 09:08:17 +0000

    jr_gettext.php cleaned up, removing older comments and tidyied up formatting.

--- jomres/libraries/jomres/functions/jr_gettext.php
+++ jomres/libraries/jomres/functions/jr_gettext.php
@@ -43,8 +43,10 @@ function jr_gettext( $theConstant, $theValue, $okToEdit = true, $isLink = false 
 	$property_uid = (int) get_showtime( 'property_uid' );
 
 	global $text_bucket;
-	if ( !isset( $text_bucket ) ) $text_bucket = array ();
-	if ( isset( $text_bucket[ $property_uid ][ $theConstant ][ $okToEdit ] ) ) return $text_bucket[ $property_uid ][ $theConstant ][ $okToEdit ];
+	if ( !isset( $text_bucket ) ) 
+		$text_bucket = array ();
+	if ( isset( $text_bucket[ $property_uid ][ $theConstant ][ $okToEdit ] ) ) 
+		return $text_bucket[ $property_uid ][ $theConstant ][ $okToEdit ];
 
 	$customTextObj   = jomres_singleton_abstract::getInstance( 'custom_text' );
 	$customTextArray = $customTextObj->get_custom_text();
@@ -54,26 +56,31 @@ function jr_gettext( $theConstant, $theValue, $okToEdit = true, $isLink = false 
 		{
 		$thisJRUser = jomres_singleton_abstract::getInstance( 'jr_user' );
 
-		if ( !isset( $tmpBookingHandler->user_settings[ 'editing_on' ] ) ) $tmpBookingHandler->user_settings[ 'editing_on' ] = false;
+		if ( !isset( $tmpBookingHandler->user_settings[ 'editing_on' ] ) ) 
+			$tmpBookingHandler->user_settings[ 'editing_on' ] = false;
 
-		if ( !$thisJRUser->userIsManager ) $tmpBookingHandler->user_settings[ 'editing_on' ] = false;
+		if ( !$thisJRUser->userIsManager ) 
+			$tmpBookingHandler->user_settings[ 'editing_on' ] = false;
 
-		if ( $thisJRUser->userIsManager && $thisJRUser->accesslevel < 2 ) $tmpBookingHandler->user_settings[ 'editing_on' ] = false;
+		if ( $thisJRUser->userIsManager && $thisJRUser->accesslevel < 2 ) 
+			$tmpBookingHandler->user_settings[ 'editing_on' ] = false;
 
 		$editing = $tmpBookingHandler->user_settings[ 'editing_on' ];
 		}
 	else
-	$tmpBookingHandler->user_settings[ 'editing_on' ] = false;
+		$tmpBookingHandler->user_settings[ 'editing_on' ] = false;
 
 	$mrConfig   = getPropertySpecificSettings( $property_uid );
 	$siteConfig = jomres_singleton_abstract::getInstance( 'jomres_config_site_singleton' );
 	$jrConfig   = $siteConfig->get();
-	if ( isset( $thisJRUser->accesslevel ) ) $accessLevel = $thisJRUser->accesslevel;
+	if ( isset( $thisJRUser->accesslevel ) ) 
+		$accessLevel = $thisJRUser->accesslevel;
 	else
-	$accessLevel = 0;
+		$accessLevel = 0;
 
 	$br = "";
-	if ( get_showtime( 'task' ) == "editCustomTextAll" ) $br = "<br>";
+	if ( get_showtime( 'task' ) == "editCustomTextAll" ) 
+		$br = "<br />";
 
 	if ( count( $customTextArray ) > 0 )
 		{
@@ -87,20 +94,8 @@ function jr_gettext( $theConstant, $theValue, $okToEdit = true, $isLink = false 
 			}
 		}
 	else
-	$theText = jr_get_defined( $theConstant, $theValue );
+		$theText = jr_get_defined( $theConstant, $theValue );
 
-	/* 	if (isset($thisJRUser))
-			{
-			$MiniComponents =jomres_singleton_abstract::getInstance('mcHandler');
-			$componentArgs = array(
-			'constant'=>$theConstant,
-			'property_uid'=>$property_uid,
-			'text_to_translate'=>$theText
-			);
-			$MiniComponents->triggerEvent('05100',$componentArgs);
-			} */
-
-	//$theText=jomres_reconvertString($theText);
 	if ( isset( $thisJRUser ) )
 		{
 		if ( ( $_REQUEST[ 'task' ] == "touch_templates" || $_REQUEST[ 'task' ] == "translate_locales" || $_REQUEST[ 'task' ] == "translate_lang_file_strings" ) && $thisJRUser->userIsManager )
@@ -111,81 +106,43 @@ function jr_gettext( $theConstant, $theValue, $okToEdit = true, $isLink = false 
 			$editing                                       = true;
 			}
 
-		if ( $jrConfig[ 'allowHTMLeditor' ] != "1" ) $theText = jomres_remove_HTML( $theText );
+		if ( $jrConfig[ 'allowHTMLeditor' ] != "1" ) 
+			$theText = jomres_remove_HTML( $theText );
 
 		$theText = jomres_decode( $theText );
 
 		if ( $thisJRUser->userIsManager && ( $editing || ( $jrConfig[ 'editingModeAffectsAllProperties' ] == "1" && $thisJRUser->superPropertyManager == true ) ) && $okToEdit && ( $accessLevel == 2 ) )
 			{
-			if ( strlen( trim( $theText ) ) == 0 || strtolower( trim( $theText ) ) == "<span></span>" || strtolower( trim( $theText ) ) == "<span> </span>" || strtolower( trim( $theText ) ) == "<span>  </span>" ) $theText = "";
+			if ( strlen( trim( $theText ) ) == 0 || strtolower( trim( $theText ) ) == "<span></span>" || strtolower( trim( $theText ) ) == "<span> </span>" || strtolower( trim( $theText ) ) == "<span>  </span>" ) 
+				$theText = "";
+			
 			$indexphp = "index.php";
-			$title    = ' title="' . jr_gettext( '_JOMRES_COM_MR_VRCT_ROOM_LINKTEXT', _JOMRES_COM_MR_VRCT_ROOM_LINKTEXT, false ) . '" ';
+			$title    = ' title="' . jr_get_defined( '_JOMRES_COM_MR_VRCT_ROOM_LINKTEXT', 'Edit item' ) . '" ';
 
 			if ( $isLink )
 				{
-				// $link = JOMRES_SITEPAGE_URL.'&task=editCustomText&lng='.get_showtime('lang').'&theConstant='.$theConstant."&property_uid=".$property_uid;
-				// $editingLink="<a class=\"jomrestexteditable\" $title href=\"$link\" target=\"_blank\" ><img src=\"".get_showtime('live_site')."/".JOMRES_ROOT_DIRECTORY."/images/jricon.png\" width=\"10\" height=\"10\" border=\"0\"></a>";
-				// $theText=$editingLink.$theText;
+				//do nothing
 				}
 			else
 				{
 				if ( $jrConfig[ 'editinplace' ] == 1 && $_REQUEST[ 'no_html' ] != '1' )
 					{
-					// replaced with x-editable code below
-					// if (!defined('JOMRESJQUERY_EDITINPLACE'))
-					// {
-					// define("JOMRESJQUERY_EDITINPLACE",1);
-					// echo '
-					// <script type="text/javascript">
-					// jomresJquery(document).ready(function() {';
-					// if ($_REQUEST['task']=="touch_templates" || $_REQUEST['task']=="translate_locales" || $_REQUEST['task']=="translate_lang_file_strings")
-					// echo 'jomresJquery(".jqueryeditable").editable("'.JOMRES_SITEPAGE_URL_ADMIN_AJAX.'&task=editinplace&no_html=1&lang='.get_showtime("lang").'", ';
-					// else
-					// echo 'jomresJquery(".jqueryeditable").editable("'.JOMRES_SITEPAGE_URL_AJAX.'&task=editinplace&no_html=1", ';
-					// echo "	{
-					// indicator : '".jr_gettext('JOMRES_WORD_SAVING',JOMRES_WORD_SAVING,false)."',
-					// id			: 'theConstant',
-					// name		: 'newtext',
-					// type		: 'textarea',
-					// cancel		: 'x',
-					// submit		: 'OK',
-					// tooltip		: '".htmlspecialchars(jr_gettext('_JOMRES_COM_MR_VRCT_ROOM_LINKTEXT',_JOMRES_COM_MR_VRCT_ROOM_LINKTEXT,false))."',
-					// height		: '20',
-					// width		: '200',
-					// style		: 'inherit'
-					// });
-					// });
-					// </script>
-					// ";
-					// }
-					// if (using_bootstrap())
-					// $theText='<span class="jqueryeditable label label-important" id="'.$theConstant.'" >'.jomres_remove_HTML($theText,'').'</span>';
-					// else
-					// $theText='<span class="jomrestexteditable"><div class="jqueryeditable" id="'.$theConstant.'" >'.htmlspecialchars($theText).'</div></span>';
-
 					if ( jomres_cmsspecific_areweinadminarea() ) $url = JOMRES_SITEPAGE_URL_ADMIN_AJAX . '&task=editinplace&no_html=1&lang=' . get_showtime( "lang" );
 					else
 					$url = JOMRES_SITEPAGE_URL_AJAX . '&task=editinplace&no_html=1';
 
 					$theText = '<a href="#" id="' . $theConstant . '" data-type="text" data-pk="' . $theConstant . '" data-url="' . $url . '" data-original-title="' . htmlspecialchars( $theText ) . '">' . htmlspecialchars( $theText ) . '</a>
 					<script>jomresJquery(\'#' . $theConstant . '\').editable();</script>';
-
 					}
 				else
 					{
-					// $link = JOMRES_SITEPAGE_URL_AJAX.'&task=editCustomText&popup=1&lng='.get_showtime('lang').'&theConstant='.$theConstant."&property_uid=".$property_uid;
-					// $editingLink="<a class=\"jomrestexteditable\" $title href=\"$link\" target=\"_blank\" >$theText</a>";
-					// $theText=$editingLink.$br;
+					//do nothing
 					}
 				}
 			}
 		}
 
-	//$theText = jomres_purify_html($theText);
 	$text_bucket[ $property_uid ][ $theConstant ][ $okToEdit ] = $theText;
 
 	return $theText;
 	}
-
-
-?>
