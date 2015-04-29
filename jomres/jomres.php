92aa84b1456cc695870236475dd0902a8602db8f
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2015-04-29 11:23:32 +0000

    .

--- jomres/jomres.php
+++ jomres/jomres.php
@@ -7,6 +7,7 @@
  * @package Jomres
  * @copyright	2005-2014 Vince Wooll
  * Jomres (tm) PHP, CSS & Javascript files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly.
+ 
  **/
 
 ##################################################################
