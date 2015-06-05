054e942be1e6f680902e455b57c7cef1e3cb8b4f
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2015-06-05 15:47:10 +0000

    Added a new string to language files.

--- language/propertyrental/sl-SI.php
+++ language/propertyrental/sl-SI.php
@@ -2608,3 +2608,5 @@ jr_define('_OPENEXCHANGE_API',"Open Exchange Rates API key");
 jr_define('_OPENEXCHANGE_API_DESC','To convert prices between services you will need an Open Exchange Rates API key. You MUST have an API key to display prices correctly in Jomres, however you can register for a free key at https://openexchangerates.org/signup/free (hourly updates, 1000 requests/month - no HTTPS, email support or advanced features). Jomres downloads exchange rates once a day so as long as you use the key on only a handful of sites, you\'re unlikely to exceed these limits. Previous to Jomres 8.3 we used an older, undocumented Yahoo feature however have discovered that using this feature is in contradiction of Yahoo\'s Terms and Conditions. As a result we have to assume that the feature may disappear at some point in the future, hence the change to using Open Exchange Rates.');
 
 jr_define('_JOMRES_COMMISSION',"Commission");
+
+jr_define('_JOMRES_MEDIA_CENTRE_NOTES_CORE_EXTRAS',"If you upload multiple images for individual optional extras, only the first image will be used.");
