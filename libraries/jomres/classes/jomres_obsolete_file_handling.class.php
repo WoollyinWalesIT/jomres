65ed57f621090a85052245ea0982b061bffed6aa
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2015-06-22 16:37:37 +0000

    Retired old stats functionality, and introduced new stats functionality that's responsive and easier to write for.

--- libraries/jomres/classes/jomres_obsolete_file_handling.class.php
+++ libraries/jomres/classes/jomres_obsolete_file_handling.class.php
@@ -1104,7 +1104,8 @@ class jomres_obsolete_file_handling
 		$this->add_obs_file( $this->dir_templates_jqueryui_administrator . 'partners_choose.html' );
 		$this->add_obs_file( $this->dir_templates_bootstrap_administrator . 'partners_choose.html' );
 		$this->add_obs_file( $this->dir_templates_bootstrap3_administrator . 'partners_choose.html' );
-
+		
+		$this->add_obs_file( $this->dir_minicomponents . 'j06002fe_stats.class.php' );
 		}
 
 	function add_obs_file( $path_and_file )
