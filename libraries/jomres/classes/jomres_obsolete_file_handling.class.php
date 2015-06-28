8f22e52523bc1e7245c25b4bba048a9b47df5787
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2015-06-28 08:30:41 +0000

    Modified slideshow feature to show multiple slideshows on a page. Modified show rooms list to show a small slideshow for each room's images.

--- libraries/jomres/classes/jomres_obsolete_file_handling.class.php
+++ libraries/jomres/classes/jomres_obsolete_file_handling.class.php
@@ -1121,6 +1121,9 @@ class jomres_obsolete_file_handling
 		$this->add_obs_file( $this->dir_minicomponents . 'j01055showroomdetails.class.php' );
 		$this->add_obs_file( $this->dir_minicomponents . 'j06000show_property_room_features.class.php' );
 		
+		$this->add_obs_file( $this->dir_templates_jqueryui_frontend . 'slideshow_medium.html' );
+		$this->add_obs_file( $this->dir_templates_bootstrap_frontend . 'slideshow_medium.html' );
+		$this->add_obs_file( $this->dir_templates_bootstrap3_frontend . 'slideshow_medium.html' );
 		}
 
 	function add_obs_file( $path_and_file )
