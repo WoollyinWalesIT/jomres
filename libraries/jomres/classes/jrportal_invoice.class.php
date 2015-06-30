3271701b098703c9a6715a045086e48f998cc7bc
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2015-06-30 12:48:19 +0000

    Tweaked a language string to ensure it's passed thru jr_gettext before line items are added to an invoice.

--- libraries/jomres/classes/jrportal_invoice.class.php
+++ libraries/jomres/classes/jrportal_invoice.class.php
@@ -794,7 +794,7 @@ class jrportal_invoice
 		if ( number_format( $balance, 2, '.', '' ) > 0.00 )
 			{
 			$line_item_data = array ( 'tax_code_id' => 0, 
-									 'name' => '_JOMRES_AJAXFORM_BILLING_BALANCE_PAYMENT', 
+									 'name' => jr_gettext("_JOMRES_AJAXFORM_BILLING_BALANCE_PAYMENT",_JOMRES_AJAXFORM_BILLING_BALANCE_PAYMENT,false,false),
 									 'description' => '('.$today.')', 
 									 'init_price' => 0-number_format( $balance, 2, '.', '' ), 
 									 'init_qty' => 1, 
