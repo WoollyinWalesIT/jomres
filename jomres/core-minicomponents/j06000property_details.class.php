72dd9e5439ad29c563d49cea22443100395e2e40
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2015-10-12 16:33:47 +0000

    Added a new 06000 script "property_details" that allows us to call Jomres via the asamodule mambot.

---
+++ jomres/core-minicomponents/j06000property_details.class.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Core file
+ *
+ * @author Vince Wooll <sales@jomres.net>
+ * @version Jomres 8
+ * @package Jomres
+ * @copyright	2005-2015 Vince Wooll
+ * Jomres (tm) PHP, CSS & Javascript files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly.
+ **/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+class j06000property_details
+	{
+	function __construct( $componentArgs )
+		{
+		// Must be in all minicomponents. Minicomponents with templates that can contain editable text should run $this->template_touch() else just return
+		$MiniComponents = jomres_singleton_abstract::getInstance( 'mcHandler' );
+		if ( $MiniComponents->template_touch )
+			{
+			$this->template_touchable = false;
+
+			return;
+			}
+		$thisJRUser		= jomres_singleton_abstract::getInstance( 'jr_user' );
+		
+		$property_uid = intval( jomresGetParam( $_REQUEST, 'property_uid', 0 ) );
+		$current_property_details = jomres_singleton_abstract::getInstance( 'basic_property_details' );
+		$current_property_details->gather_data( $property_uid );
+
+		if ( $property_uid > 0 )
+			{
+			if  ( $current_property_details->published == 1 || in_array( $property_uid , $thisJRUser->authorisedProperties ) )
+				{
+				property_header( $property_uid );
+				$MiniComponents->triggerEvent( '00016',  array ( "property_uid" => $property_uid ) );
+				}
+			}
+		}
+
+
+	// This must be included in every Event/Mini-component
+	function getRetVals()
+		{
+		return $this->retVals;
+		}
+	}
