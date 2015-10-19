50160b9d62f6d14cb43ea269fb2f01aa23a0f1a9
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2015-10-19 11:22:15 +0000

    Added a small plugin to show property description on it's own if required. Removed rooms slideshow from rooms list, as it's already a separate plugin which can be called independently.

---
+++ core-minicomponents/j06000show_property_description.class.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Core file
+ *
+ * @author Vince Wooll <sales@jomres.net>
+ * @version Jomres 8
+ * @package Jomres
+ * @copyright	2005-2015 Vince Wooll
+ * Jomres (tm) PHP, CSS & Javascript files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly.
+ **/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+class j06000show_property_description
+	{
+	function __construct( $componentArgs )
+		{
+		// Must be in all minicomponents. Minicomponents with templates that can contain editable text should run $this->template_touch() else just return
+		$MiniComponents = jomres_singleton_abstract::getInstance( 'mcHandler' );
+		if ( $MiniComponents->template_touch )
+			{
+			$this->template_touchable = false;
+
+			return;
+			}
+		$output = array ();
+
+		if (isset($componentArgs['property_uid']))
+			$property_uid = (int)$componentArgs['property_uid'];
+		elseif ( isset ( $_REQUEST ['property_uid'] ))
+			$property_uid = (int)$_REQUEST ['property_uid'];
+		else 
+			return;
+		
+		if (!user_can_view_this_property($property_uid)) return;
+
+		if (isset($componentArgs['output_now']))
+			$output_now = $componentArgs['output_now'];
+		else
+			$output_now = true;
+
+		$current_property_details = jomres_singleton_abstract::getInstance( 'basic_property_details' );
+		$current_property_details->gather_data( $property_uid );
+		
+		$output['_JOMRES_COM_MR_VRCT_PROPERTY_HEADER_PROPDESCRIPTION']	= jr_gettext( '_JOMRES_COM_MR_VRCT_PROPERTY_HEADER_PROPDESCRIPTION', _JOMRES_COM_MR_VRCT_PROPERTY_HEADER_PROPDESCRIPTION );
+		$output['PROPERTY_DESCRIPTION']									= $current_property_details->property_description;
+		
+		$pageoutput = array( $output );
+		$tmpl = new patTemplate();
+		$tmpl->setRoot( JOMRES_TEMPLATEPATH_FRONTEND );
+		$tmpl->addRows( 'pageoutput',$pageoutput);
+		$tmpl->readTemplatesFromInput( 'show_property_description.html' );
+		$template = $tmpl->getParsedTemplate();
+		if ( $output_now )
+			echo $template;
+		else
+			$this->retVals = $template;
+				
+		}
+
+
+	// This must be included in every Event/Mini-component
+	function getRetVals()
+		{
+		return $this->retVals;
+		}
+	}
