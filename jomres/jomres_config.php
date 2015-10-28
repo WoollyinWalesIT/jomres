5534ee00753d90102eaca3067070ea65028d2be7
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2015-10-28 12:01:00 +0000

    Finalise Jomres 9.2.0

--- jomres/jomres_config.php
+++ jomres/jomres_config.php
@@ -16,7 +16,7 @@ defined( '_JOMRES_INITCHECK' ) or die( '' );
 // ################################################################
 
 
-$mrConfig[ 'version' ] = '9.1.6';
+$mrConfig[ 'version' ] = '9.2.0';
 
 $mrConfig[ 'newTariffModels' ]                 = "2";
 $mrConfig[ 'singlePersonSuppliment' ]          = '0';
