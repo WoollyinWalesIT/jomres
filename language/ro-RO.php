14def1ad27b62c7308397a55f0793c7040d57eeb
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2016-01-04 16:55:25 +0000

    Updated the Is Wrapped option's description to clarify it's usage.

--- language/ro-RO.php
+++ language/ro-RO.php
@@ -715,7 +715,7 @@ jr_define( '_JOMRES_COM_JRCONFIG_GLOBALCURRENCYYESNO', 'Use global currency?' );
 jr_define( '_JOMRES_COM_JRCONFIG_GLOBALCURRENCYYESNO_DESC', 'Set this to yes to force all properties to use the same currency (e.g. & # 8 3 6 4 ;)' );
 jr_define( '_JOMRES_COM_JRCONFIG_GLOBALCURRENCY', 'Global currency symbol' );
 jr_define( '_JOMRES_COM_JRCONFIG_ISWRAPPED', 'Component wrapped' );
-jr_define( '_JOMRES_COM_JRCONFIG_ISWRAPPED_DESC', 'Set this to Yes if the component is wrapped so that module and header areas are not to be seen' );
+jr_define( '_JOMRES_COM_JRCONFIG_ISWRAPPED_DESC', 'Set this to Yes if if you want to force the site into Full Page view. This might be useful if you intend to access the site via iframes.' );
 jr_define( '_JOMRES_COM_USERIS_SUPERPROPERTYMANAGER', 'Super Property Manager' );
 jr_define( '_JOMRES_COM_WEEKENDONLY', 'Numai in weekend-uri' );
 jr_define( '_JOMRES_COM_WEEKENDDAYS', 'Zilele weekend-ului' );
