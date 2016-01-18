fe60fec3de1235b98e7ad3995fe25fe7e7043502
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2016-01-18 13:18:43 +0000

    HTML purifier updated to 4.7

--- libraries/htmlpurifier/htmlpurifier-4.4.0-standalone/standalone/HTMLPurifier/ConfigSchema/ValidatorAtom.php
+++ libraries/htmlpurifier/htmlpurifier-4.4.0-standalone/standalone/HTMLPurifier/ConfigSchema/ValidatorAtom.php
@@ -7,77 +7,124 @@
  * are independent and usually scalar.
  */
 class HTMLPurifier_ConfigSchema_ValidatorAtom
-	{
+{
+    /**
+     * @type string
+     */
+    protected $context;
 
-	protected $context, $obj, $member, $contents;
+    /**
+     * @type object
+     */
+    protected $obj;
 
-	public function __construct( $context, $obj, $member )
-		{
-		$this->context  = $context;
-		$this->obj      = $obj;
-		$this->member   = $member;
-		$this->contents =& $obj->$member;
-		}
+    /**
+     * @type string
+     */
+    protected $member;
 
-	public function assertIsString()
-		{
-		if ( !is_string( $this->contents ) ) $this->error( 'must be a string' );
+    /**
+     * @type mixed
+     */
+    protected $contents;
 
-		return $this;
-		}
+    public function __construct($context, $obj, $member)
+    {
+        $this->context = $context;
+        $this->obj = $obj;
+        $this->member = $member;
+        $this->contents =& $obj->$member;
+    }
 
-	public function assertIsBool()
-		{
-		if ( !is_bool( $this->contents ) ) $this->error( 'must be a boolean' );
+    /**
+     * @return HTMLPurifier_ConfigSchema_ValidatorAtom
+     */
+    public function assertIsString()
+    {
+        if (!is_string($this->contents)) {
+            $this->error('must be a string');
+        }
+        return $this;
+    }
 
-		return $this;
-		}
+    /**
+     * @return HTMLPurifier_ConfigSchema_ValidatorAtom
+     */
+    public function assertIsBool()
+    {
+        if (!is_bool($this->contents)) {
+            $this->error('must be a boolean');
+        }
+        return $this;
+    }
 
-	public function assertIsArray()
-		{
-		if ( !is_array( $this->contents ) ) $this->error( 'must be an array' );
+    /**
+     * @return HTMLPurifier_ConfigSchema_ValidatorAtom
+     */
+    public function assertIsArray()
+    {
+        if (!is_array($this->contents)) {
+            $this->error('must be an array');
+        }
+        return $this;
+    }
 
-		return $this;
-		}
+    /**
+     * @return HTMLPurifier_ConfigSchema_ValidatorAtom
+     */
+    public function assertNotNull()
+    {
+        if ($this->contents === null) {
+            $this->error('must not be null');
+        }
+        return $this;
+    }
 
-	public function assertNotNull()
-		{
-		if ( $this->contents === null ) $this->error( 'must not be null' );
+    /**
+     * @return HTMLPurifier_ConfigSchema_ValidatorAtom
+     */
+    public function assertAlnum()
+    {
+        $this->assertIsString();
+        if (!ctype_alnum($this->contents)) {
+            $this->error('must be alphanumeric');
+        }
+        return $this;
+    }
 
-		return $this;
-		}
+    /**
+     * @return HTMLPurifier_ConfigSchema_ValidatorAtom
+     */
+    public function assertNotEmpty()
+    {
+        if (empty($this->contents)) {
+            $this->error('must not be empty');
+        }
+        return $this;
+    }
 
-	public function assertAlnum()
-		{
-		$this->assertIsString();
-		if ( !ctype_alnum( $this->contents ) ) $this->error( 'must be alphanumeric' );
+    /**
+     * @return HTMLPurifier_ConfigSchema_ValidatorAtom
+     */
+    public function assertIsLookup()
+    {
+        $this->assertIsArray();
+        foreach ($this->contents as $v) {
+            if ($v !== true) {
+                $this->error('must be a lookup array');
+            }
+        }
+        return $this;
+    }
 
-		return $this;
-		}
-
-	public function assertNotEmpty()
-		{
-		if ( empty( $this->contents ) ) $this->error( 'must not be empty' );
-
-		return $this;
-		}
-
-	public function assertIsLookup()
-		{
-		$this->assertIsArray();
-		foreach ( $this->contents as $v )
-			{
-			if ( $v !== true ) $this->error( 'must be a lookup array' );
-			}
-
-		return $this;
-		}
-
-	protected function error( $msg )
-		{
-		throw new HTMLPurifier_ConfigSchema_Exception( ucfirst( $this->member ) . ' in ' . $this->context . ' ' . $msg );
-		}
-
-	}
+    /**
+     * @param string $msg
+     * @throws HTMLPurifier_ConfigSchema_Exception
+     */
+    protected function error($msg)
+    {
+        throw new HTMLPurifier_ConfigSchema_Exception(ucfirst($this->member) . ' in ' . $this->context . ' ' . $msg);
+    }
+}
 
 // vim: et sw=4 sts=4
--- libraries/htmlpurifier/htmlpurifier-4.4.0-standalone/standalone/HTMLPurifier/Language/classes/en-x-test.php
+++ libraries/htmlpurifier/htmlpurifier-4.4.0-standalone/standalone/HTMLPurifier/Language/classes/en-x-test.php
@@ -3,9 +3,7 @@
 // private class for unit testing
 
 class HTMLPurifier_Language_en_x_test extends HTMLPurifier_Language
-	{
-
-
-	}
+{
+}
 
 // vim: et sw=4 sts=4
