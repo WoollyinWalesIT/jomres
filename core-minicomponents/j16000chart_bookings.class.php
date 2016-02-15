028025fe39e4730da399b5ab4e9e4a0156f446c3
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2016-02-15 08:12:11 +0000

    Added code to limit the number of decimal places in charts.

--- core-minicomponents/j16000chart_bookings.class.php
+++ core-minicomponents/j16000chart_bookings.class.php
@@ -69,7 +69,7 @@ class j16000chart_bookings
 				$month =  date("n" , strtotime($r->raised_date) );
 				$year =  date("Y" , strtotime($r->raised_date) );
 				
-				$results[$year][$month] += $r->grand_total;
+				$results[$year][$month] += number_format((float)$r->grand_total, 2, '.', '');
 				}
 			
 			//sort results by year ascending
