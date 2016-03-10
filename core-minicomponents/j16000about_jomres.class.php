d78e20553c1369fad206504d9cc5c546cd988a9d
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2016-03-10 13:27:55 +0000

    About page updated.

--- core-minicomponents/j16000about_jomres.class.php
+++ core-minicomponents/j16000about_jomres.class.php
@@ -8,7 +8,7 @@
  * @copyright	2005-2015 Vince Wooll
  * Jomres (tm) PHP, CSS & Javascript files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly.
  **/
-
+ 
 // ################################################################
 defined( '_JOMRES_INITCHECK' ) or die( '' );
 // ################################################################
@@ -40,13 +40,13 @@ class j16000about_jomres
 		<h2 class=page-header>About Jomres</h2>
 		<div class="' . $class . '" style="width:100%;">
 		<div style="' . $style . '">
-		<p><a href="http://www.jomres.net" target="_blank">Jomres</a> is (probably) Joomla\'s favourite booking system.</p>
+		<p><a href="https://www.jomres.net" target="_blank">Jomres</a> is (probably) Joomla\'s favourite booking system.</p>
 		<p>Developed and maintained by Vince Wooll and <a href="http://www.jomres-extras.com/" target="_blank">Aladar Barthi</a>, it was initially concieved in early 2005 as a Mambo based solution to a client\'s hotel management needs. While it wasn\'t originally expected to be an online booking system it quickly morphed into one as users requested more and more features.</p>
 		<p>As the number of feature requests grew Vince knew that he would need to dedicate more time to the project and in July 2005 Jomres was formally released as a commercial project. Since then Jomres has become one of Mambo and now Joomla\'s longest running projects, having survived various versions of Mambo, then Joomla 1.0, 1.5, 1.6, 1.7, 2.5 and 3.</p>
-		<p>Jomres 8.1 (Available May 2014) further stamps it\'s reputation of being the CMS\'s booking system of choice by adding Wordpress to the list of CMS that it can be installed in.</p>
-		<p>Over the years it has seen many new evolutions and features, the primary of these being Jomres\' unique and exceptionally powerful <a href="http://manual.jomres.net/developers_guide_minicomps.html" target="_blank">"Minicomponent"</a> system. This functionality allows us (and you) to replace core functionality without modifying any core files, and indeed to run applications that exist within the Jomres framework (for example, the Auction House plugin).</p>
+		<p>Jomres further stamps it\'s reputation of being the CMS\'s booking system of choice by adding Wordpress to the list of CMS that it can be installed in.</p>
+		<p>Over the years it has seen many new evolutions and features, the primary of these being Jomres\' unique and exceptionally powerful "Minicomponent" system. This functionality allows us (and you) to replace core functionality without modifying any core files, and indeed to run applications that exist within the Jomres framework (for example, the Auction House plugin).</p>
 		<p>In addition to this, Jomres offers more unique features such as it\'s powerful "editing mode", which gives us the opportunity to create unique strings for each property on the system, and different language files for different property types. </p>
-		<p>We are very proud of Jomres, and hope that you enjoy working on it as much as we continue to enjoy working with our users. If you have a support license, you can get support for the system through our ticket system at <a href="http://tickets.jomres.net" target="_blank">tickets.jomres.net</a>, or if you\'d like to just chat, please say "Hi" <a href="http://www.jomres.net/index.php?option=com_kunena&view=listcat&Itemid=291" target="_blank">in the forums</a>.
+		<p>We are very proud of Jomres, and hope that you enjoy working on it as much as we continue to enjoy working with our users. If you have a support license, you can get support for the system through our ticket system at <a href="https://tickets.jomres.net" target="_blank">tickets.jomres.net</a>.
 		</div></div>
 		';
 		}
