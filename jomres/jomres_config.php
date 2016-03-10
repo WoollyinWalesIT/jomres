d0405e8b4d5936205471f9005f826e873f2d212a
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2016-03-10 14:35:08 +0000

    Finalise Jomres 9.6.1

--- jomres/jomres_config.php
+++ jomres/jomres_config.php
@@ -4,9 +4,9 @@
 	* Core file
  *
  * @author Vince Wooll <sales@jomres.net>
- * @version Jomres 8
+ * @version Jomres 9
  * @package Jomres
- * @copyright	2005-2014 Vince Wooll
+ * * @copyright	2005-2016 Vince Wooll
  * Jomres (tm) PHP, CSS & Javascript files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly.
  **/
  
@@ -15,7 +15,7 @@
 defined( '_JOMRES_INITCHECK' ) or die( '' );
 // ################################################################
 
-$mrConfig[ 'version' ] = '9.6.0';
+$mrConfig[ 'version' ] = '9.6.1';
 
 $mrConfig[ 'newTariffModels' ]                 = "2";
 $mrConfig[ 'singlePersonSuppliment' ]          = '0';
--- jomres/language/propertyrental/de-DE.php
+++ jomres/language/propertyrental/de-DE.php
@@ -3,9 +3,9 @@
  * Core file
  *
  * @author Vince Wooll <sales@jomres.net>
- * @version Jomres 8
+ * @version Jomres 9
  * @package Jomres
- * @copyright	2005-2014 Vince Wooll
+ * @copyright	2005-2016 Vince Wooll
  * Jomres is currently available for use in all personal or commercial projects under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly.
  **/
 // German Translation since Jomres V2: Roger Perren (http://www.blueforce.ch)
