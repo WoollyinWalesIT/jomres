c40c45c7070661037d77f74be601cf50f9201a89
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2016-04-15 12:24:39 +0000

    Resolved a notice level warning.

--- jomres/core-minicomponents/j10003control_panel_buttons.class.php
+++ jomres/core-minicomponents/j10003control_panel_buttons.class.php
@@ -31,6 +31,9 @@ class j10003control_panel_buttons
 		$control_panel_buttons_categorised = array ();
 		foreach ( $control_panel_buttons as $button )
 			{
+			if (!isset($button[ 'order' ]))
+				$button[ 'order' ] = 0;
+			
 			$category                                          = $button[ 'category' ];
 			$order                                             = $button[ 'order' ];
 			$control_panel_buttons_categorised[ $category ][ ] = $button;
