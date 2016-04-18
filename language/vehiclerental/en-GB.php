a71330c560b957d867ecff96dd8157df3f8e47d5
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2016-04-18 11:36:43 +0000

    Added some missing definitions to language files.

--- language/vehiclerental/en-GB.php
+++ language/vehiclerental/en-GB.php
@@ -2478,7 +2478,25 @@ jr_define('TOUR_ID_TOUR_TARGET_EDIT_GUEST_DISCOUNT_TITLE' , "Discount." );
 jr_define('TOUR_ID_TOUR_TARGET_EDIT_GUEST_DISCOUNT_CONTENT' , "You can choose to give this guest an automatic discount which will be applied whenever they make a booking." );
 
 
-jr_define('DATATABLES_SINFO' , "Showing _START_ to _END_ of _TOTAL_ entries" );
+jr_define('DATATABLES_SEMPTYTABLE'		, "No data available in table" );
+jr_define('DATATABLES_SINFO'			, "Showing _START_ to _END_ of _TOTAL_ entries" );
+jr_define('DATATABLES_SINFOEMPTY'		, "Showing 0 to 0 of 0 entries" );
+jr_define('DATATABLES_SINFOFILTERED'	, "(filtered from _MAX_ total entries)" );
+jr_define('DATATABLES_SINFOPOSTFIX'		, "" );
+jr_define('DATATABLES_SINFOTHOUSANDS'	, "," );
+jr_define('DATATABLES_SLENGTHMENU'		, "Show _MENU_ entries" );
+jr_define('DATATABLES_SLOADINGRECORDS'	, "Loading..." );
+jr_define('DATATABLES_SPROCESSING'		, "Processing..." );
+jr_define('DATATABLES_SSEARCH'			, "Search:" );
+jr_define('DATATABLES_SZERORECORDS'		, "No matching records found" );
+jr_define('DATATABLES_SFIRST'			, "First" );
+jr_define('DATATABLES_SLAST'			, "Last" );
+jr_define('DATATABLES_SNEXT'			, "Next" );
+jr_define('DATATABLES_SPREVIOUS'		, "Previous" );
+jr_define('DATATABLES_SSORTASCENDING'	, ": activate to sort column ascending" );
+jr_define('DATATABLES_SSORTDESCENDING'	, ": activate to sort column descending" );
+
+jr_define('DATATABLES_SHOWHIDE'	, "Change columns" );
 
 jr_define('_JOMRES_BOOKING_INQUIRY_HAPPROVAL',"Approval");
 jr_define('_JOMRES_BOOKING_REJECT_INQUIRY',"Reject booking inquiry");
