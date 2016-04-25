605a1a7ff166f3149c9497724e6da2b5de497158
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2016-04-25 09:16:19 +0000

    Template path trigger changed to 00003 due to them needing to be set earlier.

--- jomres/core-minicomponents/j00003define_template_paths.class.php
+++ jomres/core-minicomponents/j00003define_template_paths.class.php
@@ -13,7 +13,7 @@
 defined( '_JOMRES_INITCHECK' ) or die( '' );
 // ################################################################
 
-class j00004define_template_paths
+class j00003define_template_paths
 	{
 	function __construct( $componentArgs )
 		{
