4030e926bbb8b07a1a31047315e4afc8f3427b0f
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2016-05-04 08:44:43 +0000

    Hardened new api functionality to provide a whitelist of filenames

--- jomres/api/routes.php
+++ jomres/api/routes.php
@@ -15,10 +15,15 @@ defined( '_JOMRES_INITCHECK' ) or die( '' );
 
 $request = Flight::request();
 $bang = explode("/" , $request->url);
-	
-if ( file_exists( __DIR__ .'../../'.'core-plugins'.DIRECTORY_SEPARATOR."api_feature_".$bang[1].DIRECTORY_SEPARATOR.$request->method .DIRECTORY_SEPARATOR. $bang[1].".php" ) )
-	require_once  (__DIR__ .'../../'.'core-plugins'.DIRECTORY_SEPARATOR."api_feature_".$bang[1].DIRECTORY_SEPARATOR.$request->method .DIRECTORY_SEPARATOR. $bang[1].".php");
-elseif ( file_exists( __DIR__ .'../../'.'remote_plugins'.DIRECTORY_SEPARATOR."api_feature_".$bang[1].DIRECTORY_SEPARATOR.$request->method .DIRECTORY_SEPARATOR. $bang[1].".php" ) )
-	require_once  (__DIR__ .'../../'.'remote_plugins.'.DIRECTORY_SEPARATOR."api_feature_".$bang[1].DIRECTORY_SEPARATOR.$request->method .DIRECTORY_SEPARATOR. $bang[1].".php");
+$filename = filter_var($$bang[1], FILTER_SANITIZE_STRING);
+
+$features_files = Flight::get("features_files");
+if (!in_array($filename.".php" , $features_files ))
+	Flight::halt(404, 'Request not allowed');
+
+if ( file_exists( __DIR__ .'../../'.'core-plugins'.DIRECTORY_SEPARATOR."api_feature_".$filename.DIRECTORY_SEPARATOR.$request->method .DIRECTORY_SEPARATOR. $filename.".php" ) )
+	require_once  (__DIR__ .'../../'.'core-plugins'.DIRECTORY_SEPARATOR."api_feature_".$filename.DIRECTORY_SEPARATOR.$request->method .DIRECTORY_SEPARATOR. $filename.".php");
+elseif ( file_exists( __DIR__ .'../../'.'remote_plugins'.DIRECTORY_SEPARATOR."api_feature_".$filename.DIRECTORY_SEPARATOR.$request->method .DIRECTORY_SEPARATOR. $filename.".php" ) )
+	require_once  (__DIR__ .'../../'.'remote_plugins.'.DIRECTORY_SEPARATOR."api_feature_".$filename.DIRECTORY_SEPARATOR.$request->method .DIRECTORY_SEPARATOR. $filename.".php");
 else
 	Flight::halt(404, 'Request unknown');
