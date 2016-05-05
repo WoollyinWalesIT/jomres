6d724ec05acb50a1e01b9bb68e1bc86c1c538c5a
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2016-05-05 09:36:41 +0000

    Finalise Jomres 9.8.-3

--- jomres_config.php
+++ jomres_config.php
@@ -16,7 +16,7 @@ defined( '_JOMRES_INITCHECK' ) or die( '' );
 // ################################################################
 
 $mrConfig = array (
-  'version' => '9.7.4',
+  'version' => '9.8.-3',
   'newTariffModels' => '2',
   'singlePersonSuppliment' => '0',
   'singlePersonSupplimentCost' => '10',
