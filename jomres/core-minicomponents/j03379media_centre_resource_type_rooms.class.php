cfbb1bf05b0788115d68ece35a5803d4bae9c8c8
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2016-05-06 09:00:51 +0000

    Gave some default values if they're not set.

--- jomres/core-minicomponents/j03379media_centre_resource_type_rooms.class.php
+++ jomres/core-minicomponents/j03379media_centre_resource_type_rooms.class.php
@@ -28,6 +28,10 @@ class j03379media_centre_resource_type_rooms
 		$defaultProperty = getDefaultProperty();
 		$mrConfig = getPropertySpecificSettings( $defaultProperty );
 		$this->ret_vals = array();
+		if (!isset($mrConfig[ 'singleRoomProperty' ]))
+			$mrConfig[ 'singleRoomProperty' ] ="0";
+		if (!isset($mrConfig[ 'is_real_estate_listing' ]))
+			$mrConfig[ 'is_real_estate_listing' ] ="0";
 		
 		if ($mrConfig[ 'singleRoomProperty' ] != "1" && $mrConfig['is_real_estate_listing'] != "1" )
 			{
