4ddfad64075dcc23d539cf4f72bfc786050368c6
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2016-05-06 13:17:32 +0000

    Improved new "framework" functionality ( that includes Jomres and if necessary CMS framework ) files.

--- framework.php
+++ framework.php
@@ -13,99 +13,89 @@
 defined( '_JOMRES_INITCHECK' ) or die( '' );
 // ################################################################
 
-class jomres_framework 
+if (file_exists(dirname(__FILE__).'/../jomres_root.php'))
+		require_once (dirname(__FILE__).'/../jomres_root.php');
+	else 
+		die();
+	
+if (defined('API_STARTED'))
 	{
-	public function __construct() 
+	//we need to include cms specific files
+	load_cms_environment();
+	}
+
+require_once(dirname(__FILE__).'/integration.php');
+
+//jomres framework
+load_jomres_environment();
+	
+function load_cms_environment()
+	{
+	if (file_exists(dirname(__FILE__).'/../configuration.php'))
 		{
+		define('JPATH_BASE', dirname(__FILE__) . '/../' );
+		require_once ( JPATH_BASE . 'includes/defines.php' );
+		require_once ( JPATH_BASE . 'includes/framework.php' );
+
+		/* Create the Application */
+		$app = JFactory::getApplication('site');
+		}
+	elseif (file_exists(dirname(__FILE__).'/../wp-config.php'))
+		{
+		define('WP_USE_THEMES', false);
+		/** Loads the WordPress Environment */
+		require ( dirname(__FILE__) . '/../wp-load.php');
 		}
 	
-	public function _init()
-		{
-		if (defined('API_STARTED'))
-			{
-			//we need to include cms specific files
-			$this->load_cms_enviornment();
-			}
-		
-		//jomres framework
-		$this->load_jomres_environment();
-		}
+	return true;
+	}
+
+function load_jomres_environment()
+	{
+	//site config object
+	$siteConfig = jomres_singleton_abstract::getInstance('jomres_config_site_singleton');
+	$jrConfig   = $siteConfig->get();
 	
-	private function load_cms_enviornment()
-		{
-		if ( this_cms_is_joomla() )
-			{
-			define('JPATH_BASE', '../../');
-			require_once ( JPATH_BASE .'/includes/defines.php' );
-			require_once ( JPATH_BASE .'/includes/framework.php' );
+	//language object - load default language file/context 
+	$jomreslang = jomres_singleton_abstract::getInstance( 'jomres_language' );
+	$jomreslang->get_language($jrConfig[ 'language_context' ]);
 
-			/* Create the Application */
-			$app = JFactory::getApplication('site');
-			}
-		elseif( this_cms_is_wordpress() )
-			{
-			define('WP_USE_THEMES', false);
-			/** Loads the WordPress Environment */
-			require ('./wp-load.php');
-			}
-		
-		return true;
-		}
+	//user object
+	$thisJRUser=jomres_singleton_abstract::getInstance('jr_user');
 	
-	private function load_jomres_environment()
+	//booking object
+	$tmpBookingHandler =jomres_singleton_abstract::getInstance('jomres_temp_booking_handler');
+
+	if (is_null($tmpBookingHandler->jomressession) || $tmpBookingHandler->jomressession == '')
 		{
-		if (file_exists(dirname(__FILE__).'/../../../../jomres_root.php'))
-			require_once (dirname(__FILE__).'/../../../../jomres_root.php');
-		else 
-			die();
-
-		require_once(dirname(__FILE__).'/../../../integration.php');
-		
-		//site config object
-		$siteConfig = jomres_singleton_abstract::getInstance('jomres_config_site_singleton');
-		$jrConfig   = $siteConfig->get();
-		
-		//language object - load default language file/context 
-		$jomreslang = jomres_singleton_abstract::getInstance( 'jomres_language' );
-		$jomreslang->get_language($jrConfig[ 'language_context' ]);
-
-		//user object
-		$thisJRUser=jomres_singleton_abstract::getInstance('jr_user');
-		
-		//booking object
-		$tmpBookingHandler =jomres_singleton_abstract::getInstance('jomres_temp_booking_handler');
-
-		if (is_null($tmpBookingHandler->jomressession) || $tmpBookingHandler->jomressession == '')
-			{
-			$tmpBookingHandler->initBookingSession(get_showtime('jomressession'));
-			$jomressession  = $tmpBookingHandler->getJomressession();
-			set_showtime( 'jomressession', $jomressession );
-			}
-
-		$property_uid = detect_property_uid();
-		
-		//load property type specific language file
-		if ($property_uid > 0)
-			{
-			$current_property_details =jomres_singleton_abstract::getInstance('basic_property_details');
-			$current_property_details->gather_data($property_uid);
-			$propertytype=$current_property_details->property_type;
-			$jomreslang->get_language($propertytype);
-			}
-
-		$customTextObj =jomres_singleton_abstract::getInstance('custom_text');
-
-		jr_import( 'jomres_currency_exchange_rates' );
-		$exchange_rates = new jomres_currency_exchange_rates( "GBP" );
-
-		$MiniComponents =jomres_singleton_abstract::getInstance('mcHandler');
-		$MiniComponents->triggerEvent('00003'); // 
-		init_javascript(); // 00004 is triggered in this function now.
-		$MiniComponents->triggerEvent('00005');
-		$componentArgs=array();
-		$MiniComponents->triggerEvent('99999',$componentArgs); // Javascript and CSS caching handling is needed 
-		$componentArgs=array();
-		
-		return true;
+		$tmpBookingHandler->initBookingSession(get_showtime('jomressession'));
+		$jomressession  = $tmpBookingHandler->getJomressession();
+		set_showtime( 'jomressession', $jomressession );
 		}
+
+	$property_uid = detect_property_uid();
+	
+	//load property type specific language file
+	if ($property_uid > 0)
+		{
+		$current_property_details =jomres_singleton_abstract::getInstance('basic_property_details');
+		$current_property_details->gather_data($property_uid);
+		$propertytype=$current_property_details->property_type;
+		$jomreslang->get_language($propertytype);
+		}
+
+	$customTextObj =jomres_singleton_abstract::getInstance('custom_text');
+
+	jr_import( 'jomres_currency_exchange_rates' );
+	$exchange_rates = new jomres_currency_exchange_rates( "GBP" );
+
+	$MiniComponents =jomres_singleton_abstract::getInstance('mcHandler');
+	$MiniComponents->triggerEvent('00003'); // 
+	init_javascript(); // 00004 is triggered in this function now.
+	$MiniComponents->triggerEvent('00005');
+	$componentArgs=array();
+	$MiniComponents->triggerEvent('99999',$componentArgs); // Javascript and CSS caching handling is needed 
+	$componentArgs=array();
+	
+	return true;
 	}
