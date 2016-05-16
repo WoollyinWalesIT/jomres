7b858049b11fbefb609a09d99f655c8d08e6ee8f
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2016-05-16 09:19:38 +0000

    Added custom methods & logging of replies to API calls.

--- api/index.php
+++ api/index.php
@@ -8,11 +8,7 @@
  * @copyright	2005-2016 Vince Wooll
  * Jomres (tm) PHP, CSS & Javascript files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly.
  **/
- 
-require 'vendor/autoload.php';
 
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
 
 // create a log channel
 define("TRANSACTION_ID" , time() );
@@ -25,31 +21,27 @@ if (!PRODUCTION)
 	ini_set('display_errors', '1');
 	}
 
-$logger = new Logger('api');
-$logger->pushHandler(new StreamHandler('../temp/monolog/jomres_api.log', Logger::DEBUG));
+require 'vendor/autoload.php';
+require 'classes/logging.class.php';
 
 if (isset($_POST['grant_type']) && isset($_POST['grant_type']) == 'client_credentials')
 	{
 	$client_id = filter_var($_POST['client_id'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH );
-	$logger->addInfo('Received a token request from '.$client_id);
+	//$logger->addInfo('Received a token request from '.$client_id);
+	logging::log_message('Received a token request from '.$client_id);
 	require_once __DIR__.'/oauth/token.php';
 	}
 else
 	{
 	$request = filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
-	$logger->addInfo(TRANSACTION_ID.' Received a token which sent '.$request);
+	//$logger->addInfo(TRANSACTION_ID.' Received a token which sent '.$request);
+	logging::log_message(' Received a token which sent '.$request);
 	require_once __DIR__.'/oauth/resource.php';
 	}
 
 if (!defined('_JOMRES_INITCHECK'))
-define('_JOMRES_INITCHECK', 1 );
-	
-define('API_STARTED', true );
+	define('_JOMRES_INITCHECK', 1 );
 
-$token = $server->getAccessTokenData(OAuth2\Request::createFromGlobals());
-
-
-$scopes = explode("," , $token['scope']);
 
 require 'classes/validate_scope.class.php';
 require 'classes/validate_property_access.class.php';
@@ -57,6 +49,11 @@ require 'classes/call.class.php';
 require 'classes/call_self.class.php';
 require 'classes/all_api_features.class.php';
 
+define('API_STARTED', true );
+
+
+$token = $server->getAccessTokenData(OAuth2\Request::createFromGlobals());
+$scopes = explode("," , $token['scope']);
 
 try
 	{
@@ -77,13 +74,14 @@ try
 
 	$api_features	= new all_api_features();
 	$features_files	= $api_features->get();
-
+	
 	Flight::set("token" , $token);
 	Flight::set("user_id" , $token['user_id']);
 	Flight::set("scopes" , explode("," , $token['scope']));
 	Flight::set("dbprefix" , JOMRES_API_DB_DB_PREFIX);
 	Flight::set("features_files" , $features_files);
 
+	require "custom_methods.php";
 	require "routes.php";
 
 	Flight::start();
@@ -97,7 +95,7 @@ catch(Exception $e)
 	}
 
 
-function validate_property_uid($property_uid = '')
+/* function validate_property_uid($property_uid = '')
 	{
 	if ($property_uid == 0)
 		return false;
@@ -106,4 +104,7 @@ function validate_property_uid($property_uid = '')
 	if (!in_array ($property_uid , $users_properties ) )
 		return false;
 	return true;
-	}
+	} */
+	
+
+
