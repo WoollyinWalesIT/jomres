fc47bc2d995d33283f0fb9049960e4971b5b97d7
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2016-05-19 08:50:10 +0000

    Improved API logging messages, ensured login failure is set to warning level.

--- jomres/api/classes/logging.class.php
+++ jomres/api/classes/logging.class.php
@@ -18,10 +18,17 @@ class logging
 		{
 		}
 	
-	public static function log_message ( $message )
+	public static function log_message ( $message , $level = "DEBUG" )
 		{
 		$logger = new Logger('api');
 		$logger->pushHandler(new StreamHandler('../temp/monolog/jomres_api.log', Logger::DEBUG));
-		$logger->addInfo(TRANSACTION_ID." ".$message);
+		if ($level == "DEBUG" )
+			$logger->addDebug(TRANSACTION_ID." ".$message);
+		elseif ($level == "INFO" )
+			$logger->addInfo(TRANSACTION_ID." ".$message);
+		elseif ($level == "WARNING") 
+			$logger->addWarning(TRANSACTION_ID." ".$message);
+		else
+			$logger->addError(TRANSACTION_ID." ".$message);
 		}
 	}
