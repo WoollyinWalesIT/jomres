357bc1b973fab8e38f9d9a8711b6c546160cc226
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2016-06-01 08:07:19 +0000

    Added shortcodes documentation to Help section.

---
+++ jomres/core-minicomponents/j10002zzz_about_shortcodes.class.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Core file
+ *
+ * @author Vince Wooll <sales@jomres.net>
+ * @version Jomres 9
+ * @package Jomres
+ * @copyright	2005-2016 Vince Wooll
+ * Jomres (tm) PHP, CSS & Javascript files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly.
+ **/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+class j10002zzz_about_shortcodes
+	{
+	function __construct()
+		{
+		// Must be in all minicomponents. Minicomponents with templates that can contain editable text should run $this->template_touch() else just return
+		$MiniComponents = jomres_singleton_abstract::getInstance( 'mcHandler' );
+		if ( $MiniComponents->template_touch )
+			{
+			$this->template_touchable = true;
+
+			return;
+			}
+		$htmlFuncs = jomres_singleton_abstract::getInstance( 'html_functions' );
+
+		$this->cpanelButton = $htmlFuncs->cpanelButton( JOMRES_SITEPAGE_URL_ADMIN . "&task=about_shortcodes", 'jrhouse.png', jr_gettext( "Shortcodes", 'Shortcodes', false, false ), "/".JOMRES_ROOT_DIRECTORY."/images/jomresimages/small/", jr_gettext( "_JOMRES_CUSTOMCODE_MENUCATEGORIES_HELP", '_JOMRES_CUSTOMCODE_MENUCATEGORIES_HELP', false, false ) );
+		}
+
+	function touch_template_language()
+		{
+		$output = array ();
+
+		$output[ ] = jr_gettext( "_JOMRES_CUSTOMCODE_SUPPORT_ABOUTJOMRES", '_JOMRES_CUSTOMCODE_SUPPORT_ABOUTJOMRES' );
+
+		foreach ( $output as $o )
+			{
+			echo $o;
+			echo "<br/>";
+			}
+		}
+
+	// This must be included in every Event/Mini-component
+	function getRetVals()
+		{
+		return $this->cpanelButton;
+		}
+	}
