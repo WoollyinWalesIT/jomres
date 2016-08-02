e707a361712263a48010cf538602453a426c1777
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2016-08-02 11:59:44 +0000

    Added booking balance output to emails.

--- jomres/libraries/jomres/classes/jomres_generic_booking_email.class.php
+++ jomres/libraries/jomres/classes/jomres_generic_booking_email.class.php
@@ -189,6 +189,7 @@ class jomres_generic_booking_email
 		$this->data[$contract_uid]['DEPARTURE'] = outputDate( $current_contract_details->contract[$contract_uid]['contractdeets']['departure'] );
 		$this->data[$contract_uid]['TOTAL'] = output_price( $current_contract_details->contract[$contract_uid]['contractdeets']['contract_total'] );
 		$this->data[$contract_uid]['DEPOSIT'] = output_price( $current_contract_details->contract[$contract_uid]['contractdeets']['deposit_required'] );
+		$this->data[$contract_uid]['BALANCE'] = output_price( $current_contract_details->contract[$contract_uid]['contractdeets']['contract_total'] - $current_contract_details->contract[$contract_uid]['contractdeets']['deposit_required'] );
 		$this->data[$contract_uid]['SPECIAL_REQUIREMENTS'] = jomres_decode($current_contract_details->contract[$contract_uid]['contractdeets']['special_reqs']);
 		
 		$this->data[$contract_uid]['ALLOCATION_NOTE'] = '';
