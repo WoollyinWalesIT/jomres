32e9857b3f9b67e74c00a40f2a12d634ddbf9731
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2016-09-09 07:12:14 +0000

    Modified log viewer so that new changes don't affect viewing of thrown errors.

--- jomres/core-minicomponents/j16000view_log_file.class.php
+++ jomres/core-minicomponents/j16000view_log_file.class.php
@@ -38,69 +38,76 @@ class j16000view_log_file
 		
 		if (is_file($jrConfig['log_path'].$filename))
 			{
-			$output=array();
-			$pageoutput=array();
-			$rows = array();
-			
-			$output['PAGETITLE'] = "Log channel";
-			
-			$raw_contents = file_get_contents($jrConfig['log_path'].$filename);
-			
-			$records = explode(":::::",$raw_contents);
-			foreach ($records as $record)
+			$bang = explode(".",$filename);
+			if( isset($bang[1]) && $bang[1] == "html" )
 				{
-				$content_array = explode("~~",$record);
-				if (isset($content_array[7]))
-					{
-					$r = array();
-					
-					$bang = explode("." ,$content_array[1]);
-					if (!isset($bang[1]))
-						$bang[1] = '';
-
-					switch ( $bang[1] )
-						{
-						case 'WARNING: ':
-							$r['ROWCLASS'] = 'warning';
-							break;
-						case 'ERROR: ':
-						case 'CRITICAL: ':
-						case 'ALERT: ':
-						case 'EMERGENCY: ':
-							$r['ROWCLASS'] = 'error';
-							break;
-						case 'INFO: ':
-						case 'NOTICE: ':
-							$r['ROWCLASS'] = 'info';
-							break;
-						default :
-							$r['ROWCLASS'] = '';
-							break;
-						}
-					
-					$r['DATETIME']	= $content_array[0];
-					$r['CHANNEL']	= $content_array[1];
-					$r['USERNAME']	= $content_array[2];
-					$r['SESSION']	= $content_array[3];
-					$r['URL']		= $content_array[4];
-					$r['MESSAGE']	= jomres_purify_html($content_array[5]);
-					$r['CONTEXT']	= $content_array[6];
-					$r['EXTRA']		= $content_array[7];
-					if ($session != "" && $r['SESSION'] == $session )
-						$rows[]=$r;
-					elseif ( $session == "")
-						$rows[]=$r;
-					}
+				echo nl2br(file_get_contents($jrConfig['log_path'].$filename));
 				}
+			else
+				{
+				$output=array();
+				$pageoutput=array();
+				$rows = array();
+				
+				$output['PAGETITLE'] = "Log channel";
+				
+				$raw_contents = file_get_contents($jrConfig['log_path'].$filename);
+				
+				$records = explode(":::::",$raw_contents);
+				foreach ($records as $record)
+					{
+					$content_array = explode("~~",$record);
+					if (isset($content_array[7]))
+						{
+						$r = array();
+						
+						$bang = explode("." ,$content_array[1]);
+						if (!isset($bang[1]))
+							$bang[1] = '';
 
-			$pageoutput[ ] = $output;
-			$tmpl          = new patTemplate();
-			$tmpl->setRoot( JOMRES_TEMPLATEPATH_ADMINISTRATOR );
-			$tmpl->readTemplatesFromInput( 'view_log_channel.html' );
-			$tmpl->addRows( 'pageoutput', $pageoutput );
-			$tmpl->addRows( 'rows', $rows );
-			$tmpl->displayParsedTemplate();
-			
+						switch ( $bang[1] )
+							{
+							case 'WARNING: ':
+								$r['ROWCLASS'] = 'warning';
+								break;
+							case 'ERROR: ':
+							case 'CRITICAL: ':
+							case 'ALERT: ':
+							case 'EMERGENCY: ':
+								$r['ROWCLASS'] = 'error';
+								break;
+							case 'INFO: ':
+							case 'NOTICE: ':
+								$r['ROWCLASS'] = 'info';
+								break;
+							default :
+								$r['ROWCLASS'] = '';
+								break;
+							}
+						
+						$r['DATETIME']	= $content_array[0];
+						$r['CHANNEL']	= $content_array[1];
+						$r['USERNAME']	= $content_array[2];
+						$r['SESSION']	= $content_array[3];
+						$r['URL']		= $content_array[4];
+						$r['MESSAGE']	= jomres_purify_html($content_array[5]);
+						$r['CONTEXT']	= $content_array[6];
+						$r['EXTRA']		= $content_array[7];
+						if ($session != "" && $r['SESSION'] == $session )
+							$rows[]=$r;
+						elseif ( $session == "")
+							$rows[]=$r;
+						}
+					}
+
+				$pageoutput[ ] = $output;
+				$tmpl          = new patTemplate();
+				$tmpl->setRoot( JOMRES_TEMPLATEPATH_ADMINISTRATOR );
+				$tmpl->readTemplatesFromInput( 'view_log_channel.html' );
+				$tmpl->addRows( 'pageoutput', $pageoutput );
+				$tmpl->addRows( 'rows', $rows );
+				$tmpl->displayParsedTemplate();
+				}
 			}
 		}
 
