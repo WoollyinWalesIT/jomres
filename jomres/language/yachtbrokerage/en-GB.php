667a755fbf30b1712f94ae55bd98b923d6e476ca
jomres <jomres@5edc5717-e69c-4d17-855f-ad4bef0d7826>
2016-11-01 10:44:18 +0000

    Added a longtext class and appropriate javascript so that tab contents can be limited to N characters.

--- jomres/language/yachtbrokerage/en-GB.php
+++ jomres/language/yachtbrokerage/en-GB.php
@@ -3121,3 +3121,6 @@ jr_define( 'INTEGRITY_CHECK_NOPROBLEMS', "Awesome! No problems were found." );
 
 
 jr_define('_JOMRES_PROPERTYTYPE_MARKER',"Google maps marker");
+
+jr_define( '_JOMRES_JAVASCRIPT_READMORE', 'Read more' );
+jr_define( '_JOMRES_JAVASCRIPT_READLESS', 'Read less' );
