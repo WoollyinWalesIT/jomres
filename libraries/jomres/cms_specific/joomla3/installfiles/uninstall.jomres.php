19e27735403905a56d942bc1b0cce1f53f68c256
Vince Wooll <sales@jomres.net>
2016-11-11 09:27:33 +0100

    Applied file version changes

--- libraries/jomres/cms_specific/joomla3/installfiles/uninstall.jomres.php
+++ libraries/jomres/cms_specific/joomla3/installfiles/uninstall.jomres.php
@@ -3,7 +3,7 @@
  * Core file
  *
  * @author Vince Wooll <sales@jomres.net>
- * @version Jomres 9.8.17
+ * @version Jomres 9.8.18
  * @package Jomres
  * @copyright	2005-2016 Vince Wooll
  * Jomres is currently available for use in all personal or commercial projects under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly.
