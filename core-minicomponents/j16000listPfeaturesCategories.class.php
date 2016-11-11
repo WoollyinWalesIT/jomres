2a8cc8fc1083d399806a2480efa13f4f76387b47
Vince Wooll <sales@jomres.net>
2016-11-11 09:51:30 +0100

    Updated several more files where the old version was still in situ.

--- core-minicomponents/j16000listPfeaturesCategories.class.php
+++ core-minicomponents/j16000listPfeaturesCategories.class.php
@@ -2,7 +2,7 @@
 /**
  * Core file
  * @author Vince Wooll <sales@jomres.net>
- * @version Jomres 4 
+ * @version Jomres 9.8.18
 * @package Jomres
 * @copyright	2005-2011 Vince Wooll
 * Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly, however all images, css and javascript which are copyright Vince Wooll are not GPL licensed and are not freely distributable. 
