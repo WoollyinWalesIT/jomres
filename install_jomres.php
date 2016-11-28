f15b99d5aef8953094f96cda57c0e9a00968f506
Vince Wooll <sales@jomres.net>
2016-11-28 10:14:03 +0100

    Installer updated to add API Oauth tables + the "system" user. This allows us to make use of the "call self" feature of Jomres which promotes code re-use.

--- install_jomres.php
+++ install_jomres.php
@@ -21,8 +21,8 @@ if (isset($_REQUEST[ 'autoupgrade' ])) {
 }
 
 // Set this to "development" to prevent the installer from setting the site to production and therefore deleting this file on run.
-define('PROD_DEV', 'production');
-// define ("PROD_DEV" , "development");
+//define('PROD_DEV', 'production');
+ define ("PROD_DEV" , "development");
 
 // Useful for testing installer changes, uncomment to prevent redirection after run
 // define ( "ERRORS_SHOWN_NO_REDIRECT" , 1 );
@@ -209,6 +209,7 @@ if ($folderChecksPassed && $functionChecksPassed) {
             if (ACTION == 'Install') { // Installing
                 //output_message ( "Creating Jomres tables if they don't already exist.");
                 createJomresTables();
+                add_api_tables();
                 //output_message ( "Inserting sample data");
                 insertSampleData();
                 //output_message ( "Importing configuration settings to database");
@@ -221,7 +222,7 @@ if ($folderChecksPassed && $functionChecksPassed) {
                 import_countries();
                 import_regions();
                 insert_pfeature_categories();
-
+                
                 require_once _JOMRES_DETECTED_CMS_SPECIFIC_FILES.'cms_specific_installation.php';
                 showCompletedText();
             } elseif (ACTION == 'Upgrade') { // Upgrading
@@ -592,8 +593,56 @@ function doTableUpdates()
 
     drop_orphan_line_items_table();
     drop_room_images_table();
-
+    add_api_tables();
     updateSiteSettings('update_time', time());
+    
+}
+
+
+function add_api_tables() {
+    $query = "CREATE TABLE IF NOT EXISTS  #__jomres_oauth_clients (
+        client_id VARCHAR(80) NOT NULL, 
+        client_secret VARCHAR(80), 
+        redirect_uri VARCHAR(2000) NOT NULL, 
+        grant_types VARCHAR(80), 
+        scope VARCHAR(1000), 
+        user_id VARCHAR(80), 
+        CONSTRAINT clients_client_id_pk 
+        PRIMARY KEY (client_id)
+        )";
+    doInsertSql($query,"");
+
+    $query = "CREATE TABLE IF NOT EXISTS  #__jomres_oauth_access_tokens (access_token VARCHAR(40) NOT NULL, client_id VARCHAR(80) NOT NULL, user_id VARCHAR(255), expires TIMESTAMP NOT NULL, scope VARCHAR(2000), CONSTRAINT access_token_pk PRIMARY KEY (access_token))";
+    doInsertSql($query,"");
+
+    $query = "CREATE TABLE IF NOT EXISTS  #__jomres_oauth_authorization_codes (authorization_code VARCHAR(40) NOT NULL, client_id VARCHAR(80) NOT NULL, user_id VARCHAR(255), redirect_uri VARCHAR(2000), expires TIMESTAMP NOT NULL, scope VARCHAR(2000), CONSTRAINT auth_code_pk PRIMARY KEY (authorization_code))";
+    doInsertSql($query,"");
+
+    $query = "CREATE TABLE IF NOT EXISTS  #__jomres_oauth_refresh_tokens (refresh_token VARCHAR(40) NOT NULL, client_id VARCHAR(80) NOT NULL, user_id VARCHAR(255), expires TIMESTAMP NOT NULL, scope VARCHAR(2000), CONSTRAINT refresh_token_pk PRIMARY KEY (refresh_token))";
+    doInsertSql($query,"");
+
+
+    $query = "CREATE TABLE IF NOT EXISTS  #__jomres_oauth_users (username VARCHAR(255) NOT NULL, password VARCHAR(2000), first_name VARCHAR(255), last_name VARCHAR(255), CONSTRAINT username_pk PRIMARY KEY (username))";
+    doInsertSql($query,"");
+
+
+    $query = "CREATE TABLE IF NOT EXISTS  #__jomres_oauth_scopes (scope TEXT, is_default BOOLEAN)";
+    doInsertSql($query,"");
+
+    $query = "CREATE TABLE IF NOT EXISTS  #__jomres_oauth_jwt (client_id VARCHAR(80) NOT NULL, subject VARCHAR(80), public_key VARCHAR(2000), CONSTRAINT jwt_client_id_pk PRIMARY KEY (client_id))";
+    doInsertSql($query,"");
+
+    // We need to see if there's a "system" user in the database, if there's not we'll create them. This is a once only action
+    $query = "SELECT client_id,scope FROM #__jomres_oauth_clients WHERE client_id = 'system' LIMIT 1";
+    $result = doSelectSql($query);
+    if (count($result)==0) {
+        $query = "INSERT INTO #__jomres_oauth_clients 
+        (`client_id`,`client_secret`,`redirect_uri`,`grant_types`,`scope`,`user_id`) 
+        VALUES 
+        ('system','".createNewAPIKey()."','',null,'*',99999999999999999999)";
+        if ( !doInsertSql( $query, jr_gettext( '_OAUTH_CREATED', '_OAUTH_CREATED', false ) ) )
+            throw new Exception("Unable to update oauth client details, mysql db failure");
+    }
 }
 
 function checkManagersUsernameColExists()
