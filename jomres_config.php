5373fc002918ad3a03731136153037c043240781
Vince Wooll <sales@jomres.net>
2016-12-21 11:41:18 +0100

    Updated changelog & version ( doesn't mean a new release, webhook testing requires 9.8.22 )

--- jomres_config.php
+++ jomres_config.php
@@ -15,7 +15,7 @@ defined('_JOMRES_INITCHECK') or die('');
 // ################################################################
 
 $mrConfig = array(
-  'version' => '9.8.21',
+  'version' => '9.8.22',
   'newTariffModels' => '2',
   'singlePersonSuppliment' => '0',
   'singlePersonSupplimentCost' => '10',
