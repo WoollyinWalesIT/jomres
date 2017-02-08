08643e5c4e6dc6d68a866c68a243fa6eb0573663
alibarthi <alibarthi@gmail.com>
2017-02-08 09:44:46 +0200

    Added support for overriding the set_booking_number function

--- libraries/jomres/functions/functions.php
+++ libraries/jomres/functions/functions.php
@@ -1119,27 +1119,6 @@ function get_remote_ip_number()
     return (int) $bang[ 0 ].'.'.(int) $bang[ 1 ].'.'.(int) $bang[ 2 ].'.'.(int) $bang[ 3 ];
 }
 
-function set_booking_number()
-{
-    $tmpBookingHandler = jomres_singleton_abstract::getInstance('jomres_temp_booking_handler');
-    if (!isset($tmpBookingHandler->tmpbooking[ 'booking_number' ]) || trim($tmpBookingHandler->tmpbooking[ 'booking_number' ]) == '' || $tmpBookingHandler->tmpbooking[ 'booking_number' ] == 0) {
-        $keeplooking = true;
-        while ($keeplooking):
-            $cartnumber = mt_rand(10000000, 99999999);
-        $query = "SELECT contract_uid FROM #__jomres_contracts WHERE tag = '".$cartnumber."' LIMIT 1";
-        $bklist = doSelectSql($query);
-        if (count($bklist) == 0) {
-            $keeplooking = false;
-        }
-        endwhile;
-        $tmpBookingHandler->tmpbooking[ 'booking_number' ] = $cartnumber;
-    } else {
-        $cartnumber = $tmpBookingHandler->tmpbooking[ 'booking_number' ];
-    }
-
-    return $cartnumber;
-}
-
 function get_booking_number()
 {
     $tmpBookingHandler = jomres_singleton_abstract::getInstance('jomres_temp_booking_handler');
---
+++ libraries/jomres/functions/set_booking_number.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Core file.
+ *
+ * @author Vince Wooll <sales@jomres.net>
+ *
+ * @version Jomres 9.8.25
+ *
+ * @copyright	2005-2017 Vince Wooll
+ * Jomres (tm) PHP, CSS & Javascript files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly
+ **/
+
+// ################################################################
+defined('_JOMRES_INITCHECK') or die('');
+// ################################################################
+
+// The purpose of this function is to allow us to override the booking number programatically.
+function set_booking_number()
+{
+    $tmpBookingHandler = jomres_singleton_abstract::getInstance('jomres_temp_booking_handler');
+    if (!isset($tmpBookingHandler->tmpbooking[ 'booking_number' ]) || trim($tmpBookingHandler->tmpbooking[ 'booking_number' ]) == '' || $tmpBookingHandler->tmpbooking[ 'booking_number' ] == 0) {
+        $keeplooking = true;
+        while ($keeplooking):
+            $cartnumber = mt_rand(10000000, 99999999);
+        $query = "SELECT contract_uid FROM #__jomres_contracts WHERE tag = '".$cartnumber."' LIMIT 1";
+        $bklist = doSelectSql($query);
+        if (count($bklist) == 0) {
+            $keeplooking = false;
+        }
+        endwhile;
+        $tmpBookingHandler->tmpbooking[ 'booking_number' ] = $cartnumber;
+    } else {
+        $cartnumber = $tmpBookingHandler->tmpbooking[ 'booking_number' ];
+    }
+
+    return $cartnumber;
+}
