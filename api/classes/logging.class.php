1974a1545a36636fb81c0387b26735b6cba68abe
Vince Wooll <sales@jomres.net>
2017-03-17 15:31:03 +0100

    Added backtracing to debug level logging.

--- api/classes/logging.class.php
+++ api/classes/logging.class.php
@@ -144,13 +144,20 @@ class logging
             return $record;
         });
 
-
-        $context = array( 'info_dump' => $further_info_dump);
+        $trace = '';
+        if ( $level == 'DEBUG') {
+            $backtrace = debug_backtrace();
+            $trace = "<br/> File ".$backtrace[1]['file']." Line ".$backtrace[1]['line']. " Function ".$backtrace[1]['function']."<br/> ";
+            $trace .= " File ".$backtrace[2]['file']." Line ".$backtrace[2]['line']. " Function ".$backtrace[2]['function']."<br/> ";
+            $trace .= " File ".$backtrace[3]['file']." Line ".$backtrace[3]['line']. " Function ".$backtrace[3]['function']."<br/> "; 
+        }
+        $context = array( 'info_dump' => $further_info_dump.$trace);
 
         switch ($level) {
             default:
             case 'DEBUG':
                 if ($jrConfig['development_production'] == 'development') {
+
                     $logger->addDebug($message, $context); // Detailed debug information.
                 }
                 break;
