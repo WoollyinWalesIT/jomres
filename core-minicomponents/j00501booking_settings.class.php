4afdf7982f7a8a19c9dbd77507325c89433d3644
alibarthi <alibarthi@gmail.com>
2017-03-23 10:01:26 +0200

    Moved the Cancellation Threashold setting in a more appropriate position in property configuration

--- core-minicomponents/j00501booking_settings.class.php
+++ core-minicomponents/j00501booking_settings.class.php
@@ -129,6 +129,13 @@ class j00501booking_settings
                 $configurationPanel->setmiddle($weekenddayDropdown);
                 $configurationPanel->setright(jr_gettext('_JOMRES_COM_WEEKENDDAYS_DESC', '_JOMRES_COM_WEEKENDDAYS_DESC', false));
                 $configurationPanel->insertSetting();
+				
+				$threashold_dropdown = jomresHTML::integerSelectList(0, 100, 1, 'cfg_cancellation_threashold', 'class="inputbox" size="1"', (int) $mrConfig[ 'cancellation_threashold' ]);
+
+				$configurationPanel->setleft(jr_gettext('_JOMRES_COM_A_ODDS_CANCELLATION_THREASHOLD', '_JOMRES_COM_A_ODDS_CANCELLATION_THREASHOLD', false));
+				$configurationPanel->setmiddle($threashold_dropdown);
+				$configurationPanel->setright(jr_gettext('_JOMRES_COM_A_ODDS_CANCELLATION_THREASHOLD_DESC', '_JOMRES_COM_A_ODDS_CANCELLATION_THREASHOLD_DESC', false));
+				$configurationPanel->insertSetting();
             }
 
             $configurationPanel->insertHeading(jr_gettext('_JOMRES_HFIXED_PERIODS', '_JOMRES_HFIXED_PERIODS', false));
@@ -201,13 +208,6 @@ class j00501booking_settings
                 }
             }
         }
-		
-		$threashold_dropdown = jomresHTML::integerSelectList(0, 100, 1, 'cfg_cancellation_threashold', 'class="inputbox" size="1"', (int) $mrConfig[ 'cancellation_threashold' ]);
-
-		$configurationPanel->setleft(jr_gettext('_JOMRES_COM_A_ODDS_CANCELLATION_THREASHOLD', '_JOMRES_COM_A_ODDS_CANCELLATION_THREASHOLD', false));
-		$configurationPanel->setmiddle($threashold_dropdown);
-		$configurationPanel->setright(jr_gettext('_JOMRES_COM_A_ODDS_CANCELLATION_THREASHOLD_DESC', '_JOMRES_COM_A_ODDS_CANCELLATION_THREASHOLD_DESC', false));
-		$configurationPanel->insertSetting();
 
         if ($mrConfig[ 'is_real_estate_listing' ] == 0) {
             if (!isset($jrConfig[ 'minimum_deposit_percentage' ])) {
