566ba5222ec66f1aa35dae818d263e98ca39e083
Vince Wooll <sales@jomres.net>
2017-04-11 10:23:32 +0200

    Jomres version declaration updated.

--- jomres_config.php
+++ jomres_config.php
@@ -15,7 +15,7 @@ defined('_JOMRES_INITCHECK') or die('');
 // ################################################################
 
 $mrConfig = array(
-  'version' => '9.8.29',
+  'version' => '9.8.30',
   'newTariffModels' => '2',
   'singlePersonSuppliment' => '0',
   'singlePersonSupplimentCost' => '10',
