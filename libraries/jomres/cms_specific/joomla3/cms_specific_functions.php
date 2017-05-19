df7e7a430fbcad85916dbc7d3ec51a97224e41da
alibarthi <alibarthi@gmail.com>
2017-05-19 14:50:22 +0300

    Temp fix for php 7.1 session ids which have max 256 chars. Jomtres sessions only allow 50 chars, so we`ll substr the first 50 chars of session id

--- libraries/jomres/cms_specific/joomla3/cms_specific_functions.php
+++ libraries/jomres/cms_specific/joomla3/cms_specific_functions.php
@@ -23,7 +23,12 @@ function jomres_cmsspecific_getsessionid()
 {
     $session = JFactory::getSession();
 
-    return $session_id = $session->getId();
+    $session_id = $session->getId();
+	
+	//TODO: find a better way for sessions in PHP 7.1 which have 256 max length
+	$session_id = substr($session_id, 0, 50);
+	
+	return $session_id;
 }
 
 // Date is sent in format YYYY/mm/dd, e.g. 2013/
