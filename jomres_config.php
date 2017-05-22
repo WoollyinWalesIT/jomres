e5d8388f032aabccd42735d87b87ae79459cf72c
Vince Wooll <sales@jomres.net>
2017-05-22 16:57:28 +0200

    Updated Jomres version number to allow downloading of updated, 9.9.1 plugins.

--- jomres_config.php
+++ jomres_config.php
@@ -15,7 +15,7 @@ defined('_JOMRES_INITCHECK') or die('');
 // ################################################################
 
 $mrConfig = array(
-  'version' => '9.9.0',
+  'version' => '9.9.1',
   'newTariffModels' => '2',
   'singlePersonSuppliment' => '0',
   'singlePersonSupplimentCost' => '10',
