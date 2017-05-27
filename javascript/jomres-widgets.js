d749a31eebcb96d8ac2cd6cacab9d76883503850
alibarthi <alibarthi@gmail.com>
2017-05-27 14:35:29 +0300

    .

--- javascript/jomres-widgets.js
+++ javascript/jomres-widgets.js
@@ -36,7 +36,7 @@ jomresJquery(document).ready(function() {
 
 	jomresJquery( '.jr-widget-toggle' ).on( "click", function() {
 		var icon = jomresJquery( this );
-		icon.toggleClass( 'fa-caret-up fa-caret-down' );
+		icon.toggleClass( 'fa-toggle-off fa-toggle-on' );
 		icon.closest( '.jr-widget-panel' ).find( '.jr-widget-content' ).toggle();
 	});
 	
