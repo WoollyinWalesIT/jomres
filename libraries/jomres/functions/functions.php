65ce09ab3c4652189cbb0ee39c0a6427507311be
alibarthi <alibarthi@gmail.com>
2017-05-31 09:20:13 +0300

    Solved a redirect problem when installing jomres

--- libraries/jomres/functions/functions.php
+++ libraries/jomres/functions/functions.php
@@ -2321,12 +2321,15 @@ function jomres_audit($query, $op = '')
 function jomresRedirect($url, $msg = '', $code = 302)
 {
     logging::log_message($msg, 'Core', 'INFO');
-    $MiniComponents = jomres_getSingleton('mcHandler');
-    $MiniComponents->triggerEvent('08000'); // Optional, post run items that *must* be run ( watchers ).
-	
-	//we have to save the session data every time we redirect
-	$tmpBookingHandler = jomres_singleton_abstract::getInstance('jomres_temp_booking_handler');
-	$tmpBookingHandler->close_jomres_session();
+    
+	if (!defined('AUTO_UPGRADE')) {
+		$MiniComponents = jomres_getSingleton('mcHandler');
+		$MiniComponents->triggerEvent('08000'); // Optional, post run items that *must* be run ( watchers ).
+
+		//we have to save the session data every time we redirect
+		$tmpBookingHandler = jomres_singleton_abstract::getInstance('jomres_temp_booking_handler');
+		$tmpBookingHandler->close_jomres_session();
+	}
 	
     if (strncmp('cli', PHP_SAPI, 3) !== 0) {
         if (headers_sent() !== true) {
