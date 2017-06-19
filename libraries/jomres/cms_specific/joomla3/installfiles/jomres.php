9f9c1313391219dd6842cb73eca136b2530a74e8
Vince Wooll <sales@jomres.net>
2017-06-19 09:04:26 +0200

    Files hashes and changelog updated.

--- libraries/jomres/cms_specific/joomla3/installfiles/jomres.php
+++ libraries/jomres/cms_specific/joomla3/installfiles/jomres.php
@@ -4,7 +4,7 @@
 *
 * @author Vince Wooll <sales@jomres.net>
 *
- * @version Jomres 9.9.4
+ * @version Jomres 9.9.5
  *
 * @copyright	2005-2013 Vince Wooll
 * Jomres is currently available for use in all personal or commercial projects under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly
