ed714f4af8761e7988b41142d7b3d579f003fba9
alibarthi <alibarthi@gmail.com>
2017-07-30 09:48:15 +0300

    Made FAQ work inline too, not just in a modal

--- core-minicomponents/j06000faq.class.php
+++ core-minicomponents/j06000faq.class.php
@@ -40,14 +40,12 @@ class j06000faq
         if ($thisJRUser->userIsManager) {
             if ($output_now) {
                 echo $kb->get_manager_faq();
-                exit;
             } else {
                 $this->retVals = $kb->get_manager_faq();
             }
         } else {
             if ($output_now) {
                 echo $kb->get_guest_faq();
-                exit;
             } else {
                 $this->retVals = $kb->get_guest_faq();
             }
