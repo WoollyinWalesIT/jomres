5bfc86ba979a5c5de86617aa9781ddd4c2dabbd3
Vince Wooll <vince@jomres.net>
2017-09-26 22:31:47 +0200

    Changelog and version updated

--- jomres_config.php
+++ jomres_config.php
@@ -15,7 +15,7 @@ defined('_JOMRES_INITCHECK') or die('');
 // ################################################################
 
 $mrConfig = array(
-  'version' => '9.9.11',
+  'version' => '9.9.12',
   'newTariffModels' => '2',
   'singlePersonSuppliment' => '0',
   'singlePersonSupplimentCost' => '10',
