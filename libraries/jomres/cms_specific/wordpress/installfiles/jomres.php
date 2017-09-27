128299534284bae4d1965a8100a0b04c8c6800b5
Vince Wooll <vince@jomres.net>
2017-09-27 09:49:17 +0200

    File versions and hash updated.

--- libraries/jomres/cms_specific/wordpress/installfiles/jomres.php
+++ libraries/jomres/cms_specific/wordpress/installfiles/jomres.php
@@ -4,13 +4,13 @@ Plugin Name: Jomres
 Plugin URI: http://www.jomres.net
 Description: The complete hotel booking system for Wordpress!
 Author: Vince Wooll
-Version: 9.9.11
+Version: 9.9.12
 Author URI: http://www.jomres.net/
 */
 defined('WPINC') or die;
 
 if (!defined('JOMRES_WP_PLUGIN_VERSION')) {
-    define('JOMRES_WP_PLUGIN_VERSION', '9.9.11');
+    define('JOMRES_WP_PLUGIN_VERSION', '9.9.12');
 }
 
 if (!class_exists('WP_Jomres')) {
