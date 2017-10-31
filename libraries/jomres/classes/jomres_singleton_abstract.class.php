aeb9eb8d1f729217e4871ce0898ad44151fdee8c
Vince Wooll <sales@jomres.net>
2017-10-31 10:07:18 +0100

    Tweaked a backtrace to provide more information.

--- libraries/jomres/classes/jomres_singleton_abstract.class.php
+++ libraries/jomres/classes/jomres_singleton_abstract.class.php
@@ -54,9 +54,10 @@ class jomres_singleton_abstract
 		echo 'Error, class '.$class." doesn't exist.";
         $trace = '';
             $backtrace = debug_backtrace();
-            $trace = "<br/> File ".$backtrace[1]['file']." Line ".$backtrace[1]['line']. " Function ".$backtrace[1]['function']."<br/> ";
-            $trace .= " File ".$backtrace[2]['file']." Line ".$backtrace[2]['line']. " Function ".$backtrace[2]['function']."<br/> ";
-            $trace .= " File ".$backtrace[3]['file']." Line ".$backtrace[3]['line']. " Function ".$backtrace[3]['function']."<br/> "; 
+		$trace = "<br/> File ".$backtrace[0]['file']." Line ".$backtrace[0]['line']. " Function ".$backtrace[0]['function']."<br/> ";
+        $trace .= "File ".$backtrace[1]['file']." Line ".$backtrace[1]['line']. " Function ".$backtrace[1]['function']."<br/> ";
+        $trace .= " File ".$backtrace[2]['file']." Line ".$backtrace[2]['line']. " Function ".$backtrace[2]['function']."<br/> ";
+        $trace .= " File ".$backtrace[3]['file']." Line ".$backtrace[3]['line']. " Function ".$backtrace[3]['function']."<br/> "; 
         echo $trace;
 		exit;
     }
