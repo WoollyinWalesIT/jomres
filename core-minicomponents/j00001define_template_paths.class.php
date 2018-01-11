8a1e462d87da517e4626d405f58cd1feffdda8ad
alibarthi <alibarthi@gmail.com>
2018-01-11 10:41:23 +0200

    Moved templates dir to assets

--- core-minicomponents/j00001define_template_paths.class.php
+++ core-minicomponents/j00001define_template_paths.class.php
@@ -34,34 +34,29 @@ class j00001define_template_paths
 
         if (!defined('JOMRES_TEMPLATEPATH_FRONTEND')) {
             if (!using_bootstrap()) {
-                define('JOMRES_TEMPLATEPATH_FRONTEND', JOMRESPATH_BASE.JRDS.'templates'.JRDS.'jquery_ui'.JRDS.'frontend');
+                define('JOMRES_TEMPLATEPATH_FRONTEND', JOMRESPATH_BASE.JRDS.'assets'.JRDS.'templates'.JRDS.'jquery_ui'.JRDS.'frontend');
             } else {
-                define('JOMRES_TEMPLATEPATH_FRONTEND', JOMRESPATH_BASE.JRDS.'templates'.JRDS.'bootstrap'.$jrConfig[ 'bootstrap_version' ].JRDS.'frontend');
+                define('JOMRES_TEMPLATEPATH_FRONTEND', JOMRESPATH_BASE.JRDS.'assets'.JRDS.'templates'.JRDS.'bootstrap'.$jrConfig[ 'bootstrap_version' ].JRDS.'frontend');
             }
         }
 
         if (!defined('JOMRES_TEMPLATEPATH_BACKEND')) {
             if (!using_bootstrap()) {
-                define('JOMRES_TEMPLATEPATH_BACKEND', JOMRESPATH_BASE.JRDS.'templates'.JRDS.'jquery_ui'.JRDS.'backend');
+                define('JOMRES_TEMPLATEPATH_BACKEND', JOMRESPATH_BASE.JRDS.'assets'.JRDS.'templates'.JRDS.'jquery_ui'.JRDS.'backend');
             } else {
-                define('JOMRES_TEMPLATEPATH_BACKEND', JOMRESPATH_BASE.JRDS.'templates'.JRDS.'bootstrap'.$jrConfig[ 'bootstrap_version' ].JRDS.'backend');
+                define('JOMRES_TEMPLATEPATH_BACKEND', JOMRESPATH_BASE.JRDS.'assets'.JRDS.'templates'.JRDS.'bootstrap'.$jrConfig[ 'bootstrap_version' ].JRDS.'backend');
             }
         }
 
         if (!defined('JOMRES_TEMPLATEPATH_ADMINISTRATOR')) {
             if (!using_bootstrap()) {
-                define('JOMRES_TEMPLATEPATH_ADMINISTRATOR', JOMRESPATH_BASE.JRDS.'templates'.JRDS.'jquery_ui'.JRDS.'administrator');
+                define('JOMRES_TEMPLATEPATH_ADMINISTRATOR', JOMRESPATH_BASE.JRDS.'assets'.JRDS.'templates'.JRDS.'jquery_ui'.JRDS.'administrator');
             } else {
-                define('JOMRES_TEMPLATEPATH_ADMINISTRATOR', JOMRESPATH_BASE.JRDS.'templates'.JRDS.'bootstrap'.JRDS.'administrator');
+                define('JOMRES_TEMPLATEPATH_ADMINISTRATOR', JOMRESPATH_BASE.JRDS.'assets'.JRDS.'templates'.JRDS.'bootstrap'.JRDS.'administrator');
             }
         }
     }
 
-/**
- * Must be included in every mini-component.
- #
- * Returns any settings the the mini-component wants to send back to the calling script. In addition to being returned to the calling script they are put into an array in the mcHandler object as eg. $mcHandler->miniComponentData[$ePoint][$eName]
- */
     // This must be included in every Event/Mini-component
     public function getRetVals()
     {
