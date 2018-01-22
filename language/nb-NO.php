29bd6559f96c9ffac9f2cd5c9631c2b2d07c1e31
Vince Wooll <sales@jomres.net>
2018-01-22 19:13:39 +0100

    File hashes and versions updated.

--- language/nb-NO.php
+++ language/nb-NO.php
@@ -1,12 +1,15 @@
 <?php
 /**
-* Core file
-* @author Vince Wooll <sales@jomres.net>
-* @version Jomres 7
-* @package Jomres
-* @copyright	2005-2013 Vince Wooll
-* Jomres is currently available for use in all personal or commercial projects under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly. 
-**/
+ * Core file.
+ *
+ * @author Vince Wooll <sales@jomres.net>
+ *
+ * @version Jomres 9.9.18
+ *
+ * @copyright	2005-2018 Vince Wooll
+ * Jomres (tm) PHP, CSS & Javascript files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly
+ **/
+ 
 ##################################################################
 defined( '_JOMRES_INITCHECK' ) or die( '' );
 ##################################################################
