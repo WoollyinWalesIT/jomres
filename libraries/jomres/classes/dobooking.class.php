ef1f19634ae386ab3ef3998ed840b35940f62359
alibarthi <alibarthi@gmail.com>
2018-01-26 11:33:25 +0200

    Fixed a notice

--- libraries/jomres/classes/dobooking.class.php
+++ libraries/jomres/classes/dobooking.class.php
@@ -5029,7 +5029,7 @@ class dobooking
 									if ($this->cfg_perPersonPerNight == '1') {
 										$pernight = jr_gettext('_JOMRES_FRONT_TARIFFS_PPPN', '_JOMRES_FRONT_TARIFFS_PPPN', false);
 									} else {
-										$pernight = jr_gettext('_JOMRES_FRONT_TARIFFS_PN', _JOMRES_FRONT_TARIFFS_PN, false);
+										$pernight = jr_gettext('_JOMRES_FRONT_TARIFFS_PN', '_JOMRES_FRONT_TARIFFS_PN', false);
 									}
 									//echo ';jomresJquery.jGrowl(\'' . jr_gettext( '_JOMRES_STAYFORAMINIMUMOF', _JOMRES_STAYFORAMINIMUMOF, false ) . " " . $key . " " . jr_gettext( '_JOMRES_NIGHTSFOR', _JOMRES_NIGHTSFOR, false ) . " " . output_price( $val ) . $pernight . '\', { life: 20000 });';
 								}
