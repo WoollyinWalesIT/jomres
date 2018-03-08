f5eab4d7d7cbe9d12a5ea486adce898daa1df349
alibarthi <alibarthi@gmail.com>
2018-03-08 17:47:38 +0200

    Added a sample update file

---
+++ database/updates/9.10.0/sample.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Core file.
+ *
+ * @author Vince Wooll <sales@jomres.net>
+ *
+ * @version Jomres 9.10.0-alpha1
+ *
+ * @copyright	2005-2017 Vince Wooll
+ * Jomres (tm) PHP, CSS & Javascript files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly
+ **/
+
+// ################################################################
+defined('_JOMRES_INITCHECK') or die('');
+// ################################################################
+
+//this is a sample update files that does nothing
+return;
