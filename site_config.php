9ab57725a39bc8c0838b58d858106585c2128c74
alibarthi <alibarthi@gmail.com>
2018-03-14 11:18:53 +0200

    Updated version in site config

--- site_config.php
+++ site_config.php
@@ -12,7 +12,7 @@
 defined('_JOMRES_INITCHECK') or die('');
 
 $jrConfig = array(
-  'version' => '9.10.0',
+  'version' => '9.10.1',
   'defaultCrate' => '1',
   'property_list_limit' => '9',
   'useGlobalCurrency' => '1',
