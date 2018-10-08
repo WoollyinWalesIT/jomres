4f359ffcd600ec04272f681e42901afd51e2be03
micha <micha@konzeptplus.ch>
2018-10-08 10:39:57 +0200

    Version changed

--- language/de-CH.php
+++ language/de-CH.php
@@ -4,7 +4,7 @@
  *
  * @author Vince Wooll <sales@jomres.net>
  *
- * @version Jomres 9.12.0
+ * @version Jomres 9.13.0
  *
  * @copyright	2005-2018 Vince Wooll
  * Jomres is currently available for use in all personal or commercial projects under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly
