e9766b666f265fc4efb62747d8038721030885a1
Vince Wooll <sales@jomres.net>
2018-10-31 11:43:28 +0100

    File versions updated.

--- api/classes/logging.class.php
+++ api/classes/logging.class.php
@@ -6,7 +6,7 @@
  *
  * @author Vince Wooll <sales@jomres.net>
  *
- * @version Jomres 9.13.0
+ * @version Jomres 9.14.0
  *
  * @copyright	2005-2018 Vince Wooll
  * Jomres (tm) PHP, CSS & Javascript files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly
