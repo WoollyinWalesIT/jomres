85bc1fc228d731b3608aeefa3749ea726e1210d8
Vince Wooll <sales@jomres.net>
2019-02-13 11:28:30 +0100

    Improved property bookings list by including custom invoice numbers where appropriate in the invoice id column. Closes #381

--- core-minicomponents/j06001list_bookings_ajax.class.php
+++ core-minicomponents/j06001list_bookings_ajax.class.php
@@ -51,8 +51,8 @@ class j06001list_bookings_ajax
 
 		$rows = array();
 
-		//set the table coulmns, in the exact orcer in which they`re displayed in the table
-		$aColumns = array('a.contract_uid', 'a.contract_uid', 'a.tag', 'a.property_uid', 'a.arrival', 'a.departure', 'b.enc_firstname', 'b.enc_surname', 'b.enc_tel_landline', 'b.enc_tel_mobile', 'b.enc_email', 'a.contract_total', 'a.deposit_required', 'a.deposit_paid', 'a.special_reqs', 'a.invoice_uid', 'a.timestamp', 'a.last_changed', 'a.approved', 'a.username');
+		//set the table columns, in the exact orcer in which they`re displayed in the table
+		$aColumns = array('a.contract_uid', 'a.contract_uid', 'a.tag', 'a.property_uid', 'a.arrival', 'a.departure', 'b.enc_firstname', 'b.enc_surname', 'b.enc_tel_landline', 'b.enc_tel_mobile', 'b.enc_email', 'a.contract_total', 'a.deposit_required', 'a.deposit_paid', 'a.special_reqs', 'a.invoice_uid', 'a.timestamp', 'a.last_changed', 'a.approved', 'a.username', 'c.invoice_number');
 
 		//set columns count
 		$n = count($aColumns);
@@ -177,9 +177,11 @@ class j06001list_bookings_ajax
 						b.enc_surname, 
 						b.enc_tel_landline, 
 						b.enc_tel_mobile, 
-						b.enc_email
+						b.enc_email,
+						c.invoice_number
 					FROM #__jomres_contracts a 
-						LEFT JOIN #__jomres_guests b ON a.guest_uid = b.guests_uid '
+						LEFT JOIN #__jomres_guests b ON a.guest_uid = b.guests_uid 
+						LEFT JOIN #__jomresportal_invoices c ON a.invoice_uid  = c.id  '
 					.$clause
 					.' '.$sWhere
 					.' '.$sOrder
@@ -341,7 +343,12 @@ class j06001list_bookings_ajax
 			}
 
 			$r[] = jomres_decode($p->special_reqs);
-			$r[] = $p->invoice_uid;
+			if ($p->invoice_number != '' ) {
+				$r[] = $p->invoice_number;
+			} else {
+				$r[] = $p->invoice_uid;
+			}
+			
 			$r[] = $p->timestamp;
 			$r[] = $p->last_changed;
 
