37cfc27cfbce064d84f90174f68dc9b28978fa8f
Vince Wooll <sales@jomres.net>
2019-10-23 13:14:35 +0200

    Updated PUT method handling to ensure that it doesn't throw notices when form data is empty.

--- api/put_method_handling.php
+++ api/put_method_handling.php
@@ -16,63 +16,66 @@
 
 if ($_SERVER['REQUEST_METHOD']==="PUT") {
 	$raw_data = file_get_contents('php://input');
-	$boundary = substr($raw_data, 0, strpos($raw_data, "\r\n"));
+	if ($raw_data != '' ) {
+		$boundary = substr($raw_data, 0, strpos($raw_data, "\r\n"));
 
-	// Fetch each part
-	$parts = array_slice(explode($boundary, $raw_data), 1);
-	$data = array();
+		// Fetch each part
+		$parts = array_slice(explode($boundary, $raw_data), 1);
+		$data = array();
 
-	if (!empty($parts)) {
-		foreach ($parts as $part) {
-			// If this is the last part, break
-			if ($part == "--\r\n") break; 
+		if (!empty($parts)) {
+			foreach ($parts as $part) {
+				// If this is the last part, break
+				if ($part == "--\r\n") break; 
 
-			// Separate content from headers
-			$part = ltrim($part, "\r\n");
-			list($raw_headers, $body) = explode("\r\n\r\n", $part, 2);
+				// Separate content from headers
+				$part = ltrim($part, "\r\n");
+				list($raw_headers, $body) = explode("\r\n\r\n", $part, 2);
 
-			// Parse the headers list
-			$raw_headers = explode("\r\n", $raw_headers);
-			$headers = array();
-			foreach ($raw_headers as $header) {
-				list($name, $value) = explode(':', $header);
-				$headers[strtolower($name)] = ltrim($value, ' '); 
-			} 
-
-			// Parse the Content-Disposition to get the field name, etc.
-			if (isset($headers['content-disposition'])) {
-				$filename = null;
-				preg_match(
-					'/^(.+); *name="([^"]+)"(; *filename="([^"]+)")?/', 
-					$headers['content-disposition'], 
-					$matches
-				);
-				list(, $type, $name) = $matches;
-				isset($matches[4]) and $filename = $matches[4]; 
-
-				// handle your fields here
-				switch ($name) {
-					// this is a file upload
-					case 'userfile':
-						 //file_put_contents($filename, $body);
-						 break;
-
-					// default for all other files is to populate $data
-					default: 
-						parse_str ($name, $test);
-						$var_name = key($test);
- 						if (is_array($test[$var_name])) { // If it's an array itself then we will populate the $data[$var_name] with the array contents, which is consistent with how $_POSTed arrays are handled
-							$index =  key($test[$var_name]);
-							$data[$var_name][$index] = substr($body, 0, strlen($body) - 2);
-						} else {
-							$data[$name] = substr($body, 0, strlen($body) - 2);
-						}
-						 break;
+				// Parse the headers list
+				$raw_headers = explode("\r\n", $raw_headers);
+				$headers = array();
+				foreach ($raw_headers as $header) {
+					list($name, $value) = explode(':', $header);
+					$headers[strtolower($name)] = ltrim($value, ' '); 
 				} 
+
+				// Parse the Content-Disposition to get the field name, etc.
+				if (isset($headers['content-disposition'])) {
+					$filename = null;
+					preg_match(
+						'/^(.+); *name="([^"]+)"(; *filename="([^"]+)")?/', 
+						$headers['content-disposition'], 
+						$matches
+					);
+					list(, $type, $name) = $matches;
+					isset($matches[4]) and $filename = $matches[4]; 
+
+					// handle your fields here
+					switch ($name) {
+						// this is a file upload
+						case 'userfile':
+							 //file_put_contents($filename, $body);
+							 break;
+
+						// default for all other files is to populate $data
+						default: 
+							parse_str ($name, $test);
+							$var_name = key($test);
+							if (is_array($test[$var_name])) { // If it's an array itself then we will populate the $data[$var_name] with the array contents, which is consistent with how $_POSTed arrays are handled
+								$index =  key($test[$var_name]);
+								$data[$var_name][$index] = substr($body, 0, strlen($body) - 2);
+							} else {
+								$data[$name] = substr($body, 0, strlen($body) - 2);
+							}
+							 break;
+					} 
+				}
 			}
 		}
+	} else {
+		$data = array();
 	}
-
 	$GLOBALS['PUT'] = $data;
 	
 }
