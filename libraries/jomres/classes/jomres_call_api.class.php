54eb34379d646ba4d1a6a9030e3bbbac0affca23
Vince Wooll <sales@jomres.net>
2019-11-07 09:53:49 +0100

    Refactored the internal call api class.

--- libraries/jomres/classes/jomres_call_api.class.php
+++ libraries/jomres/classes/jomres_call_api.class.php
@@ -21,32 +21,37 @@ class jomres_call_api
 	private $token;
 	public $server;
 
-	public function __construct()
+	public function __construct( $user_id = 'system' )
 	{
+		if ($user_id == 'system' ) {
+			$this->user = new stdClass();
+			$this->user->accesslevel = 101;
+			$this->user->username = 'system';
+			$this->user->userid = 99999999999999;
+		} else {
+			$thisJRUser = jomres_singleton_abstract::getInstance('jr_user');
+			$this->user->accesslevel = $thisJRUser->accesslevel;
+			$this->user->username = $thisJRUser->username;
+			$this->user->userid = $thisJRUser->id;
+		}
+		
 		$this->init();
+		
 	}
 
 	private function init() {
 		// If the user is registered, we'll create a new API client and secret for that user if needed. That way, any calls done to the API will be traceable back to that user.
-		$thisJRUser = jomres_singleton_abstract::getInstance('jr_user');
-		if ( $thisJRUser->accesslevel > 1 ) { // The user's registered or greater
+		if ( $this->user->accesslevel > 1 ) { // The user's registered or greater
 			$auth_deets = $this->init_manager();
 			$client_id = $auth_deets['client_id'];
 			$client_secret = $auth_deets['client_secret'];
-		} else {
-			$query = "SELECT client_id,client_secret FROM #__jomres_oauth_clients WHERE client_id = 'system' LIMIT 1";
-			$result = doSelectSql($query, 2);
-			if (count($result) == 2) {
-				$client_id = $result['client_id'];
-				$client_secret = $result['client_secret'];
-			}
 		}
 
 		$MiniComponents = jomres_getSingleton('mcHandler');
 		if (isset($MiniComponents->registeredClasses['06005']['oauth']) && isset($client_secret) && trim($client_secret) != '' ) {
 			$this->server = get_showtime('live_site').'/'.JOMRES_ROOT_DIRECTORY.'/api/';
 			$data = array('grant_type' => 'client_credentials', 'client_id' => $client_id, 'client_secret' => $client_secret);
-			$token_request = $this->query_api('POST', '/', $data);
+			$token_request = $this->query_api('POST', '', $data);
 			$response = json_decode($token_request['response']);
 
 			if (isset($response->access_token)) {
@@ -54,31 +59,32 @@ class jomres_call_api
 			} else {
 				throw new Exception($response->error_description);
 			}
-		}
+		}  else {
+				throw new Exception("OAuth plugin not installed. At one time it was a separate plugin but now it is in Jomres Core. Is this a very old installation of Jomres?");
+			}
 	}
-
+	
 	private function init_manager() {
 		// We need to see if there's a user in the database, if there's not we'll create them. 
-		$thisJRUser = jomres_singleton_abstract::getInstance('jr_user');
-		$query = "SELECT client_id,scope FROM #__jomres_oauth_clients WHERE client_id = '".$thisJRUser->username."' LIMIT 1";
+		$query = "SELECT client_id,scope FROM #__jomres_oauth_clients WHERE client_id = '".$this->user->username."' LIMIT 1";
 		$result = doSelectSql($query);
 		if (empty($result)) {
 			$query = "INSERT INTO #__jomres_oauth_clients 
 				(`client_id`,`client_secret`,`redirect_uri`,`grant_types`,`scope`,`user_id`) 
 				VALUES 
-				('".$thisJRUser->username."','".createNewAPIKey()."','',null,'*',".$thisJRUser->id.")";
+				('".$this->user->username."','".createNewAPIKey()."','',null,'*',".$this->user->userid.")";
 			if (!doInsertSql($query, jr_gettext('_OAUTH_CREATED', '_OAUTH_CREATED', false))) {
 			   throw new Exception('Unable to update oauth client details, mysql db failure');
 			}
 		}
-		$query = "SELECT client_secret FROM #__jomres_oauth_clients WHERE client_id = '".$thisJRUser->username."' LIMIT 1";
+		$query = "SELECT client_secret FROM #__jomres_oauth_clients WHERE client_id = '".$this->user->username."' LIMIT 1";
 		$client_secret = doSelectSql($query, 1);
-		return array ( "client_id" => $thisJRUser->username ,"client_secret" => $client_secret );
+		return array ( "client_id" => $this->user->username ,"client_secret" => $client_secret );
 	}
 
-	public function send_request($method = '', $request = '', $data = array()) {
+	public function send_request($method = '', $endpoint = '', $data = array()) {
 		if ($this->token != '') {
-			$response = $this->query_api($method, $request, $data);
+			$response = $this->query_api($method, $endpoint, $data);
 			if ($response['response_code'] == '200' || $response['response_code'] == '204') {
 				return json_decode($response['response']);
 			} else {
@@ -89,10 +95,14 @@ class jomres_call_api
 		}
 	}
 
-	private function query_api($method = 'GET', $request = '', $data = array())
+	private function query_api($method = 'GET', $endpoint = '', $data = array())
 	{
-		$ch = curl_init($this->server.$request);
-
+		$ch = curl_init($this->server.$endpoint);
+		
+		curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 30); //timeout after 30 seconds
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		
 		switch ($method) {
 			//########################################################################################
 			case 'POST':
@@ -117,9 +127,7 @@ class jomres_call_api
 				));
 		}
 
-		curl_setopt($ch, CURLINFO_HEADER_OUT, true);
-		curl_setopt($ch, CURLOPT_TIMEOUT, 30); //timeout after 30 seconds
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
 		$result = curl_exec($ch);
 		$status = curl_getinfo($ch);
 		$response_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
