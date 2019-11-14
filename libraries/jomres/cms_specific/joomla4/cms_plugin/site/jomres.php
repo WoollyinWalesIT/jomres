b8d7b51513f69f97096fe2423cce73bdf3588b9f
Vince Wooll <sales@jomres.net>
2019-11-14 20:07:15 +0100

    Added ability to add menu options based on searching by property feature. Closes #66

--- libraries/jomres/cms_specific/joomla4/cms_plugin/site/jomres.php
+++ libraries/jomres/cms_specific/joomla4/cms_plugin/site/jomres.php
@@ -10,7 +10,7 @@
 * Jomres is currently available for use in all personal or commercial projects under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly
 **/
 defined('_JEXEC') or die('Direct Access to this location is not allowed.');
-	
+
 	/**
 	 *
 	 * @package Jomres\Core\CMS_Specific
@@ -18,52 +18,57 @@ defined('_JEXEC') or die('Direct Access to this location is not allowed.');
 	 */
 
 if (!defined('_JOMRES_INITCHECK')) {
-	define('_JOMRES_INITCHECK', 1);
+    define('_JOMRES_INITCHECK', 1);
 }
 
 if (isset($_REQUEST ['layout'])) {
 
-	switch ($_REQUEST ['layout']) {
-		case 'countries':
+    switch ($_REQUEST ['layout']) {
+        case 'countries':
 			$_REQUEST ['task'] = 'search';
 			$_REQUEST ['calledByModule'] = 'mod_jomsearch_m0';
-			$_REQUEST ['country'] = $_REQUEST ['selected_country'];
-			break;
-		case 'regions':
+            $_REQUEST ['country'] = $_REQUEST ['selected_country'];
+            break;
+        case 'regions':
 			$_REQUEST ['task'] = 'search';
 			$_REQUEST ['calledByModule'] = 'mod_jomsearch_m0';
-			$_REQUEST ['region'] = (int)$_REQUEST ['selected_region'];
-			break;
-		case 'towns':
+            $_REQUEST ['region'] = (int)$_REQUEST ['selected_region'];
+            break;
+        case 'towns':
 			$_REQUEST ['task'] = 'search';
 			$_REQUEST ['calledByModule'] = 'mod_jomsearch_m0';
-			$_REQUEST ['town'] = $_REQUEST ['selected_town'];
-			break;
-		case 'propertytypes':
+            $_REQUEST ['town'] = $_REQUEST ['selected_town'];
+            break;
+        case 'propertytypes':
 			$_REQUEST ['task'] = 'search';
 			$_REQUEST ['calledByModule'] = 'mod_jomsearch_m0';
-			$_REQUEST ['ptype'] = (int)$_REQUEST ['selected_ptype'];
-			break;
+            $_REQUEST ['ptype'] = (int)$_REQUEST ['selected_ptype'];
+            break;
 		case 'propertydetails':
 			$_REQUEST ['task'] = 'viewproperty';
-			$_REQUEST ['property_uid'] = $_REQUEST ['selected_property'];
-			break;
+            $_REQUEST ['property_uid'] = $_REQUEST ['selected_property'];
+            break;
 		case 'propertycategories':
 			$_REQUEST ['task'] = 'search';
-			$_REQUEST ['calledByModule'] = 'mod_jomsearch_m0';
-			$_REQUEST ['cat_id'] = (int)$_REQUEST ['selected_category'];
-			break;
-		default:
-			break;
-		}
+            $_REQUEST ['calledByModule'] = 'mod_jomsearch_m0';
+            $_REQUEST ['cat_id'] = (int)$_REQUEST ['selected_category'];
+            break;
+		case 'propertyfeatures':
+			$_REQUEST ['task'] = 'search';
+            $_REQUEST ['calledByModule'] = 'mod_jomsearch_m0';
+            $_REQUEST ['feature_uids'] = (int)$_REQUEST ['feature_abbv'];
+            break;
+        default:
+            break;
+        }
 }
 
 if (!defined('JOMRES_ROOT_DIRECTORY')) {
-	if (file_exists(dirname(__FILE__).'/../../jomres_root.php')) {
-		require_once dirname(__FILE__).'/../../jomres_root.php';
-	} else {
-		define('JOMRES_ROOT_DIRECTORY', 'jomres');
-	}
+    if (file_exists(dirname(__FILE__).'/../../jomres_root.php')) {
+        require_once dirname(__FILE__).'/../../jomres_root.php';
+    } else {
+        define('JOMRES_ROOT_DIRECTORY', 'jomres');
+    }
 }
 
 require_once dirname(__FILE__).'/../../'.JOMRES_ROOT_DIRECTORY.'/jomres.php';
