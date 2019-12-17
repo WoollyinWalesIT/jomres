57a087a8c7d92fde40716ffd67135fb06a6ba70c
Vince Wooll <sales@jomres.net>
2019-12-17 12:47:41 +0100

    Added some throttling to ensure that syndicate servers don't get spammed by api hits quite as much.

--- core-minicomponents/j06000cron_syndication_get_syndicate_properties.class.php
+++ core-minicomponents/j06000cron_syndication_get_syndicate_properties.class.php
@@ -43,7 +43,7 @@ class j06000cron_syndication_get_syndicate_properties
 		
 		$query = "SELECT `id` , `domain` , `api_url` , `last_checked` , `approved` FROM #__jomres_syndication_domains WHERE approved = 1";
 		$result = doSelectSql($query);
-		
+
 		$existing_domains = array();
 
 		if (!empty($result)) {
@@ -52,131 +52,144 @@ class j06000cron_syndication_get_syndicate_properties
 			}
 		}
 
-		
 		$local_domain = parse_url(get_showtime('live_site'));
 		$local_hostname = $local_domain['host'];
 		
 		$now = date("Y-m-d H:i:s");
 		
 		if (!empty($result)) {
-			foreach ($result as $r) {
-				
-				$syndication_domain_id = $r->id;
-				$domain = parse_url($r->api_url);
+				foreach ($result as $r) {
+					
+					$domain = parse_url($r->api_url);
+					
+					$datetime1 = new DateTime($now);
+					$datetime2 = new DateTime($r->last_checked);
+					$interval = $datetime1->diff($datetime2);
+
+					if ($interval->h > 12 ) {
+
+						$syndication_domain_id = $r->id;
 						
-				$query = "SELECT propertys_uid FROM #__jomres_syndication_properties WHERE syndication_domain_id = ".(int)$syndication_domain_id;
-				$domain_properties = doSelectSql($query);
-				
-				$local_domain_properties = array();
-				if (!empty($domain_properties)) {
-					foreach ($domain_properties as $p) {
-						$local_domain_properties[] = $p->propertys_uid;
-					}
-				}
-				
-				try {
-					$client = new GuzzleHttp\Client();
-					$response = $client->request('GET', $r->api_url.'core/get_properties'.'/' , ['connect_timeout' => 4 , 'read_timeout' => 30, 'verify' => false , 'http_errors' => false] );
 
-					if ((string)$response->getStatusCode() == "404") {
-						$query = "UPDATE  #__jomres_syndication_domains SET 
-						`last_checked` = '".date("Y-m-d H:i:s" , strtotime("+1 year") )."' ,
-						`approved` = 0 ,
-						`unapproval_reason` = 'system'
-						WHERE id = ".(int)$syndication_domain_id;
-					doInsertSql($query);
-					} else {
-						if ( $local_hostname != $domain['host'] ) {
-							logging::log_message("Checking for new properties for ".$domain['host'], 'Syndication', 'DEBUG');
-							
-							$body				= json_decode((string)$response->getBody());
+						$query = "SELECT propertys_uid FROM #__jomres_syndication_properties WHERE syndication_domain_id = ".(int)$syndication_domain_id;
+						$domain_properties = doSelectSql($query);
+						
+						$local_domain_properties = array();
+						if (!empty($domain_properties)) {
+							foreach ($domain_properties as $p) {
+								$local_domain_properties[] = $p->propertys_uid;
+							}
+						}
+						
+						try {
+							$client = new GuzzleHttp\Client();
+							$response = $client->request('GET', $r->api_url.'core/get_properties'.'/' , ['connect_timeout' => 4 , 'read_timeout' => 30, 'verify' => false , 'http_errors' => false] );
 
-							if (!empty($body->data->properties[0]->properties)){
-								$row_str = '';
-								foreach ($body->data->properties[0]->properties as $property) {
-									if (!in_array( $property->propertys_uid, $local_domain_properties)) {
+							if ((string)$response->getStatusCode() == "404") {
+								$query = "UPDATE  #__jomres_syndication_domains SET 
+								`last_checked` = '".date("Y-m-d H:i:s" , strtotime("+1 year") )."' ,
+								`approved` = 0 ,
+								`unapproval_reason` = 'system'
+								WHERE id = ".(int)$syndication_domain_id;
+							doInsertSql($query);
+							} else {
+								if ( $local_hostname != $domain['host'] ) {
+									logging::log_message("Checking for new properties for ".$domain['host'], 'Syndication', 'DEBUG');
+									
+									$body				= json_decode((string)$response->getBody());
 
-										$image_exists =true;
+									if (!empty($body->data->properties[0]->properties)){
+										$row_str = '';
+										foreach ($body->data->properties[0]->properties as $property) {
+											if (!in_array( $property->propertys_uid, $local_domain_properties)) {
 
-										$bang = explode("/",$property->thumbnail_location);
-										if ( 
-											isset($property->propertys_uid) && 
-											isset($property->view_property_url) &&
-											isset($property->booking_form_url) &&
-											isset($property->thumbnail_location) &&
-											parse_url($property->view_property_url) && 
-											parse_url($property->booking_form_url) && 
-											end($bang) != 'noimage_small.gif' && 
-											$image_exists
-											)  {
-												$row_str .= "
-													('".$syndication_domain_id."',
-													'".$property->view_property_url."',
-													'".$property->booking_form_url."',
-													".(int)$property->propertys_uid.",
-													'".filter_var($property->name, FILTER_SANITIZE_STRING)."',
-													".(int)$property->multi_room_property.",
-													'".filter_var($property->lat, FILTER_SANITIZE_STRING)."',
-													'".filter_var($property->long, FILTER_SANITIZE_STRING)."',
-													'".filter_var($property->metadescription, FILTER_SANITIZE_STRING)."',
-													'".filter_var($property->thumbnail_location, FILTER_SANITIZE_STRING)."',
-													'".$now."',
-													'".$now."',
-													'1'),";
+												$image_exists =true;
+
+												$bang = explode("/",$property->thumbnail_location);
+												if ( 
+													isset($property->propertys_uid) && 
+													isset($property->view_property_url) &&
+													isset($property->booking_form_url) &&
+													isset($property->thumbnail_location) &&
+													parse_url($property->view_property_url) && 
+													parse_url($property->booking_form_url) && 
+													end($bang) != 'noimage_small.gif' && 
+													$image_exists
+													)  {
+														$row_str .= "
+															('".$syndication_domain_id."',
+															'".$property->view_property_url."',
+															'".$property->booking_form_url."',
+															".(int)$property->propertys_uid.",
+															'".filter_var($property->name, FILTER_SANITIZE_STRING)."',
+															".(int)$property->multi_room_property.",
+															'".filter_var($property->lat, FILTER_SANITIZE_STRING)."',
+															'".filter_var($property->long, FILTER_SANITIZE_STRING)."',
+															'".filter_var($property->metadescription, FILTER_SANITIZE_STRING)."',
+															'".filter_var($property->thumbnail_location, FILTER_SANITIZE_STRING)."',
+															'".$now."',
+															'".$now."',
+															'1'),";
+												}
+											}
+										}
+										if ($row_str != '' ) {
+											$row_str = substr($row_str, 0, -1);
+											
+											$query = "INSERT INTO #__jomres_syndication_properties (
+												`syndication_domain_id`,
+												`view_property_url`,
+												`booking_form_url`,
+												`propertys_uid`,
+												`name`,
+												`multi_room_property`,
+												`lat`,
+												`long`,
+												`metadescription`,
+												`thumbnail_location`,
+												`date_added`,
+												`last_checked`,
+												`approved`
+											)
+											VALUES ".$row_str;
+
+											doInsertSql($query);
+										} else {
+											$query = "
+												UPDATE #__jomres_syndication_domains SET
+													`last_checked` =  '".$now."'
+													WHERE id = ".(int)$r->id."
+													";
+											doInsertSql($query);
 										}
 									}
 								}
-								if ($row_str != '' ) {
-									$row_str = substr($row_str, 0, -1);
-									
-									$query = "INSERT INTO #__jomres_syndication_properties (
-										`syndication_domain_id`,
-										`view_property_url`,
-										`booking_form_url`,
-										`propertys_uid`,
-										`name`,
-										`multi_room_property`,
-										`lat`,
-										`long`,
-										`metadescription`,
-										`thumbnail_location`,
-										`date_added`,
-										`last_checked`,
-										`approved`
-									)
-									VALUES ".$row_str;
-
-									doInsertSql($query);
-								} else {
-									logging::log_message("No new properties found for domain ".$domain['host'], 'Syndication', 'DEBUG');
-								}
 							}
 						}
-					}
-				}
-				catch (GuzzleHttp\Exception\RequestException $e) {
+						catch (GuzzleHttp\Exception\RequestException $e) {
+								if (!in_array( $domain['host'] , $existing_domains )) {
+									$query = "
+										INSERT INTO #__jomres_syndication_domains SET
+											`domain` = '".$domain['host']."',
+											`last_checked` =  '".$now."'
+											`approved` = 0,
+											`unapproval_reason` = 'system'
+											";
+								} else {
+									$query = "
+										UPDATE #__jomres_syndication_domains SET
+											`last_checked` =  '".$now."',
+											`approved` = 0,
+											`unapproval_reason` = 'system'
+											WHERE id = ".(int)$r->id."
+											";
+								}
+								doInsertSql($query);
 
-						if (!in_array( $domain['host'] , $existing_domains )) {
-							$query = "
-								INSERT INTO #__jomres_syndication_domains SET
-									`domain` = '".$domain['host']."',
-									`last_checked` =  '".$now."'
-									`approved` = 0,
-									`unapproval_reason` = 'system'
-									";
-						} else {
-							$query = "
-								UPDATE #__jomres_syndication_domains SET
-									`last_checked` =  '".$now."',
-									`approved` = 0,
-									`unapproval_reason` = 'system'
-									WHERE id = ".(int)$r->id."
-									";
+								logging::log_message("Tried to get properties information for domain ".$domain['host']." but received 404 message so either the server is offline, or it cannot response to core api requests. Blocking from future requests until recheck time ", 'Syndication', 'DEBUG');
 						}
-						doInsertSql($query);
-
-						logging::log_message("Tried to get properties information for domain ".$domain['host']." but received 404 message so either the server is offline, or it cannot response to core api requests. Blocking from future requests until recheck time ", 'Syndication', 'DEBUG');
-
+				} else {
+					logging::log_message("It is too soon to check ".$domain['host']. " Interval = ".$interval->h, 'Syndication', 'DEBUG');
 				}
 			}
 		}
