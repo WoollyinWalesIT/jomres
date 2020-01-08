318a519c6b140496bbf1250f2fb47f732d4c973e
Vince Wooll <sales@jomres.net>
2020-01-08 14:34:41 +0100

    Fixed an issue where pdfs are not deleted on property details update

--- libraries/jomres/classes/jomres_properties.class.php
+++ libraries/jomres/classes/jomres_properties.class.php
@@ -445,7 +445,11 @@ class jomres_properties
 				$this->setPublished(0);
 			}
 		}
-
+		
+		if ( file_exists (JOMRES_MPDF_ABSPATH.JRDS."terms_and_conditions_".(int)$this->propertys_uid.".pdf") ) {
+			unlink(JOMRES_MPDF_ABSPATH.JRDS."terms_and_conditions_".(int)$this->propertys_uid.".pdf");
+		}
+		
 		$webhook_notification						   	= new stdClass();
 		$webhook_notification->webhook_event				= 'property_updated';
 		$webhook_notification->webhook_event_description	= 'Logs when a property is updated.';
