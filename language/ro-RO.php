0055c33bc6206529087869b7d3ec1847069b126a
Vince Wooll <vince.wooll@gmail.com>
2020-06-17 14:32:42 +0200

    Added Connect admin panel to Core

--- language/ro-RO.php
+++ language/ro-RO.php
@@ -2651,3 +2651,19 @@ jr_define('_JOMRES_CONFIG_SYNDICATION_DESC',"Once you have your site setup and r
 
 jr_define('_JOMRES_MICROMANAGE_CREATE_ROOM_1',"You need at least one room of one of the following types before you can create a tariff :");
 jr_define('_JOMRES_MICROMANAGE_CREATE_ROOM_2',"Go to Settings > Rooms and create a room first, once  you have done that you will be able to create a tariff for that room type.");
+
+jr_define('JOMRES_PLATFORM',"Jomres Platform");
+
+
+jr_define('JOMRES_PLATFORM_CONNECTED',"Connected");
+jr_define('JOMRES_PLATFORM_CONNECTED_DESC',"Set this to Yes if you have already connected your sTRIPE account with the Jomres Platform.");
+
+jr_define('JOMRES_PLATFORM_ACCOUNT_ID',"Stripe Live Account number");
+jr_define('JOMRES_PLATFORM_ACCOUNT_ID_DESC',"This is your Stripe Account id which can be found in your Stripe Dashboard under Settings > Account information and looks like acct_xxxxxxxxx You would use this field if you have multiple Jomres installations and you want to use the same account across all sites and you have already connected to us. If you have not connected to the Jomres Platform, it's not sufficient to enter your details here. Instead, please visit the Get Connected menu option in the toolbar, under the Help section. If you want to test payments, in the Debugging tab of Site Configuration set your site from Production to Development.");
+
+jr_define('JOMRES_PLATFORM_LIVE_SECRET_KEY',"Stripe live secret key");
+jr_define('JOMRES_PLATFORM_LIVE_SECRET_KEY_DESC','Go to Developers > API keys in your Stripe Dashboard to find your secret keys. These are used by the system to verify booking data returned from the payment form is valid and that deposits have been paid before saving the booking.' );
+
+jr_define('JOMRES_PLATFORM_TEST_SECRET_KEY',"Stripe test secret key");
+jr_define('JOMRES_PLATFORM_TEST_SECRET_KEY_DESC',' ' );
+
