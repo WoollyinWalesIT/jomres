9b34ce31a33b9318d6edf0350e7e12b95b1c0e4d
Vince Wooll <vince.wooll@gmail.com>
2020-06-30 11:23:27 +0200

    Moved a function in wp installer

--- libraries/jomres/cms_specific/wordpress/cms_plugin/jomres.php
+++ libraries/jomres/cms_specific/wordpress/cms_plugin/jomres.php
@@ -82,22 +82,6 @@ if ( ! defined( 'JOMRES_ROOT_DIRECTORY' ) ) {
 }
 
 /**
- * Set path definitions.
- *
- *
- * @since	9.22.0
- */
-if (!function_exists('define_jomres_sub_dir_in_plugins_dir_as_root')) {
-	function define_jomres_sub_dir_in_plugins_dir_as_root()
-	{
-		$jr_root = str_replace( ABSPATH , '' ,  JOMRES_WP_PLUGIN_PATH );
-		define( 'JOMRES_ROOT_DIRECTORY', $jr_root.DIRECTORY_SEPARATOR.'jomres' );
-		define('JOMRESPATH_BASE', JOMRES_WP_PLUGIN_PATH.'jomres'.DIRECTORY_SEPARATOR);
-		define('JRDS', DIRECTORY_SEPARATOR );
-	}
-}
-
-/**
  * The Jomres wp plugin functions.
  */
 require_once plugin_dir_path( __FILE__ ) . 'includes/functions.php';
