aec6d62f796337886b078401f808f5cf6dfda2ee
Vince Wooll <vince.wooll@gmail.com>
2020-06-30 12:05:24 +0200

    Updated installation process for wp to ensure that Jomres can be installed under the WP Plugins dir

--- libraries/jomres/cms_specific/wordpress/cms_plugin/jomres.php
+++ libraries/jomres/cms_specific/wordpress/cms_plugin/jomres.php
@@ -58,8 +58,6 @@ if ( is_admin() && ! defined( '_JOMRES_INITCHECK_ADMIN' ) ) {
 	define( '_JOMRES_INITCHECK_ADMIN', 1 );
 }
 
-
-
 /**
  * Set path definitions.
  *
@@ -76,6 +74,124 @@ if (!function_exists('define_jomres_sub_dir_in_plugins_dir_as_root')) {
 	}
 }
 
+
+/**
+ * Runs Jomres installation or update routine.
+ *
+ * Donwloads Jomres, unzips and runs the jomres install or update
+ *
+ * @since	9.9.19
+ */
+if (!function_exists('run_jomres_installer')) {
+	function run_jomres_installer($method = 'install')
+	{
+
+		@ignore_user_abort(true);
+		@set_time_limit(0);
+
+		require_once(ABSPATH . 'wp-admin/includes/file.php');
+
+		WP_Filesystem();
+
+		global $wp_filesystem;
+
+		//get the latest jomres version download url
+		$url = 'http://updates.jomres4.net/getlatest.php?includebeta=true';
+		$nightly_url = 'http://updates.jomres4.net/nightly/';
+
+		$nightly = false;
+
+		if (WP_DEBUG) {
+			$nightly = true;
+		} elseif (file_exists(ABSPATH . JOMRES_ROOT_DIRECTORY . '/configuration.php')) {
+			include ABSPATH . JOMRES_ROOT_DIRECTORY . '/configuration.php';
+
+			if ($jrConfig['development_production'] == 'development') {
+				$nightly = true;
+			}
+		}
+
+		//download jomres core
+		$response = wp_remote_get($url);
+
+		if (strlen($response['body']) == 0) {
+			jomres_notice('There was an error getting the latest Jomres version number.');
+
+			return false;
+		}
+
+		//set source and target
+		$source = get_temp_dir() . 'jomres.zip';
+		$target = ABSPATH . JOMRES_ROOT_DIRECTORY;
+
+		//check if /jomres dir is writable
+		if (!wp_is_writable($target)) {
+			jomres_notice('Jomres dir ' . $target . ' can`t be created or it`s not writable. Using FTP, create the directory manually then re-run the installer, many times this will solve the problem.');
+
+			return false;
+		}
+
+		//download Jomres
+		$options = array(
+			'timeout' => 300,
+			'stream' => true,
+			'filename' => $source
+		);
+
+		if (!$nightly) {
+			$response = wp_remote_get($response['body'], $options);
+		} else {
+			$response = wp_remote_get($nightly_url, $options);
+		}
+
+		if (is_wp_error($response)) {
+			jomres_notice('There was an error downloading jomres.zip.');
+
+			return false;
+		}
+
+		//unzip jomres files
+		$unzipfile = unzip_file($source, $target);
+
+		if (is_wp_error($unzipfile)) {
+			jomres_notice('There was an error unzipping the Jomres files.');
+
+			return false;
+		}
+
+		//delete downloaded zip
+		if (file_exists($source)) {
+			unlink($source);
+		}
+
+		//install Jomres
+		try {
+			require_once ABSPATH . JOMRES_ROOT_DIRECTORY . '/libraries/jomres/classes/jomres_install.class.php';
+
+			$jomres_install = new jomres_install($method);
+
+			$messages = $jomres_install->getMessages();
+
+			//if there are no installation errors, update jomres_wp_plugin_version in db
+			if (empty($messages)) {
+				update_option('jomres_wp_plugin_version', JOMRES_WP_PLUGIN_VERSION);
+			}
+		} catch (Exception $e) {
+			$messages = $jomres_install->getMessages();
+
+			foreach ($messages as $m) {
+				jomres_notice($m);
+			}
+
+			jomres_notice($e->getMessage());
+
+			return false;
+		}
+
+		return true;
+
+	}
+}
 
 /**
  * Jomres root directory.
