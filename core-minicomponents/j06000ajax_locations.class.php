9f9778427eae7030e166734294f627056e49e39f
Vince Wooll <vince.wooll@gmail.com>
2020-07-06 19:44:49 +0200

    Added a new ajax script designed to respond with populated locations

---
+++ core-minicomponents/j06000ajax_locations.class.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Core file.
+ *
+ * @author Vince Wooll <sales@jomres.net>
+ *
+ * @version Jomres 9.23.0
+ *
+ * @copyright	2005-2020 Vince Wooll
+ * Jomres (tm) PHP, CSS & Javascript files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly
+ **/
+
+// ################################################################
+defined('_JOMRES_INITCHECK') or die('');
+// ################################################################
+	
+	/**
+	 * @package Jomres\Core\Minicomponents
+	 *
+     * Ajax script. Set the guest budget found in the list properties page
+	 * 
+	 */
+
+class j06000ajax_locations
+{	
+	/**
+	 *
+	 * Constructor
+	 * 
+	 * Main functionality of the Minicomponent 
+	 *
+	 * 
+	 * 
+	 */
+	 
+	public function __construct()
+	{
+		$MiniComponents = jomres_singleton_abstract::getInstance('mcHandler');
+		if ($MiniComponents->template_touch) {
+			$this->template_touchable = false;
+
+			return;
+		}
+
+		$requested_budget = (int) jomresGetParam($_REQUEST, 'budget_figure', 0);
+		jr_import('jomres_user_budget');
+		$budget = new jomres_user_budget();
+		$budget->set_budget($requested_budget);
+	}
+
+
+	public function getRetVals()
+	{
+		return null;
+	}
+}
