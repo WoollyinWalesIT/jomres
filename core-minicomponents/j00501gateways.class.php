6f4e757b1cc638e7ac1d0a52950f08cb2c22468c
Vince Wooll <vince.wooll@gmail.com>
2020-07-23 17:30:02 +0200

    Disable gateways tab if using jomres platform

--- core-minicomponents/j00501gateways.class.php
+++ core-minicomponents/j00501gateways.class.php
@@ -58,6 +58,10 @@ class j00501gateways
 			return;
 			}
 
+		if ($MiniComponents->eventSpecificlyExistsCheck('06000', "connected")) { // No point in showing the gateway options as we are using Jomres Platform
+			return;
+		}
+
 		$lists = $componentArgs[ 'lists' ];
 
 			$configurationPanel->startPanel(jr_gettext('_JOMRES_COM_A_GATEWAYLIST', '_JOMRES_COM_A_GATEWAYLIST', false));
