340a6a65ac03c9cc4b466766f0bdbe1b28b5f942
Vince Wooll <vince.wooll@gmail.com>
2020-07-28 12:05:12 +0200

    Duplicated a js function for J3 & J4

--- assets/js/jomres.js
+++ assets/js/jomres.js
@@ -1232,6 +1232,55 @@ document.addEventListener('DOMContentLoaded', function() {
 	};
 });
 
+
+/*
+ * jHeartbeat 0.3.0
+ * (C)Alex Richards - http://www.ajtrichards.co.uk/
+ */
+jomresJquery(document).ready(function (){
+	jomresJquery.jheartbeat = {
+		options: {
+			url: "heartbeat_default.asp",
+			delay: 10000,
+			div_id: "test_div"
+		},
+		beatfunction: function () {
+		},
+		timeoutobj: {
+			id: -1
+		},
+		set: function (options, onbeatfunction) {
+			if (this.timeoutobj.id > -1) {
+				clearTimeout(this.timeoutobj);
+			}
+			if (options) {
+				jomresJquery.extend(this.options, options);
+			}
+			if (onbeatfunction) {
+				this.beatfunction = onbeatfunction;
+			}
+			// Add the HeartBeatDIV to the page
+			jomresJquery("body").append("<div id=\"" + this.options.div_id + "\" style=\"display: none;\"></div>");
+			this.timeoutobj.id = setTimeout("jomresJquery.jheartbeat.beat();", this.options.delay);
+		},
+		beat: function () {
+			jomresJquery.ajax({
+				url: this.options.url,
+				dataType: "html",
+				type: "GET",
+				error: function (e) {
+					jomresJquery('#' + jomresJquery.jheartbeat.options.div_id).append("");
+				},
+				success: function (data) {
+					jomresJquery('#' + jomresJquery.jheartbeat.options.div_id).html(data);
+				}
+			});
+			this.timeoutobj.id = setTimeout("jomresJquery.jheartbeat.beat();", this.options.delay);
+			this.beatfunction();
+		}
+	};
+});
+
 function jomres_print(div) {
 	jomresJquery('body').css('visibility','hidden');
 	jomresJquery(div).css('visibility','visible');
