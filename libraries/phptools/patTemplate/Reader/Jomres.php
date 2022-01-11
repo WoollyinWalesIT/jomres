a50a2cedad9b98be8aa540f9fe1a83d3e69fd7eb
Vince Wooll <vince.wooll@gmail.com>
2022-01-11 15:37:09 +0200

    File versions updated

--- libraries/phptools/patTemplate/Reader/Jomres.php
+++ libraries/phptools/patTemplate/Reader/Jomres.php
@@ -6,7 +6,7 @@ defined( '_JOMRES_INITCHECK' ) or die( '' );
  * Core file
  *
  * @author Vince Wooll <sales@jomres.net>
-* * @version Jomres 10.1.1
+* * @version Jomres 10.1.2
  * @package Jomres
  * @copyright	2005-2022 Vince Wooll
  * Jomres (tm) PHP, CSS & Javascript files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly.
