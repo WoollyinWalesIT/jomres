85fd34a2ea0329080569c490e94abe7833525249
Vince Wooll <vince.wooll@gmail.com>
2022-01-21 13:44:19 +0200

    Revert "Don't include google map source url if api key not set"
    This reverts commit 3311833575ddfeafb36cc638bf30aba09eff6142.

--- libraries/jomres/functions/functions.php
+++ libraries/jomres/functions/functions.php
@@ -1808,10 +1808,6 @@ function add_gmaps_source()
 			$apikey = '&key='.$jrConfig[ 'google_maps_api_key' ];
 		}
 
-		if ($apikey == '' ) {
-			return;
-		}
-
 		jomres_cmsspecific_addheaddata('javascript', 'https://maps.googleapis.com/maps/api/js?v=3&language='.$shortcode.$apikey, '&foo=bar', $includeVersion = false, $async = true);
 	}
 }
