88778319c245f5f9e3b1189e9329eece02748a49
Vince Wooll <vince.wooll@gmail.com>
2022-02-11 13:15:04 +0200

    Added show_property_reviews_stars script and template files

---
+++ core-minicomponents/j06000show_property_reviews_stars.class.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * Core file.
+ *
+ * @author Vince Wooll <sales@jomres.net>
+ *
+* * @version Jomres 10.1.3
+ *
+ * @copyright	2005-2022 Vince Wooll
+ * Jomres (tm) PHP, CSS & Javascript files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly
+ **/
+
+// ################################################################
+defined('_JOMRES_INITCHECK') or die('');
+// ################################################################
+	
+	/**
+	 * @package Jomres\Core\Minicomponents
+	 *
+	 * 
+	 */
+
+class j06000show_property_reviews_stars
+{	
+	/**
+	 *
+	 * Constructor
+	 * 
+	 * Main functionality of the Minicomponent 
+	 *
+	 * 
+	 * 
+	 */
+	 
+	public function __construct($componentArgs)
+	{
+		$MiniComponents = jomres_singleton_abstract::getInstance('mcHandler');
+		if ($MiniComponents->template_touch) {
+			$this->template_touchable = false;
+			$this->shortcode_data = array(
+				'task' => 'show_property_reviews_stars',
+				'info' => '_JOMRES_SHORTCODES_06000SHOW_PROPERTY_REVIEWS_SUMMARY',
+				'arguments' => array(
+					0 => array(
+						'argument' => 'property_uid',
+						'arg_info' => '_JOMRES_SHORTCODES_06000SHOW_PROPERTY_REVIEWS_SUMMARY_ARG_PROPERTY_UID',
+						'arg_example' => '1',
+						),
+					),
+				);
+
+			return;
+		}
+		$this->retVals = '';
+
+		$siteConfig = jomres_singleton_abstract::getInstance('jomres_config_site_singleton');
+		$jrConfig = $siteConfig->get();
+
+		if (isset($componentArgs[ 'property_uid' ])) {
+			$property_uid = (int)$componentArgs[ 'property_uid' ];
+		} else {
+			$property_uid = (int)jomresGetParam($_REQUEST, 'property_uid', 0);
+		}
+		
+		if ($property_uid == 0) {
+			return;
+		}
+
+		if (!user_can_view_this_property($property_uid)) {
+			return;
+		}
+
+		if (isset($componentArgs[ 'output_now' ])) {
+			$output_now = $componentArgs[ 'output_now' ];
+		} else {
+			$output_now = true;
+		}
+
+		if ((int) $jrConfig[ 'use_reviews' ] == 0) {
+			return;
+		}
+
+		$output = array();
+		$pageoutput = array();
+
+		jr_import('jomres_reviews');
+		$Reviews = new jomres_reviews();
+		$Reviews->property_uid = $property_uid;
+		$itemRating = $Reviews->showRating($property_uid);
+
+		$output[ 'AVERAGE_RATING' ] = number_format($itemRating[ 'avg_rating' ], 1, '.', '');
+
+		$output[ 'RATING_STARS' ] = '';
+		for ($i = 1; $i <= floor($output[ 'AVERAGE_RATING' ]); ++$i) {
+			$output[ 'RATING_STARS' ] .= '<i class="fa fa-star" aria-hidden="true"></i> ';
+		}
+
+		$output[ 'ONE_STAR' ] = '<i class="fa fa-star" aria-hidden="true"></i> ';
+
+		$output['RATING_TEXT_COLOUR'] = 'text-success';
+		if ( $output[ 'AVERAGE_RATING' ] >= 5 && $output[ 'AVERAGE_RATING' ] < 7 ) {
+			$output['RATING_TEXT_COLOUR'] = 'text-success';
+		}
+		if ( $output[ 'AVERAGE_RATING' ] < 5 ) {
+			$output['RATING_TEXT_COLOUR'] = 'text-danger';
+		}
+
+		$pageoutput[] = $output;
+
+		$tmpl = new patTemplate();
+		$tmpl->addRows('pageoutput', $pageoutput);
+		$tmpl->setRoot(JOMRES_TEMPLATEPATH_FRONTEND);
+		$tmpl->readTemplatesFromInput('show_property_reviews_stars.html');
+		$result = $tmpl->getParsedTemplate();
+
+		if ($output_now) {
+			echo $result;
+		} else {
+			$this->retVals = $result;
+		}
+	}
+
+	//this must be included in all mini-components
+	public function getRetVals()
+	{
+		return $this->retVals;
+	}
+}
