e05370c919273aa47fea48e538e26cac75e29fc3
Vince Wooll <vince.wooll@gmail.com>
2022-02-18 15:24:04 +0200

    File versions updated

--- api/classes/all_api_features.class.php
+++ api/classes/all_api_features.class.php
@@ -9,7 +9,7 @@
  *
  * @author Vince Wooll <sales@jomres.net>
  *
-* @version Jomres 10.2.0
+ * @version Jomres 10.2.1
  *
  * @copyright	2005-2022 Vince Wooll
  * Jomres (tm) PHP, CSS & Javascript files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly
