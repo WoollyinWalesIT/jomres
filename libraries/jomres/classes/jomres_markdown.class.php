4e9f7a98207be7da57507846972e1125602834d9
Vince Wooll <vince.wooll@gmail.com>
2022-02-18 12:33:28 +0200

    Improved markdown

--- libraries/jomres/classes/jomres_markdown.class.php
+++ libraries/jomres/classes/jomres_markdown.class.php
@@ -1,49 +1,52 @@
 <?php
-/**
- * Core file.
- *
- * @author Vince Wooll <sales@jomres.net>
- *
-* @version Jomres 10.2.0
- *
- * @copyright	2005-2022 Vince Wooll
- * Jomres (tm) PHP, CSS & Javascript files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly
- **/
+	/**
+	 * Core file.
+	 *
+	 * @author Vince Wooll <sales@jomres.net>
+	 *
+	 * @version Jomres 10.2.0
+	 *
+	 * @copyright	2005-2022 Vince Wooll
+	 * Jomres (tm) PHP, CSS & Javascript files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly
+	 **/
 
 // ################################################################
-defined('_JOMRES_INITCHECK') or die('');
+	defined('_JOMRES_INITCHECK') or die('');
 // ################################################################
 
 // A wrapper class for parsing data through a markdown class
-	
+
 	/**
 	 *
 	 * @package Jomres\Core\Classes
 	 *
 	 */
 
-class jomres_markdown {
-	
-	/**
-	 * 
-	 *
-	 *
-	 */
+	class jomres_markdown {
 
-	public function __construct() {
-		//require_once (JOMRES_LIBRARIES_ABSPATH.'Parsedown'.JRDS.'Parsedown.php');
-		$this->Parsedown = new Parsedown();
-		$this->Parsedown->setBreaksEnabled(true);
+		/**
+		 *
+		 *
+		 *
+		 */
+
+		public function __construct() {
+			//require_once (JOMRES_LIBRARIES_ABSPATH.'Parsedown'.JRDS.'Parsedown.php');
+			$this->Parsedown = new Parsedown();
+			$this->Parsedown->setBreaksEnabled(true);
+		}
+
+		/**
+		 *
+		 *
+		 *
+		 */
+
+		public function get_markdown($string) {
+			$string = str_replace ( "&#10;" , "\n" , $string);
+			$string = str_replace ( "&#38;" , "&" , $string);
+
+			return $this->Parsedown->text($string);
+		}
+
 	}
-	
-	/**
-	 * 
-	 *
-	 *
-	 */
-
-	public function get_markdown($string) {
-		return $this->Parsedown->text($string);
-	}
-
-}
