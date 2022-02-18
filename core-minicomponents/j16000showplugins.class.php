bd8e6cef6687b7552245e1005dfec99faaa06a1d
Vince Wooll <vince.wooll@gmail.com>
2022-02-18 13:51:00 +0200

    Don't invite plugin manager installation if key isn't valid.

--- core-minicomponents/j16000showplugins.class.php
+++ core-minicomponents/j16000showplugins.class.php
@@ -54,16 +54,13 @@ class j16000showplugins
 		
 		$force_plugin_manager_reinstallation = false;
 
-		// Jomres 9.10 specific code, we need to check to see if the task == showplugins, and if so double check the plugin manager's version. If it's < 1.9 we need to force an update of the plugin manager before the plugin manager script can be shown
-		// Without this check and force of the reinstallation of the plugin manager, users will only be able to update the 40 or so plugins one by one, which would be a significant annoyance.
-		// Todo remove sometime after January 2019
-		if (isset($MiniComponents->registeredClasses['16000']['showplugins']['real_filepath'])) { // admin.php has found that the installed version of the plugin manager is 1.8 or less, and has forced us to run the Core plugin manager script, not the plugin version. 
+		if (isset($MiniComponents->registeredClasses['16000']['showplugins']['real_filepath'])) { // admin.php has found that the installed version of the plugin manager is 1.8 or less, and has forced us to run the Core plugin manager script, not the plugin version.
 			if (file_exists($MiniComponents->registeredClasses['16000']['showplugins']['real_filepath']."plugin_info.php") ) {
 				require_once($MiniComponents->registeredClasses['16000']['showplugins']['real_filepath']."plugin_info.php");
 				$plugin_info_plugin_manager = new plugin_info_plugin_manager();
 				$bang = explode("." , $plugin_info_plugin_manager->data['version'] );
-				if ( $bang [0] <= 2 ) {
-					if ($bang [1] <= 8) {
+				if ( $bang [0] <= 3 ) {
+					if ($bang [1] <= 9) {
 						$force_plugin_manager_reinstallation = true;
 					}
 				}
@@ -74,6 +71,11 @@ class j16000showplugins
 		$key_validation->check_license_key(true); //only needed if we want to force a recheck
 
 		$this->key_valid = $key_validation->key_valid;
+
+		if (!$this->key_valid) {
+			echo '<span class="alert alert-danger">'.jr_gettext('_JOMRES_SUPPORTKEY_DESC_INVALID', '_JOMRES_SUPPORTKEY_DESC_INVALID', false).'</span>';
+			return;
+		}
 
 		if (
 			(!file_exists(JOMRES_COREPLUGINS_ABSPATH.'plugin_manager'.JRDS.'plugin_info.php'))  ||
