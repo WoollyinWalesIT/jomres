271665f904e5ecbc5e104ca286c0086d4f6925a4
Vince Wooll <vince.wooll@gmail.com>
2022-06-23 11:14:45 +0300

    Change the log out Jomres main menu item. Joomla's fontawesome library doesn't have a log out icon that I can find, for some weird reason, and because the Quickstarts are being delivered with more complete fontawesome libraries this change now makes sense.

--- core-minicomponents/j09995menu.class.php
+++ core-minicomponents/j09995menu.class.php
@@ -75,7 +75,7 @@ class j09995menu
 			$font_awesome_envelope = 'fa-envelope';
 			$font_awesome_picture = 'fa-images';
 			$font_awesome_dashboard = 'fa-tachometer-alt';
-			$font_awesome_logout = 'sign-out-alt';
+			$font_awesome_logout = 'fas fa-sign-out';
 			$font_awesome_delete = 'fa-trash';
 			$font_awesome_tariffs = 'fa-dollar-sign';
 			$font_awesome_edit = 'fa-edit';
