805389e389a427402864c721b09c9a54ff7371c1
Vince Wooll <vince.wooll@gmail.com>
2022-06-25 11:25:57 +0300

    Room rate per day changed to 11,7

---
+++ database/updates/10.5.0/alter_jomres_rates_roomrateperday_double_11_7.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Core file.
+ *
+ * @author Vince Wooll <sales@jomres.net>
+ *
+ *  @version Jomres 10.4.0 (Platty Joobs edition)
+ *
+ * @copyright	2005-2022 Vince Wooll
+ * Jomres (tm) PHP, CSS & Javascript files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly
+ **/
+
+// ################################################################
+defined('_JOMRES_INITCHECK') or die('');
+// ################################################################
+/**
+ *
+ * @package Jomres\Core\Database
+ *
+ * Database modification during updates
+ *
+ **/
+
+$query = "ALTER TABLE `#__jomres_rates` CHANGE `roomrateperday` `roomrateperday` DOUBLE(11,7) NOT NULL DEFAULT '0.00' ";
+doInsertSql($query,"");
+
