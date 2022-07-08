75a005551f3202debc164cda80c3934a09a2f961
Vince Wooll <vince.wooll@gmail.com>
2022-07-08 11:06:47 +0300

    Fixed some notices in list properties

--- core-minicomponents/j01010listpropertys.class.php
+++ core-minicomponents/j01010listpropertys.class.php
@@ -197,9 +197,9 @@
 								array_key_exists( $lpt ,$available_photo_templates ) ) {
 								$layout_template = $lpt.'.html';
 							}
-							if ( array_key_exists( $lpt , $available_list_templates) ) {
+							if ( array_key_exists( $lpt , $available_list_templates) && isset($available_list_templates[$lpt]['show_property_list_header']) ) {
 								$show_property_list_header = $available_list_templates[$lpt]['show_property_list_header'];
-							} else {
+							} elseif (isset($available_photo_templates[$lpt]['show_property_list_header'])) {
 								$show_property_list_header = $available_photo_templates[$lpt]['show_property_list_header'];
 							}
 						}
