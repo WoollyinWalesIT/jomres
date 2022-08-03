b3560e9eef8ff6c493204b858636e79106dce412
Vince Wooll <vince.wooll@gmail.com>
2022-08-03 19:29:08 +0300

    File versions updated

--- core-minicomponents/j06002savecustomertype.class.php
+++ core-minicomponents/j06002savecustomertype.class.php
@@ -2,7 +2,7 @@
 /**
 * Jomres CMS Agnostic Plugin
 * @author Woollyinwales IT <sales@jomres.net>
- *  @version Jomres 10.5.2
+ *  @version Jomres 10.5.3
 * @package Jomres
  * @copyright	2005-2022 Vince Wooll
 * Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
