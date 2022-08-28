d9ddf76cfd741762d03a0ff6db2ffd671a23fcb0
Vince Wooll <vince.wooll@gmail.com>
2022-08-28 22:07:24 +0300

    Tweaked new copy functionality, tested in C and FF.

--- assets/js/jomres.js
+++ assets/js/jomres.js
@@ -41,14 +41,17 @@ jomresJquery(document).ready(function () {
 	  
 });
 
-function jomresCopyToClipboard( input_id ) {
+jomresJquery('.copy_to_clipboard').on("click",function(e){
+	e.preventDefault();
+});
+
+function jomresCopyToClipboard(input_id) {
+
 	var copyText = document.getElementById(input_id);
 	copyText.select();
-	copyText.setSelectionRange(0, 99999); /* For mobile devices */
-	navigator.clipboard.writeText(copyText.value);
-	alert(copyText.value);
-	return false;
+	document.execCommand("copy");
 }
+
 
 
 function Block_Error() {
