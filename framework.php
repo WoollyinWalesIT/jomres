70ae156d80094d4b4418475ce73b1ff750e23e56
Vince Wooll <vince.wooll@gmail.com>
2022-10-01 10:35:55 +0300

    File versions and changelog updated

--- framework.php
+++ framework.php
@@ -3,7 +3,7 @@
  *
  * @author Vince Wooll <sales@jomres.net>
  *
- *  @version Jomres 10.5.4
+ *  @version Jomres 10.5.5
  *
  * @copyright	2005-2022 Vince Wooll
  * Jomres (tm) PHP, CSS & Javascript files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly
