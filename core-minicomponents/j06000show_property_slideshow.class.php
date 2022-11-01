30faf59441afc600fa03b19242b0e3be9f941557
Vince Wooll <vince.wooll@gmail.com>
2022-11-01 17:31:53 +0200

    Set slideshow image sizes for different pages

--- core-minicomponents/j06000show_property_slideshow.class.php
+++ core-minicomponents/j06000show_property_slideshow.class.php
@@ -1,103 +1,117 @@
 <?php
-/**
- * Core file.
- *
- * @author Vince Wooll <sales@jomres.net>
- *
- *  @version Jomres 10.5.5
- *
- * @copyright	2005-2022 Vince Wooll
- * Jomres (tm) PHP, CSS & Javascript files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly
- **/
+	/**
+	 * Core file.
+	 *
+	 * @author Vince Wooll <sales@jomres.net>
+	 *
+	 *  @version Jomres 10.5.5
+	 *
+	 * @copyright	2005-2022 Vince Wooll
+	 * Jomres (tm) PHP, CSS & Javascript files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly
+	 **/
 
 // ################################################################
-defined('_JOMRES_INITCHECK') or die('');
+	defined('_JOMRES_INITCHECK') or die('');
 // ################################################################
-	
+
 	/**
 	 * @package Jomres\Core\Minicomponents
 	 *
 	 *
 	 */
 
-class j06000show_property_slideshow
-{
-
-	/**
-	 *
-	 * Constructor
-	 *
-	 * Main functionality of the Minicomponent
-	 *
-	 *
-	 *
-	 */
-	 
-	public function __construct($componentArgs)
+	class j06000show_property_slideshow
 	{
-		// Must be in all minicomponents. Minicomponents with templates that can contain editable text should run $this->template_touch() else just return
-		$MiniComponents = jomres_singleton_abstract::getInstance('mcHandler');
-		if ($MiniComponents->template_touch) {
-			$this->template_touchable = false;
-			$this->shortcode_data = array(
-				'task' => 'show_property_slideshow',
-				'info' => '_JOMRES_SHORTCODES_06000SHOW_PROPERTY_SLIDESHOW',
-				'arguments' => array(0 => array(
+
+		/**
+		 *
+		 * Constructor
+		 *
+		 * Main functionality of the Minicomponent
+		 *
+		 *
+		 *
+		 */
+
+		public function __construct($componentArgs)
+		{
+			// Must be in all minicomponents. Minicomponents with templates that can contain editable text should run $this->template_touch() else just return
+			$MiniComponents = jomres_singleton_abstract::getInstance('mcHandler');
+			if ($MiniComponents->template_touch) {
+				$this->template_touchable = false;
+				$this->shortcode_data = array(
+					'task' => 'show_property_slideshow',
+					'info' => '_JOMRES_SHORTCODES_06000SHOW_PROPERTY_SLIDESHOW',
+					'arguments' => array(0 => array(
 						'argument' => 'property_uid',
 						'arg_info' => '_JOMRES_SHORTCODES_06000SHOW_PROPERTY_SLIDESHOW_ARG_PROPERTY_UID',
 						'arg_example' => '1',
-						),
+					),
 					),
 				);
 
-			return;
+				return;
+			}
+			$this->retVals = '';
+
+			if (isset($componentArgs[ 'property_uid' ])) {
+				$property_uid = (int)$componentArgs[ 'property_uid' ];
+			} else {
+				$property_uid = (int)jomresGetParam($_REQUEST, 'property_uid', 0);
+			}
+
+			if ($property_uid == 0) {
+				return;
+			}
+
+			if (!user_can_view_this_property($property_uid)) {
+				return;
+			}
+
+			if (isset($componentArgs[ 'output_now' ])) {
+				$output_now = $componentArgs[ 'output_now' ];
+			} else {
+				$output_now = true;
+			}
+
+			if (isset($componentArgs[ 'image_size' ])) {
+				$image_size = (string)$componentArgs[ 'image_size' ];
+			} else {
+				if ( isset($_REQUEST['image_size']) && trim($_REQUEST['image_size']) != '' ) {
+					$image_size = (int)jomresGetParam($_REQUEST, 'image_size', '');
+				} else {
+					$image_size = 'small';
+				}
+			}
+
+			if ( $image_size != 'small' && $image_size != 'medium' && $image_size != 'large' ) {
+				$image_size = 'small';
+			}
+
+			$output = array();
+
+			$result = $MiniComponents->specificEvent('01060', 'slideshow', array('property_uid' => $property_uid  , 'image_size' => $image_size  ));
+			$output[ 'SLIDESHOW' ] = $result ['slideshow'];
+
+			$pageoutput = array();
+			$pageoutput[] = $output;
+
+			$tmpl = new patTemplate();
+			$tmpl->addRows('pageoutput', $pageoutput);
+			$tmpl->setRoot(JOMRES_TEMPLATEPATH_FRONTEND);
+			$tmpl->readTemplatesFromInput('show_property_slideshow.html');
+			$result = $tmpl->getParsedTemplate();
+
+			if ($output_now) {
+				echo $result;
+			} else {
+				$this->retVals = $result;
+			}
 		}
-		$this->retVals = '';
 
-		if (isset($componentArgs[ 'property_uid' ])) {
-			$property_uid = (int)$componentArgs[ 'property_uid' ];
-		} else {
-			$property_uid = (int)jomresGetParam($_REQUEST, 'property_uid', 0);
-		}
-		
-		if ($property_uid == 0) {
-			return;
-		}
 
-		if (!user_can_view_this_property($property_uid)) {
-			return;
-		}
-
-		if (isset($componentArgs[ 'output_now' ])) {
-			$output_now = $componentArgs[ 'output_now' ];
-		} else {
-			$output_now = true;
-		}
-
-		$output = array();
-
-		$result = $MiniComponents->specificEvent('01060', 'slideshow', array('property_uid' => $property_uid));
-		$output[ 'SLIDESHOW' ] = $result ['slideshow'];
-
-		$pageoutput = array();
-		$pageoutput[] = $output;
-
-		$tmpl = new patTemplate();
-		$tmpl->addRows('pageoutput', $pageoutput);
-		$tmpl->setRoot(JOMRES_TEMPLATEPATH_FRONTEND);
-		$tmpl->readTemplatesFromInput('show_property_slideshow.html');
-		$result = $tmpl->getParsedTemplate();
-
-		if ($output_now) {
-			echo $result;
-		} else {
-			$this->retVals = $result;
+		public function getRetVals()
+		{
+			return $this->retVals;
 		}
 	}
-
-
-	public function getRetVals()
-	{
-		return $this->retVals;
-	}
-}
