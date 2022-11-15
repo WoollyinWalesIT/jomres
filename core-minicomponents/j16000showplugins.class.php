12986e3a0394dce43da236ed4e3b0aee671937b5
Vince Wooll <vince.wooll@gmail.com>
2022-11-15 08:20:47 +0200

    Change a directory definition to adapt to PHP8.x related changes.

--- core-minicomponents/j16000showplugins.class.php
+++ core-minicomponents/j16000showplugins.class.php
@@ -193,7 +193,7 @@ function createPath($path)
 	if (is_dir($path)) {
 		return true;
 	}
-	$prev_path = substr($path, 0, strrpos($path, '/', -2) + 1);
+	$prev_path = substr($path, 0, strrpos($path, JRDS, -2) + 1);
 	$return = createPath($prev_path);
 	return ($return && is_writable($prev_path)) ? mkdir($path) : false;
 }
