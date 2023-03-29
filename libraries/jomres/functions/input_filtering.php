43c26b5c30909ef41014150b6959946b3d164978
Vince Wooll <vince.wooll@gmail.com>
2023-03-29 10:59:10 +0300

    Update input filtering for Joomla 4 to resolve J5 depreciation

--- libraries/jomres/functions/input_filtering.php
+++ libraries/jomres/functions/input_filtering.php
@@ -168,9 +168,14 @@ function jomresGetParam($request, $element, $def = null) // variable type not us
 	}
 
 	if (!jomres_cmsspecific_areweinadminarea() && this_cms_is_joomla() && get_showtime('sef') == '1' && !AJAXCALL && !defined('AUTO_UPGRADE') && in_array($element, $sef_vars) && !array_key_exists($element, $request)) {
-		$app = Factory::getApplication();
-		$input = $app->input;
-		$dirty = $input->get($element, $def, 'STRING');
+		if ( _JOMRES_DETECTED_CMS == 'joomla4' || _JOMRES_DETECTED_CMS == 'joomla5') {
+			$dirty = Factory::getApplication()->getInput()->get($element);
+		} else {
+			$app = Factory::getApplication();
+			$input = $app->input;
+			$dirty = $input->get($element, $def, 'STRING');
+		}
+
 	} elseif (isset($request[ $element ])) {
 		$dirty = $request[ $element ];
 	} else {
