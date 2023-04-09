9faa404dc9b9efadbb6715d265213225e96594e1
Vince Wooll <vince.wooll@gmail.com>
2023-04-09 14:05:13 +0300

    Change how we detect the root directory and trigger the installation location, try our damnedest to install in /jomres not the plugin dir where possible.

--- libraries/jomres/cms_specific/wordpress/cms_plugin/jomres.php
+++ libraries/jomres/cms_specific/wordpress/cms_plugin/jomres.php
@@ -210,26 +210,33 @@ if (!function_exists('run_jomres_installer')) {
 	/**
 	 * Jomres root directory.
 	 */
-if (! defined('JOMRES_ROOT_DIRECTORY')) {
-	if (file_exists(ABSPATH . 'jomres_root.php')) {
-		require_once ABSPATH . 'jomres_root.php';
-	} elseif (file_exists(ABSPATH.'jomres'.DIRECTORY_SEPARATOR.'configuration.php')) {
+	if (! defined('JOMRES_ROOT_DIRECTORY')) {
+		if (is_dir(ABSPATH.'jomres')) {
 			define_jomres_off_root_dir_as_root();
-	} else {
-		if (file_exists(JOMRES_WP_PLUGIN_PATH.DIRECTORY_SEPARATOR.'jomres'.DIRECTORY_SEPARATOR.'jomres.php')) {
-			define_jomres_sub_dir_in_plugins_dir_as_root();
+			if (!file_exists(ABSPATH.'jomres'.DIRECTORY_SEPARATOR.'configuration.php'))  {
+				run_jomres_installer();
+			}
 		} else {
-			if (!is_dir(JOMRES_WP_PLUGIN_PATH . DIRECTORY_SEPARATOR . 'jomres' . DIRECTORY_SEPARATOR)) { // Let's see if we can install Jomres in the jomres subdir of wp-content, instead of it's traditional location off root
-				if (mkdir(JOMRES_WP_PLUGIN_PATH . DIRECTORY_SEPARATOR . 'jomres' . DIRECTORY_SEPARATOR)) {
+			if (file_exists(ABSPATH . 'jomres_root.php')) {
+				require_once ABSPATH . 'jomres_root.php';
+			} elseif (file_exists(ABSPATH.'jomres'.DIRECTORY_SEPARATOR.'configuration.php')) {
+				define_jomres_off_root_dir_as_root();
+			} else {
+				if (file_exists(JOMRES_WP_PLUGIN_PATH.DIRECTORY_SEPARATOR.'jomres'.DIRECTORY_SEPARATOR.'jomres.php')) {
 					define_jomres_sub_dir_in_plugins_dir_as_root();
-					run_jomres_installer();
+				} else {
+					if (!is_dir(JOMRES_WP_PLUGIN_PATH . DIRECTORY_SEPARATOR . 'jomres' . DIRECTORY_SEPARATOR)) { // Let's see if we can install Jomres in the jomres subdir of wp-content, instead of it's traditional location off root
+						if (mkdir(JOMRES_WP_PLUGIN_PATH . DIRECTORY_SEPARATOR . 'jomres' . DIRECTORY_SEPARATOR)) {
+							define_jomres_sub_dir_in_plugins_dir_as_root();
+							run_jomres_installer();
+						}
+					} else { // fallback for updated installations
+						define('JOMRES_ROOT_DIRECTORY', 'jomres');
+					}
 				}
-			} else { // fallback for updated installations
-				define('JOMRES_ROOT_DIRECTORY', 'jomres');
 			}
 		}
 	}
-}
 
 	/**
 	 * The Jomres wp plugin functions.
