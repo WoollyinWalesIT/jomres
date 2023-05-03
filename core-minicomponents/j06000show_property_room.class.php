421fee37db88ab1f6268fd101e9681bf5e054acd
Vince Wooll <vince.wooll@gmail.com>
2023-05-03 12:16:33 +0300

    Add larger images to room feature images array

--- core-minicomponents/j06000show_property_room.class.php
+++ core-minicomponents/j06000show_property_room.class.php
@@ -140,7 +140,12 @@
 					$fr = [];
 					if ($f != '') {
 						if (jomres_bootstrap_version() == '5') {
-							$p = [ 0 => [ 'IMAGE' => $basic_room_details->all_room_features[$f]['image'] , 'FEATURE_DESCRIPTION' => $basic_room_details->all_room_features[$f]['feature_description'] ] ];
+							$p = [ 0 => [
+								'IMAGE' => $basic_room_details->all_room_features[$f]['image'] ,
+								'SMALL' => $basic_room_details->all_room_features[$f]['small'] ,
+								'MEDIUM' => $basic_room_details->all_room_features[$f]['medium'] ,
+								'LARGE' => $basic_room_details->all_room_features[$f]['large'] ,
+								'FEATURE_DESCRIPTION' => $basic_room_details->all_room_features[$f]['feature_description'] ] ];
 
 							$tmpl = new patTemplate();
 							$tmpl->addRows('pageoutput', $p);
