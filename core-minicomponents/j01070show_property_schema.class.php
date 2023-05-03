e72326a35cd06d7892175bc8847d3e13c5b1c017
Vince Wooll <vince.wooll@gmail.com>
2023-05-03 13:28:50 +0300

    Fix a couple of notices in Schema script

--- core-minicomponents/j01070show_property_schema.class.php
+++ core-minicomponents/j01070show_property_schema.class.php
@@ -298,7 +298,12 @@ class j01070show_property_schema
 					$r[ 'ROOMNUMBER' ] = stripslashes($room['room_number']);
 					$r[ 'ROOMFLOOR' ] = stripslashes($room['room_floor']);
 					$r[ 'MAXPEOPLE' ] = $room['max_people'];
-					$r[ 'BED' ] = $current_property_details->room_types[$room_type_id]['abbv'];
+					if (isset($current_property_details->room_types[$room_type_id]['abbv'])) {
+						$r[ 'BED' ] = $current_property_details->room_types[$room_type_id]['abbv'];
+					} else {
+						$r['BED'] = '';
+					}
+
 
 					$r[ 'IMAGEMEDIUM' ] = JOMRES_IMAGES_RELPATH.'noimage.svg';
 
@@ -397,8 +402,13 @@ class j01070show_property_schema
 					if ($tariff->allow_we == '0') {
 						$r[ 'NOTWEEKENDS' ] = jr_gettext('_JOMRES_FRONT_TARIFFS_NOTWEEKEND', '_JOMRES_FRONT_TARIFFS_NOTWEEKEND');
 					}
-					
-					$r['ROOM_TYPE_IMAGE'] = $room_type_images[$tariff->roomclass_uid];
+
+					if (isset($room_type_images[$tariff->roomclass_uid])) {
+						$r['ROOM_TYPE_IMAGE'] = $room_type_images[$tariff->roomclass_uid];
+					} else {
+						$r['ROOM_TYPE_IMAGE'] = '';
+					}
+
 					$tariff_deets[ ] = $r;
 				}
 			}
