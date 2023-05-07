10466c0e1fa72b262711ff8d2c1199852ede429b
Vince Wooll <vince.wooll@gmail.com>
2023-05-07 18:01:27 +0300

    Added a feature that allows you to set the language context to use in shortcodes and other urls.
    In Admin > Jomres > Property Types you can see the language context in the list of property types. In Translate lang file strings, you will see that you can switch language context at the top of the page. This allows you to modify existing strings so that the modification is assigned to a specific property type. 
    
    What does this mean in practical terms? Let's take the Camp site property type as an example.  When we change the Translate lang file strings language context to Camp site, then reword "Our Rooms" to "Our pitches", then in the Property Details page the heading "Our rooms" is changed to Our pitches". 
    
    Another example, from the Todo list :  We can change "Ok, let's get started. First you need to create some rooms for this property." to "Ok, let's get started. First you need to create some pitches for this camp site."
    
    We can also add

---
+++ core-minicomponents/j00003get_language_context.class.php
@@ -0,0 +1,78 @@
+<?php
+	/**
+	 * Core file.
+	 *
+	 * @author Vince Wooll <sales@jomres.net>
+	 *
+	  *  @version Jomres 10.7.0
+	 *
+	 * @copyright	2005-2023 Vince Wooll
+	 * Jomres (tm) PHP, CSS & Javascript files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly
+	 **/
+
+// ################################################################
+	defined('_JOMRES_INITCHECK') or die('');
+// ################################################################
+	#[AllowDynamicProperties]
+	/**
+	 * @package Jomres\Core\Minicomponents
+	 *
+	 * The script makes sure that the jomres_bootstrap_version is set before any other scripts are called, due to changes how J4 calls web assets we need to prioritise that variable
+	 *
+	 */
+
+class j00003get_language_context
+{
+	/**
+	 *
+	 * Constructor
+	 *
+	 * Main functionality of the Minicomponent
+	 *
+	 * Language context is about the property type, and is used to determine which language strings to use. This script allows urls and/or shortcodes to set which language context to use over the course of the script's run.
+	 *
+     * It is designed to be used by shortcodes which will set their own language context by adding &language_context=XXX (eg &language_context=campsite) to the arguments. For that shortcode only, Jomres will use language strings set in the Translate Lang Files page for the appropriate property type.
+     *
+     * This may cause some confusion because subsequent ajax calls will not append language_context to subsequent urls, so the language context will revert to the default property type. This is inevitable and cannot be avoided.
+     *
+     * Different property types, however, when called (e.g. when basic module output function is used) will still pull language strings that are appropriate to its property type (language context)
+     *
+     * 
+     *
+	 */
+
+	public function __construct($componentArgs)
+	{
+		// Must be in all minicomponents. Minicomponents with templates that can contain editable text should run $this->template_touch() else just return
+		$MiniComponents = jomres_singleton_abstract::getInstance('mcHandler');
+		if ($MiniComponents->template_touch) {
+			$this->template_touchable = false;
+
+			return;
+		}
+
+        $siteConfig = jomres_singleton_abstract::getInstance('jomres_config_site_singleton');
+        $jrConfig = $siteConfig->get();
+
+        $jomres_property_types = jomres_singleton_abstract::getInstance('jomres_property_types');
+        $jomres_property_types->get_all_property_types();
+
+        foreach ($jomres_property_types->property_types as $p) {
+            if ( isset($p['ptype_desc']) && trim($p['ptype_desc']) != '') {
+                $valid_property_types[] = $p['ptype_desc'];
+            }
+        }
+
+        $language_context = jomresGetParam($_REQUEST, 'language_context', $jrConfig['language_context']);
+        if (in_array($language_context , $valid_property_types)) {
+            set_showtime('property_type', $language_context);
+        }
+
+	}
+
+
+	public function getRetVals()
+	{
+		return null;
+	}
+}
