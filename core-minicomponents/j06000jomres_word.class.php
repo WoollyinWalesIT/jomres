6e7501920351aa308bbd0c83eaa3da76295fe4d2
Vince Wooll <vince.wooll@gmail.com>
2023-05-18 19:16:40 +0300

    Added a simple script to output a language definition from Jomres language files through a shortcode.

---
+++ core-minicomponents/j06000jomres_word.class.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Core file.
+ *
+ * @author Vince Wooll <sales@jomres.net>
+ *
+ *  @version Jomres 10.7.0
+ *
+ * @copyright	2005-2023 Vince Wooll
+ * Jomres (tm) PHP, CSS & Javascript files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly
+ **/
+
+// ################################################################
+defined('_JOMRES_INITCHECK') or die('');
+// ################################################################
+	#[AllowDynamicProperties]
+	/**
+	 * @package Jomres\Core\Minicomponents
+	 *
+	 * Ajax script. Set the guest budget found in the list properties page
+	 *
+	 */
+
+class j06000jomres_word
+{
+
+	/**
+	 *
+	 * Constructor
+	 *
+	 * Main functionality of the Minicomponent
+	 *
+	 *
+	 *
+	 */
+	 
+	public function __construct()
+	{
+		$MiniComponents = jomres_singleton_abstract::getInstance('mcHandler');
+		if ($MiniComponents->template_touch) {
+			$this->template_touchable = false;
+
+			return;
+		}
+
+        $word = jomresGetParam($_REQUEST, 'word', '');
+        var_dump($word);exit;
+        echo jr_gettext( $word, $word, false , false );
+	}
+
+
+	public function getRetVals()
+	{
+		return null;
+	}
+}
