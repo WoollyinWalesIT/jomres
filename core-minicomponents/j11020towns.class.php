9cd5e93ec890dc8676398eedabfaea2f24ff4efc
Vince Wooll <vince.wooll@gmail.com>
2023-05-20 20:08:15 +0300

    Use Jomres Decode on town names.

--- core-minicomponents/j11020towns.class.php
+++ core-minicomponents/j11020towns.class.php
@@ -13,66 +13,66 @@
 // ################################################################
 defined('_JOMRES_INITCHECK') or die('');
 // ################################################################
-	#[AllowDynamicProperties]
-	/**
-	 * @package Jomres\Core\Minicomponents
-	 *
-	 *
-	 */
+#[AllowDynamicProperties]
+/**
+ * @package Jomres\Core\Minicomponents
+ *
+ *
+ */
 
 class j11020towns
 {
 
-	/**
-	 *
-	 * Constructor
-	 *
-	 * Main functionality of the Minicomponent
-	 *
-	 *
-	 *
-	 */
-	 
-	function __construct($componentArgs)
-	{
-		// Must be in all minicomponents. Minicomponents with templates that can contain editable text should run $this->template_touch() else just return
-		$MiniComponents = jomres_singleton_abstract::getInstance('mcHandler');
-		if ($MiniComponents->template_touch) {
-			$this->template_touchable = false;
-			return;
-		}
-		$thisJRUser = jomres_singleton_abstract::getInstance('jr_user');
-		if (!$thisJRUser->userIsManager) {
-			return;
-		}
-		
-		$dropdown = '';
+    /**
+     *
+     * Constructor
+     *
+     * Main functionality of the Minicomponent
+     *
+     *
+     *
+     */
 
-		//big results list on big sites...we need a better way, because grouping by property town won`t work if mysql mode is set to ONLY_FULL_GROUP_BY
-		$query = "SELECT `propertys_uid`, `property_town` FROM #__jomres_propertys WHERE `property_town` != ''";
-		$townsList = doSelectSql($query);
+    function __construct($componentArgs)
+    {
+        // Must be in all minicomponents. Minicomponents with templates that can contain editable text should run $this->template_touch() else just return
+        $MiniComponents = jomres_singleton_abstract::getInstance('mcHandler');
+        if ($MiniComponents->template_touch) {
+            $this->template_touchable = false;
+            return;
+        }
+        $thisJRUser = jomres_singleton_abstract::getInstance('jr_user');
+        if (!$thisJRUser->userIsManager) {
+            return;
+        }
 
-		if (!empty($townsList)) {
-			foreach ($townsList as $t) {
-				$towns[$t->propertys_uid] = $t->property_town;
-			}
-			
-			$towns = array_unique($towns);
-			
-			$resource_options = array();
-			foreach ($towns as $k => $v) {
-				set_showtime('property_uid', $k);
-				$resource_options[ ] = jomresHTML::makeOption(jomres_cmsspecific_stringURLSafe($v), jr_gettext('_JOMRES_CUSTOMTEXT_PROPERTY_TOWN', htmlspecialchars(trim(stripslashes($v)), ENT_QUOTES)));
-			}
-			$use_bootstrap_radios = false;
-			$dropdown = jomresHTML::selectList($resource_options, 'resource_id', ' autocomplete="off" class="btn btn-success btn-lg" size="1" ', 'value', 'text', '', $use_bootstrap_radios);
-		}
-		$this->ret_vals = $dropdown;
-	}
+        $dropdown = '';
+
+        //big results list on big sites...we need a better way, because grouping by property town won`t work if mysql mode is set to ONLY_FULL_GROUP_BY
+        $query = "SELECT `propertys_uid`, `property_town` FROM #__jomres_propertys WHERE `property_town` != ''";
+        $townsList = doSelectSql($query);
+
+        if (!empty($townsList)) {
+            foreach ($townsList as $t) {
+                $towns[$t->propertys_uid] = $t->property_town;
+            }
+
+            $towns = array_unique($towns);
+
+            $resource_options = array();
+            foreach ($towns as $k => $v) {
+                set_showtime('property_uid', $k);
+                $resource_options[ ] = jomresHTML::makeOption(jomres_cmsspecific_stringURLSafe($v), jr_gettext('_JOMRES_CUSTOMTEXT_PROPERTY_TOWN', jomres_decode($v), ENT_QUOTES));
+            }
+            $use_bootstrap_radios = false;
+            $dropdown = jomresHTML::selectList($resource_options, 'resource_id', ' autocomplete="off" class="btn btn-success btn-lg" size="1" ', 'value', 'text', '', $use_bootstrap_radios);
+        }
+        $this->ret_vals = $dropdown;
+    }
 
 
-	function getRetVals()
-	{
-		return $this->ret_vals;
-	}
+    function getRetVals()
+    {
+        return $this->ret_vals;
+    }
 }
